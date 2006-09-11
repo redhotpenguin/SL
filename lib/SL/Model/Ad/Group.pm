534a6d0fd7f96b5569fb590a3e64cf82cb25a2b4
Fred Moyer <fred@redhotpenguin.com>
2006-09-11 07:01:15 +0000

    Ooops forgot this ad group stuff with the last commit.
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@484 bd012f14-6cfe-0310-9a23-91b8f1fede10

---
+++ lib/SL/Model/Ad/Group.pm
@@ -0,0 +1,50 @@
+package SL::Model::Ad::Group;
+
+use strict;
+use warnings;
+
+use base 'SL::Model';
+
+use constant DEFAULT_GROUP_ID => 1;
+
+our $default_group;
+
+sub from_ip {
+    my $class = shift;
+    my $ip    = shift;
+    my $dbh   = $class->connect;
+    my $sql   = <<SQL;
+SELECT ad_group.name
+FROM ad_group
+INNER JOIN reg_ad_group USING (ad_group_id)
+INNER JOIN reg USING (reg_id)
+WHERE reg.ip = ?
+SQL
+    my $sth = $dbh->prepare($sql);
+    $sth->bind_param( 1, $ip );
+    $sth->execute or return;
+    my $array_ref = $sth->fetchrow_arrayref;
+    return $array_ref->[0];
+}
+
+BEGIN {
+    $default_group = __PACKAGE__->default_group;
+
+    sub default_group {
+        my $class = shift;
+        return $default_group if $default_group;
+        my $dbh = $class->connect;
+        my $sql = <<SQL;
+SELECT ad_group.name
+FROM ad_group
+WHERE ad_group_id = ?
+SQL
+        my $sth = $dbh->prepare($sql);
+        $sth->bind_param( 1, DEFAULT_GROUP_ID );
+        $sth->execute or die;
+        my $array_ref = $sth->fetchrow_arrayref;
+        $default_group = $array_ref->[0] || die;
+    }
+}
+
+1;
---
+++ lib/SL/Model/URL.pm
@@ -0,0 +1,33 @@
+package SL::Model::URL;
+
+use strict;
+use warnings;
+
+use base 'SL::Model';
+use Regexp::Assemble;
+
+sub get_blacklisted_urls {
+	my $class = shift;
+	my $dbh = SL::Model->connect;
+	my $sql = <<SQL;
+SELECT url
+FROM url
+WHERE
+blacklisted = 't'
+SQL
+	my $sth = $dbh->prepare($sql);
+	$sth->execute;
+	my @urls = map { $_->[0] } @{$sth->fetchall_arrayref};
+	return wantarray ? @urls : \@urls;
+}
+
+sub blacklist_regex {
+	my ($class) = @_;
+	my @blacklists = $class->get_blacklisted_urls;
+	my $blacklist_regex = Regexp::Assemble->new;
+	$blacklist_regex->add(@blacklists);
+	print STDERR "Regex for blacklist_urls: ", $blacklist_regex->re, "\n\n";
+	return $blacklist_regex;
+}
+
+1;
