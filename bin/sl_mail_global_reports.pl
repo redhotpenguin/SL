fc3b2609a4dac0188a0602c48302f5dd49ed47e9
Fred Moyer <fred@redhotpenguin.com>
2006-11-29 07:19:40 +0000

    Mailer to mail the burned graphs.
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@512 bd012f14-6cfe-0310-9a23-91b8f1fede10

---
+++ bin/sl_mail_global_reports.pl
@@ -0,0 +1,28 @@
+use strict;
+use warnings;
+
+use MIME::Lite;
+
+my $FROM    = "SL Reporting Daemon <fred\@redhotpenguin.com";
+my $TO      = "info\@redhotpenguin.com";
+my $SUBJECT = "Global SL Report Graphs";
+my $dir     = "/tmp/data/sl/global/daily";
+
+my $msg = MIME::lite->new(
+    From    => $FROM,
+    To      => $TO,
+    Subject => $SUBJECT,
+    Type    => 'TEXT',
+    Data    => "Reports for all routers attached"
+);
+
+foreach my $type qw( views clicks rates ads ) {
+    $msg->attach(
+        Type     => 'image/png',
+        Path     => "$dir/$type.png",
+        Filename => "$type.png",
+    );
+}
+
+$msg->send;
+
