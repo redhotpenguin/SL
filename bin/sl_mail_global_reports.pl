ddb3db612ef2c2fb73e909473805c0ed771e5220
Fred Moyer <fred@redhotpenguin.com>
2006-12-16 20:33:35 +0000

    Make this configurable so that we can control it from cron.
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@518 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- bin/sl_mail_global_reports.pl
+++ bin/sl_mail_global_reports.pl
@@ -1,22 +1,52 @@
 use strict;
 use warnings;
 
+=head1 NAME
+
+ sl_mail_global_reports.pl
+
+=head1 SYNOPSIS
+
+ perl sl_mail_global_reports.pl --to=recip@rhp.com --interval=daily --interval=weekly --interval=monthly --interval=quarterly
+
+ perl sl_mail_global_reports.pl --help
+ 
+ perl sl_mail_global_reports.pl --man
+
+=cut
+
+use Getopt::Long;
+use Pod::Usage;
+
+my (@intervals, $to);
+my ($help, $man);
+
+pod2usage(1) unless @ARGV;
+GetOptions(
+	'to=s' => \$to,
+	'interval=s' => \@intervals,
+	'help' => \$help,
+	'man' => \$man,
+) or pod2usage(2);
+
+pod2usage(1) if $help;
+pod2usage( -verbose => 2) if $man;
+
 use MIME::Lite;
 
 my $FROM    = "SL Reporting Daemon <fred\@redhotpenguin.com>";
-my $TO      = "fred\@redhotpenguin.com";
 my $SUBJECT = "Global SL Report Graphs";
 my $dir     = "/tmp/data/sl/global";
 
 my $msg = MIME::Lite->new(
     From    => $FROM,
-    To      => $TO,
+    To      => $to,
     Subject => $SUBJECT,
     Type    => 'TEXT',
     Data    => "Reports for all routers attached"
 );
 
-foreach my $temporal qw( daily weekly monthly quarterly ) {
+foreach my $temporal ( @intervals ) {
 	foreach my $type qw( views clicks rates ads ) {
 		$msg->attach(
 			Type     => 'image/png',
