4f59f04d0efe4409ae90023c9f553e10c16dd26f
Fred Moyer <fred@redhotpenguin.com>
2007-01-10 05:58:00 +0000

    Hope this works.
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@559 bd012f14-6cfe-0310-9a23-91b8f1fede10

---
+++ bin/sl_update_schema.pl
@@ -0,0 +1,209 @@
+#!perl -w
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+use Pod::Usage;
+
+# Config options
+my ($from, $to);
+my ($help, $man);
+
+pod2usage(1) unless @ARGV;
+GetOptions(
+	'from=s' => \$from,
+	'to=s' => \$to,
+	'help'	   => \$help,
+	'man'	   => \$man,
+) or pod2usage(2);
+
+pod2usage(1) if $help;
+pod2usage( -verbose => 2) if $man;
+
+use DBI;
+
+my $db_options = {
+                  RaiseError         => 1,
+                  PrintError         => 1,
+                  AutoCommit         => 1,
+                  FetchHashKeyName   => 'NAME_lc',
+                  ShowErrorStatement => 1,
+                  ChopBlanks         => 1,
+			  };
+
+
+my $dsn = "dbi:Pg:dbname='sl';host=localhost";
+my $dbh = DBI->connect($dsn, 'phred', '', $db_options);
+
+$dsn = "dbi:Pg:dbname='sl2';host=localhost";
+my $dbh2 = DBI->connect($dsn, 'phred', '', $db_options);
+
+############################
+# ad_groups
+
+my $sql = <<SQL;
+SELECT ad_group_id, name
+FROM ad_group
+SQL
+
+my $group_hashref = $dbh->selectall_hashref($sql);
+
+$sql = <<SQL;
+INSERT INTO ad_sl_group
+VALUES (?,?)
+SQL
+
+my $sth = $dbh2->prepare($sql);
+foreach my $group (@{$group_hashref}) {
+    $sth->bind_param(1, $group->{ad_group_id});
+	$sth->bind_param(2, $group->{name});
+	$sth->execute;
+}
+
+#############################
+# ads
+$sql = <<SQL;
+SELECT ad.ad_id, ad.active, ad.ad_group_id, ad.text, link.uri, link.md5
+FROM ad
+JOIN link USING (ad_id)
+SQL
+
+my $ad_hashref = $dbh->selectall_hashref($sql);
+
+# put them in sl2
+$sql = <<SQL;
+INSERT INTO ad (ad_id, active, md5)
+VALUES (?, ?, ?)
+SQL
+
+$sth = $dbh2->prepare($sql);
+
+my $sql2 = <<SQL;
+INSERT INTO ad_sl (ad_id, text, uri, ad_sl_group_id)
+VALUES (?, ?, ?, ?)
+SQL
+my $sth2 = $dbh2->prepare($sql2);
+
+foreach my $ad (@{$ad_hashref}) {
+    $sth->bind_param(1, $ad->{ad_id});
+    $sth->bind_param(2, $ad->{active});
+    $sth->bind_param(3, $ad->{md5});
+    $sth->execute;
+
+	$sth2->bind_param(1, $ad->{ad_id});
+    $sth2->bind_param(2, $ad->{text});
+    $sth2->bind_param(3, $ad->{uri});
+    $sth2->bind_param(4, $ad->{ad_group_id});
+    $sth2->execute;
+}
+
+################################
+# views
+
+$sql = <<SQL;
+SELECT ad_id, ts, ip
+FROM view
+SQL
+
+my $view_arrayref = $dbh->selectall_arrayref($sql);
+
+$sql = <<SQL;
+INSERT INTO view (ad_id, cts, ip)
+VALUES (?,?,?)
+SQL
+
+$sth = $dbh2->prepare($sql);
+
+foreach my $view (@{$view_arrayref}) {
+    $sth->bind_param(1, $view->[0]);
+    $sth->bind_param(2, $view->[1]);
+    $sth->bind_param(3, $view->[2]);
+    $sth->execute;
+}
+
+#############################
+# clicks
+
+$sql = <<SQL;
+SELECT click.ts, click.ip, link.ad_id
+FROM click
+JOIN link USING (link_id)
+SQL
+
+my $click_arrayref = $dbh->selectall_arrayref($sql);
+
+$sql = <<SQL;
+INSERT INTO click (cts, ip, ad_id)
+VALUES (?, ?, ?)
+SQL
+
+$sth = $dbh2->prepare($sql);
+
+foreach my $click (@{$click_arrayref}) {
+    $sth->bind_param(1, $click->[0]);
+    $sth->bind_param(2, $click->[1]);
+    $sth->bind_param(3, $click->[2]);
+	$sth->execute;
+}
+
+1;
+
+__END__
+
+=head1 DESCRIPTION
+
+[DESCRIPTION]
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<opt1>
+
+The first option
+
+=item B<opt2>
+
+The second option
+
+=back
+
+=head1 TODO
+
+=over 4
+
+=item *
+
+Todo #1
+
+=back
+
+=head1 BUGS
+
+None yet
+
+=head1 AUTHOR
+
+[AUTHOR]
+
+=cut
+
+#===============================================================================
+#
+#         FILE:  sl_update_schema.pl
+#
+#        USAGE:  ./sl_update_schema.pl 
+#
+#  DESCRIPTION:  
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:   (), <>
+#      COMPANY:  
+#      VERSION:  1.0
+#      CREATED:  01/08/07 08:52:50 PST
+#     REVISION:  ---
+#===============================================================================
