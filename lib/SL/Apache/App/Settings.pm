5c523042a5662a36d7e78f18ef350e083e82e785
Fred Moyer <fred@redhotpenguin.com>
2007-02-06 05:28:07 +0000

    Preferences page functional.
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@596 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- lib/SL/Apache/App/Settings.pm
+++ lib/SL/Apache/App/Settings.pm
@@ -6,10 +6,11 @@ use warnings;
 use Apache2::Const -compile => qw( OK M_POST M_GET);
 use Apache2::Log     ();
 use Apache2::Request ();
-use Apache2::Upload;
+use Apache2::Upload  ();
+use Data::FormValidator ();
+use Digest::MD5  ();
 
 use base 'SL::Apache::App';
-
 use SL::Config;
 my $config    = SL::Config->new();
 my $DATA_ROOT = $config->sl_data_root;
@@ -39,6 +40,62 @@ sub dispatch_index {
           : return $self->error( $r, "Template error: " . $tmpl->error() );
 
     }
+}
+
+sub dispatch_account {
+  my ($self, $r, $errors ) = @_;
+  my $req = Apache2::Request->new($r);
+ $r->log->error("IN !");
+       my %tmpl_data = (
+            root   => $r->pnotes('root'),
+            reg    => $r->pnotes( $r->user ),
+            status => $req->param('status') || '',
+            password_updated => $req->param('password_updated') || '',
+        );
+
+  if ( $r->method_number == Apache2::Const::M_GET ) {
+    $tmpl_data{'errors'} = $errors  if ( keys %{$errors} );
+
+  my $output;
+  my $ok = $tmpl->process( 'settings/account.tmpl', \%tmpl_data, \$output );
+  $ok ?
+    return $self->ok($r, $output )
+  : return $self->error($r, "Template error: " . $tmpl->error );
+  }
+  elsif ($r->method_number == Apache2::Const::M_POST) {
+     my %errors;
+    
+       $r->method_number(Apache2::Const::M_GET);
+     # handle the password if present
+     my $path = '?';
+       if ($req->param('password') or $req->param('retype')) {
+         unless ($req->param('password') eq $req->param('retype')) {
+           $errors{'password'} = '1';
+         } else {
+         # update the password
+         $r->pnotes($r->user)->password_md5(
+             Digest::MD5::md5_hex($req->param('password')));
+         $r->pnotes($r->user)->update;
+         $path .= 'password_updated=1&';
+       }
+       }
+     # handle errors
+     if (keys %errors) {;
+       return $self->dispatch_account($r, \%errors);
+     }
+       # handle the email
+       if ($req->param('email') 
+           && ($req->param('email') ne $r->pnotes($r->user)->email)) {
+         $r->pnotes($r->user)->email($req->param('email'));
+         $r->pnotes($r->user)->update;
+         $path .= 'email_updated=1&';
+       }
+
+     my $redir = $r->construct_url( $r->uri . "$path");
+     $r->log->error("REDIR is $redir");
+      $r->internal_redirect($redir);
+        return Apache2::Const::OK;
+  }
 }
 
 sub dispatch_router {
