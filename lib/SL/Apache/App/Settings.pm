e0047eb9eac86542e077fe83b03a99e13ae30b8f
Fred Moyer <fred@redhotpenguin.com>
2007-02-07 21:00:30 +0000

    Fix the image conversion
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@600 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- lib/SL/Apache/App/Settings.pm
+++ lib/SL/Apache/App/Settings.pm
@@ -4,11 +4,12 @@ use strict;
 use warnings;
 
 use Apache2::Const -compile => qw( OK M_POST M_GET);
-use Apache2::Log     ();
-use Apache2::Request ();
-use Apache2::Upload  ();
+use Apache2::Log        ();
+use Apache2::Request    ();
+use Apache2::Upload     ();
+use Apache2::ServerUtil ();
 use Data::FormValidator ();
-use Digest::MD5  ();
+use Digest::MD5         ();
 
 use base 'SL::Apache::App';
 use SL::Config;
@@ -38,64 +39,72 @@ sub dispatch_index {
         $ok
           ? return $self->ok( $r, $output )
           : return $self->error( $r, "Template error: " . $tmpl->error() );
-
     }
 }
 
 sub dispatch_account {
-  my ($self, $r, $errors ) = @_;
-  my $req = Apache2::Request->new($r);
- $r->log->error("IN !");
-       my %tmpl_data = (
-            root   => $r->pnotes('root'),
-            reg    => $r->pnotes( $r->user ),
-            status => $req->param('status') || '',
-            password_updated => $req->param('password_updated') || '',
-        );
+    my ( $self, $r, $errors ) = @_;
+    my $req = Apache2::Request->new($r);
+    $r->log->error("IN !");
+    my %tmpl_data = (
+        root             => $r->pnotes('root'),
+        reg              => $r->pnotes( $r->user ),
+        status           => $req->param('status') || '',
+        password_updated => $req->param('password_updated') || '',
+    );
 
-  if ( $r->method_number == Apache2::Const::M_GET ) {
-    $tmpl_data{'errors'} = $errors  if ( keys %{$errors} );
+    if ( $r->method_number == Apache2::Const::M_GET ) {
+        $tmpl_data{'errors'} = $errors if ( keys %{$errors} );
 
-  my $output;
-  my $ok = $tmpl->process( 'settings/account.tmpl', \%tmpl_data, \$output );
-  $ok ?
-    return $self->ok($r, $output )
-  : return $self->error($r, "Template error: " . $tmpl->error );
-  }
-  elsif ($r->method_number == Apache2::Const::M_POST) {
-     my %errors;
-    
-       $r->method_number(Apache2::Const::M_GET);
-     # handle the password if present
-     my $path = '?';
-       if ($req->param('password') or $req->param('retype')) {
-         unless ($req->param('password') eq $req->param('retype')) {
-           $errors{'password'} = '1';
-         } else {
-         # update the password
-         $r->pnotes($r->user)->password_md5(
-             Digest::MD5::md5_hex($req->param('password')));
-         $r->pnotes($r->user)->update;
-         $path .= 'password_updated=1&';
-       }
-       }
-     # handle errors
-     if (keys %errors) {;
-       return $self->dispatch_account($r, \%errors);
-     }
-       # handle the email
-       if ($req->param('email') 
-           && ($req->param('email') ne $r->pnotes($r->user)->email)) {
-         $r->pnotes($r->user)->email($req->param('email'));
-         $r->pnotes($r->user)->update;
-         $path .= 'email_updated=1&';
-       }
+        my $output;
+        my $ok =
+          $tmpl->process( 'settings/account.tmpl', \%tmpl_data, \$output );
+        $ok
+          ? return $self->ok( $r, $output )
+          : return $self->error( $r, "Template error: " . $tmpl->error );
+    }
+    elsif ( $r->method_number == Apache2::Const::M_POST ) {
+        my %errors;
 
-     my $redir = $r->construct_url( $r->uri . "$path");
-     $r->log->error("REDIR is $redir");
-      $r->internal_redirect($redir);
+        $r->method_number(Apache2::Const::M_GET);
+
+        # handle the password if present
+        my $path = '?';
+        if ( $req->param('password') or $req->param('retype') ) {
+            unless ( $req->param('password') eq $req->param('retype') ) {
+                $errors{'password'} = '1';
+            }
+            else {
+
+                # update the password
+                $r->pnotes( $r->user )
+                  ->password_md5(
+                    Digest::MD5::md5_hex( $req->param('password') ) );
+                $r->pnotes( $r->user )->update;
+                $path .= 'password_updated=1&';
+            }
+        }
+
+        # handle errors
+        if ( keys %errors ) {
+            ;
+            return $self->dispatch_account( $r, \%errors );
+        }
+
+        # handle the email
+        if ( $req->param('email')
+            && ( $req->param('email') ne $r->pnotes( $r->user )->email ) )
+        {
+            $r->pnotes( $r->user )->email( $req->param('email') );
+            $r->pnotes( $r->user )->update;
+            $path .= 'email_updated=1&';
+        }
+
+        my $redir = $r->construct_url( $r->uri . "$path" );
+        $r->log->error("REDIR is $redir");
+        $r->internal_redirect($redir);
         return Apache2::Const::OK;
-  }
+    }
 }
 
 sub dispatch_router {
@@ -111,7 +120,7 @@ sub dispatch_router {
 
     return Apache2::Const::NOT_FOUND unless $router;
 
-    my $subdir = $r->pnotes( $r->user )->reg_id . "/" . $router->ip . "/img";
+    my $subdir = $r->pnotes( $r->user )->reg_id . "/" . $router->ip;
     my $dir    = "$DATA_ROOT/$subdir";
     unless ( -d $dir ) {
         ( system("mkdir -p $dir") == 0 ) or die "DIR IS $dir " . $!;
@@ -125,12 +134,12 @@ sub dispatch_router {
 
     if ( $r->method_number == Apache2::Const::M_GET ) {
         my %tmpl_data = (
-            root   => $r->pnotes('root'),
-            reg    => $r->pnotes( $r->user ),
-            status => $req->param('status') || '',
-			updated_name => $req->param('updated_name') || '',
-            router => $router,
-            file   => $img{'link'}
+            root         => $r->pnotes('root'),
+            reg          => $r->pnotes( $r->user ),
+            status       => $req->param('status') || '',
+            updated_name => $req->param('updated_name') || '',
+            router       => $router,
+            file         => $img{'link'}
         );
 
         my $output;
@@ -147,57 +156,48 @@ sub dispatch_router {
 
             # save the file
             if ( -e $img{'file'} ) {
-                unlink $img{'file'} or die "Could not unlink $img{'file'}, $!";
+                unlink $img{'file'}
+                  or die "Could not unlink $img{'file'}, $!";
             }
             my $new = $img{'file'} . '.new';
-			unlink ($new) if -e $new;
-			$upload->link( $new) or die "Could not link $img{'file'}, $!";
+            unlink($new) if -e $new;
+            $upload->link($new) or die "Could not link $img{'file'}, $!";
 
-			my $convert = `convert -sample 100 x 50 $new $img{'file'}`;
-			$r->log->error("Log of convert: $convert");
-			## are you my type?
-			#my $type = `file -b $img{'file'}`;
-			#if ( $type !~ m/GIF/ ) {
-			#		$r->log->error("NOT A GIF - $type");
-			#    $status = 'not_gif';
-			#    unlink $img{'file'} or die "Could not unlink $img{'file'}, $!";
-			# } else {
-            # it's a gif at least
-			# my ( $exact_type, $width, $height ) = $type =~
-			# m/(\w+) image data, version (?:\d+\w+), (\d+) x (\d+)/;
-		#m/^Image type is (\w+) image data, version (?:\d+\w+), (\d+) x (\d+)/;
-		#$r->log->error("Type is $exact_type, wid $width, h $height"); 
-                # is it the right size?
-				#       if ( ( $width > 100 ) or ( $height > 50 ) ) {
-				#		$status = 'not_sized';
-				#		unlink $img{'file'} or die "Could not unlink $img{'file'}, $!";
-				# }
-				#else {
-				$status = 'ok';
-				#$r->log->error(
-				#    "type: $exact_type, height: $height, width: $width");
-				#}
+            # convert the image
+            my $convert = `convert -sample 100x50 $new $img{'file'}`;
+            if ($convert) {
+                $r->log->error("$$ $self image conversion error: $convert");
+                die;
             }
-			# }
 
-		my $updated_name;
-        if ( $req->param('name') && ($req->param('name') ne $router->name) ) {
+            # push it to the image server
+            my $system_user = getpwuid( Apache2::ServerUtil->user_id );
+            my $static_host = $config->get('sl_app_static_host_ip');
+            my $push        =
+              `rsync -avze ssh $img{'file'} $system_user\@$static_host:`;
+            if ( $push =~ m/timed out/ ) {
+                $r->log->error("$$ $self rsync failure: $push");
+                die;
+            }
+            $status = 'ok';
+        }
+
+        my $updated_name;
+        if ( $req->param('name') && ( $req->param('name') ne $router->name ) ) {
             $router->name( $req->param('name') );
             $router->update;
-			$updated_name = 'ok';
+            $updated_name = 'ok';
         }
-		my $uri = '?';
-		if ($status) {
-			$uri .= "status=$status&";
-		}
-		if ($updated_name) {
-			$uri .= "updated_name=$updated_name&";
-		}
-		$uri .= "id=" . $router->router_id;
-		$r->log->error("URI is $uri");
-        $r->internal_redirect(
-			$r->construct_url($r->uri . $uri)
-        );
+        my $uri = '?';
+        if ($status) {
+            $uri .= "status=$status&";
+        }
+        if ($updated_name) {
+            $uri .= "updated_name=$updated_name&";
+        }
+        $uri .= "id=" . $router->router_id;
+        $r->log->error("URI is $uri");
+        $r->internal_redirect( $r->construct_url( $r->uri . $uri ) );
         return Apache2::Const::OK;
     }
 }
