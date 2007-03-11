8aaaeec865e07f032df5facecb0b59b0f59491c2
Sam Tregar <sam@tregar.com>
2007-03-11 20:18:30 +0000

    Added client for arbitrary host/port HTTP get requests.
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@649 bd012f14-6cfe-0310-9a23-91b8f1fede10

---
+++ src/SL-Client/lib/SL/Client/HTTP.pm
@@ -0,0 +1,125 @@
+package SL::Client::HTTP;
+use strict;
+use warnings;
+
+=head1 NAME
+
+SL::Client::HTTP - HTTP client for making requests to arbitrary host and port
+
+=head1 SYNOPSIS
+
+  use SL::Client::HTTP;
+
+  # make a request through an SL proxy at 192.168.1.50:8069
+  my $response = SL::Client::HTTP->get(
+    url => 'http://www.tronguy.net/pictures.shtml',
+    host => '192.168.1.50',
+    port => 8069);
+
+  # reponse is an HTTP::Response object, treat as usual
+  if ($response->is_success) {
+     my $content = $response->content;
+     # ...
+  }
+
+=head1 DESCRIPTION
+
+This module provides a way to make HTTP GET requests to arbitrary
+host/port combinations.  This is useful for testing SL proxy servers
+and may have other applications.
+
+=head1 INTERFACE
+
+=head2 get
+
+  $response = SL::Client::HTTP->get(
+    url => 'http://www.tronguy.net/pictures.shtml',
+    host => '192.168.1.50',
+    port => 8069,
+    headers => { 'User-Agent' => 'Opera' });
+
+The get() method requires three named parameters:
+
+  url - the URL for the request, including query params if any
+
+  host - the host to send the request to, does not need to match URL
+
+  port - the port to send the request to
+
+An optional 'headers' parameter is supported, which may contain a hash
+of headers to add to the response.  By default the 'User-Agent' header
+is set to:
+
+  Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.2) Gecko/20060308 Firefox/1.5.0.2
+
+The return value is a HTTP::Response object.  This method will die()
+if it encounters network communication problems.
+
+=cut
+
+
+use Params::Validate qw(validate HASHREF);
+use URI;
+use Net::HTTP;
+use HTTP::Response;
+use HTTP::Headers;
+use Carp qw(croak);
+
+sub get {
+    my $pkg  = shift;
+    my %args = validate(@_, { url  => 1,
+                              host => 1,
+                              port => 1,
+                              headers => { type => HASHREF, optional => 1 },
+                            });
+
+    my $url = URI->new($args{url})
+      or croak("Unable to parse url '$args{url}'.");
+    my $host = $args{host};
+    my $port = $args{port};
+    my $headers = $args{headers} || {};
+
+    # setup some default headers
+    $headers->{'User-Agent'} ||= 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.2) Gecko/20060308 Firefox/1.5.0.2';
+
+    my $http = Net::HTTP->new(Host => $url->host,
+                              PeerAddr => $host,
+                              PeerPort => $port);
+
+    # reinforce the point (Net::HTTP adds PeerPort to host during
+    # new())
+    $http->host($url->host);
+
+    # make the request
+    my $req = $url->path_query || "/";
+    $http->write_request(GET => $req, %$headers);
+
+    # get the resulr code, message and response headers
+    my ($code, $mess, @headers_out) = $http->read_response_headers;
+
+    # read response body
+    my $body = "";
+    while (1) {
+        my $buf;
+        my $n = $http->read_entity_body($buf, 10240);
+        die "read failed: $!" unless defined $n;
+        last unless $n;
+        $body .= $buf;
+    }
+
+    my $response = _build_response($code, $mess, \@headers_out, \$body);
+    return $response;
+}
+
+# turns data returned by Net::HTTP into a HTTP::Response object
+sub _build_response {
+    my ($code, $mess, $header_list, $body_ref) = @_;
+
+    my $header = HTTP::Headers->new(@$header_list);
+
+    my $response = HTTP::Response->new($code, $mess, $header, $$body_ref);
+    return $response;
+}
+
+
+1;
