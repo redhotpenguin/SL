09b4bed35468bb33c7398a44b47e0c642a84bb2e
Fred Moyer <fred@redhotpenguin.com>
2007-03-21 03:35:18 +0000

    Fix a bug with subrequests where no ad_data was present so no logging should take place.
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@680 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- lib/SL/Apache/Proxy/LogHandler.pm
+++ lib/SL/Apache/Proxy/LogHandler.pm
@@ -11,7 +11,10 @@ use Apache2::Log ();
 sub handler {
     my $r = shift;
     my $log_data = $r->pnotes('log_data');
-    my $logged = SL::Model::Ad->log_view( $log_data->[0], $log_data->[1] );
+	
+	# for subrequests we don't have any log_data since no ad was inserted
+	return Apache2::Const::DECLINED unless ($log_data->[0] and $log_data->[1]);
+	my $logged = SL::Model::Ad->log_view( $log_data->[0], $log_data->[1] );
 
     my $msg = sprintf("$$ logging view for ip %s, ad_id %d", @{$log_data});
     $r->log->debug($msg);
