dc1ad73937de88e78de18efadaa83a9648ee4b2c
Fred Moyer <fred@redhotpenguin.com>
2007-03-31 09:19:47 +0000

    Set the load limit at 1.5 Add a call to test that database connectivity exists, since that is the most common failure we have seen.
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@711 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- lib/SL/Apache/Proxy/PingHandler.pm
+++ lib/SL/Apache/Proxy/PingHandler.pm
@@ -6,12 +6,22 @@ use warnings;
 use Apache2::Const -compile => qw( OK SERVER_ERROR );
 use Apache2::Log ();
 use Sys::Load ();
+use SL::Model ();
 
-our $MAX_LOAD = 1;
+our $MAX_LOAD = 1.5;
 
 sub handler {
 	my $r = shift;
-    my $minute_avg = [Sys::Load::getload()]->[0];
+
+	# first check that a database handle is available
+	my $dbh = SL::Model->connect();
+	unless ($dbh) {
+		$r->log->error("Database has gone away :(");
+		return Apache2::Const::SERVER_ERROR;
+	}
+
+	# now check the load
+	my $minute_avg = [Sys::Load::getload()]->[0];
     return Apache2::Const::OK if $minute_avg < $MAX_LOAD;
 	$r->log->error("System max load $MAX_LOAD exceeded: $minute_avg");
 	return Apache2::Const::SERVER_ERROR;
