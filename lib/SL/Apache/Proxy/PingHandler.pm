25b54d7050c781aaccf9163f6d755e6b56785d08
Fred Moyer <fred@redhotpenguin.com>
2007-03-31 20:47:16 +0000

    This should be a 503 (Service Unavailable) instead of a 500 (Internal Server Error)
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@714 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- lib/SL/Apache/Proxy/PingHandler.pm
+++ lib/SL/Apache/Proxy/PingHandler.pm
@@ -3,7 +3,7 @@ package SL::Apache::Proxy::PingHandler;
 use strict;
 use warnings;
 
-use Apache2::Const -compile => qw( OK SERVER_ERROR );
+use Apache2::Const -compile => qw( OK HTTP_SERVICE_UNAVAILABLE );
 use Apache2::Log ();
 use Sys::Load ();
 use SL::Model ();
@@ -17,14 +17,14 @@ sub handler {
 	my $dbh = SL::Model->connect();
 	unless ($dbh) {
 		$r->log->error("Database has gone away :(");
-		return Apache2::Const::SERVER_ERROR;
+		return Apache2::Const::HTTP_SERVICE_UNAVAILABLE;
 	}
 
 	# now check the load
 	my $minute_avg = [Sys::Load::getload()]->[0];
     return Apache2::Const::OK if $minute_avg < $MAX_LOAD;
 	$r->log->error("System max load $MAX_LOAD exceeded: $minute_avg");
-	return Apache2::Const::SERVER_ERROR;
+	return Apache2::Const::HTTP_SERVICE_UNAVAILABLE;
 }
 
 1;
