b5142b9b184501cc2231cfda842b84f2c38bd60b
Fred Moyer <fred@redhotpenguin.com>
2007-05-12 18:42:05 +0000

    Fix to SL-Client Move model code to SL-Model
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@819 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL/sql/func/ad_md5.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION ad_md5() RETURNS trigger as $_$
-    BEGIN
-    NEW.md5 = md5(NEW.cts);
-    RETURN NEW;
-END;
-$_$
-language plpgsql;
-
--- SL/sql/table/ad.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE ad (
-    ad_id serial NOT NULL,
-	active boolean DEFAULT false,
-    md5 character varying(32),
-    cts timestamp without time zone default now ()
-);
-
-ALTER TABLE ONLY ad
-    ADD CONSTRAINT ad_pkey PRIMARY KEY (ad_id);
