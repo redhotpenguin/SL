a4e94e98fd768816ddb208410c18bfb4f3a6c9a8
Fred Moyer <fred@redhotpenguin.com>
2007-06-08 07:48:07 +0000

    Nope this one is for registration
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@883 bd012f14-6cfe-0310-9a23-91b8f1fede10

---
+++ SL/lib/SL/Apache/App/Register.pm
@@ -0,0 +1,45 @@
+package SL::Apache::App::Register;
+
+use strict;
+use warnings;
+
+use Apache2::Const -compile => qw( OK HTTP_NOT_MODFIED );
+use Apache2::RequestRec ();
+use Apache2::Connection ();
+use SL::Model::App;
+
+sub handler {
+    my $r = shift;
+
+    my $path = $r->uri();
+
+    # grab the mac address /register/ab:12:34
+    my ($mac_address) = $path =~ m/([^\/]+)$/;
+
+    # see if this router is already registered
+    my @registered_routers = SL::Model::App->resultset('Router')->search(
+        {
+            ip      => $r->connection->remote_ip,
+            macaddr => $mac_address,
+        }
+    );
+
+    if ( scalar(@registered_routers) > 0 ) {
+        return Apache2::Const::HTTP_NOT_MODFIED;
+    }
+    elsif ( scalar(@registered_routers) == 0 ) {
+
+        # register the router
+        my $router = SL::Model::App->resultset('Router')->new(
+            {
+                ip      => $r->connection->remote_ip,
+                macaddr => $mac_address,
+            }
+        );
+        $router->insert;
+        $router->update;
+        return Apache2::Const::OK;
+    }
+}
+
+1;
