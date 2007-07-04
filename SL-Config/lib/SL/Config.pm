05cee81559e1f7f4073d05f85e10c85bbc02740b
Fred Moyer <fred@redhotpenguin.com>
2007-07-04 18:10:03 +0000

    Move this into a separate repository
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@975 bd012f14-6cfe-0310-9a23-91b8f1fede10

---
+++ SL-Config/lib/SL/Config.pm
@@ -0,0 +1,137 @@
+package SL::Config;
+
+use strict;
+use warnings;
+
+=head1 NAME
+
+SL::Config
+
+=head1 ABSTRACT
+
+  use SL::Config;
+
+  $cfg  = SL::Config->new( [ '/path/to/config/file.conf' ] );
+  $val  = $cfg->key;
+  @vals = $cfg->key;
+
+=head1 DESCRIPTION
+
+This package is responsible for handling Silver Lining configuration data.
+It should make our lives less complex.
+We don't care what it does under the hood, as long as the API works.
+
+It tries to set Apache directives for as many variables as possible since they
+are really fast.
+
+=cut
+
+our ($config, $conf_dir);
+our $file = 'sl.conf';
+
+use base 'Config::ApacheFormat';
+use FindBin;
+
+sub new {
+    my $class = shift;
+
+    return $config if $config;
+    
+    my @config_files;
+    if (-d "$FindBin::Bin/../conf") {
+
+        # development
+        $conf_dir = "$FindBin::Bin/../conf";
+        @config_files = ("$conf_dir/$file", "$conf_dir/../$file");
+    }
+    elsif (-d "/etc/sl") {
+        $conf_dir = "/etc/sl";
+        @config_files = ("$conf_dir/$file");
+    }
+    else {
+        die "No file $file found in  " . 
+            "$FindBin::Bin/../conf/ or /etc/sl/!\n";
+    }
+
+    # we have a configuration file to work with, so get to work
+    $config = $class->SUPER::new();
+    my $read;
+    foreach my $config_file (@config_files) {
+$DB::single = 1;
+        next unless (-e $config_file);
+        $config->read($config_file);
+        $read++;
+    }
+    die "No config files read! conf_dir $conf_dir\n" unless $read;
+
+    $config->autoload_support(1);
+    return $config;
+}
+
+sub conf_dir {
+    my $self = shift;
+    return $conf_dir;
+}
+
+=item C<sl_db_params>
+
+Returns an array reference
+
+=cut
+
+sub sl_db_params {
+    my $self = shift;
+
+}
+
+sub tmpl_root {
+    my $self = shift;
+    return $self->sl_root . '/tmpl';
+}
+
+1;
+__END__
+
+# TODO - make directives work, possibly in a different module.
+
+use Apache2::Const -compile => qw( TAKE1 RSRC_CONF OR_ALL );
+use Apache2::Module   ();
+use Apache2::CmdParms ();
+use Apache2::Directive ();
+
+use Apache2::ServerUtil;
+use Apache2::ServerRec;
+
+my @dirs = (
+            {
+             name         => 'SLRoot',
+             errmsg       => 'the sl application root',
+             args_how     => Apache2::Const::TAKE1,
+             req_override => Apache2::Const::OR_ALL,
+            }
+           );
+
+Apache2::Module::add(__PACKAGE__, \@dirs);
+
+use Data::Dumper;
+sub SLRoot {
+    my ($cfg, $parms, $arg) = @_;
+    print STDERR "\nSLRoot cfg " . Dumper($cfg) . ", parms $parms, arg $arg";
+   $cfg->{_slroot} = $arg;
+    print STDERR "\n222SLRoot cfg " . Dumper($cfg) . ", parms $parms, arg $arg";
+}
+
+sub SERVER_CREATE {
+ my ($class, $parms) = @_;
+    print STDERR  "\nSERVER_CREATE class $class, parms $parms";
+    my $s = Apache2::ServerUtil->server;
+    #my $cfg = $s->module_config;
+    #print STDERR "\nMODULE CONFIG SERVER CREAETE IS " . Dumper($cfg);
+
+    my $cfg = Apache2::Module::get_config(__PACKAGE__, $s);
+    print STDERR "\nConfig is " . Dumper($cfg) . "\n";
+    print STDERR "\nKeep alive is " . $s->keep_alive() . "\n";
+    return bless {slroot => $cfg->{_slroot}, name => __PACKAGE__}, $class;
+}
+
+1;
