d8f8903c799305ba31e448384421e2e0370abf49
Fred Moyer <fred@redhotpenguin.com>
2007-07-10 18:28:57 +0000

    Allow the ping url to pass through
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1015 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL/lib/SL/Apache/Proxy/AccessHandler.pm
+++ SL/lib/SL/Apache/Proxy/AccessHandler.pm
@@ -3,7 +3,7 @@ package SL::Apache::Proxy::AccessHandler;
 use strict;
 use warnings;
 
-use Apache2::Const -compile => qw( OK FORBIDDEN );
+use Apache2::Const -compile => qw( OK FORBIDDEN DONE );
 use Apache2::RequestRec        ();
 use Apache2::Log               ();
 use Apache2::Connection        ();
@@ -12,8 +12,13 @@ use SL::Model::Proxy::Location ();
 sub handler {
     my $r = shift;
 
-    # see if we know this ip is registered
+    # allow /sl_secret_ping_button to pass through
+    my $url = $r->construct_url($r->unparsed_uri);
+    if ($url =~ m!/sl_secret_ping_button!) {
+        return Apache2::Const::OK;
+    }
 
+    # see if we know this ip is registered
     if (
         SL::Model::Proxy::Location->get_location_id_from_ip(
             $r->connection->remote_ip
