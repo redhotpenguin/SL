9c5c76f9290dfef9f4ccd02a6fab7fc5300d3052
Fred Moyer <fred@redhotpenguin.com>
2007-07-21 22:36:48 +0000

    Fix internal server error on second blacklist attempt.
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1090 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL/lib/SL/Apache/Proxy/BlacklistHandler.pm
+++ SL/lib/SL/Apache/Proxy/BlacklistHandler.pm
@@ -9,18 +9,67 @@ use SL::Model ();
 
 sub handler {
 	my $r = shift;
-	my $dbh = SL::Model->connect();
+
 	my $user_id = join("|", $r->connection->remote_ip, 
 		$r->pnotes('ua'), $r->construct_server());
 
+	my $dbh = eval { SL::Model->connect(); };
+	unless ($dbh) {
+		$r->log->error(sprintf("package %s db connect failed err: %s", 
+				__PACKAGE__, $@));
+		return _redirect($r, $user_id);
+	}
+
+	my $select_sth = $dbh->prepare("SELECT user_id, ts FROM user_blacklist WHERE user_id = ?");
+	$select_sth->bind_param(1, $user_id);
+	my $rv = eval { $select_sth->execute };
+	if (!$rv or $@) {
+		$r->log->error(
+			sprintf("user_blacklist select err, uid %s, err %s, url %s, referer %s", 
+				$user_id, $@, $r->pnotes('url'), $r->pnotes('referer')));
+		return _redirect($r, $user_id);
+	}
+
+	# no error, see if it has been blacklisted already
+	my $ary_ref = $select_sth->fetchrow_arrayref;
+	if (defined $ary_ref->[0]) {
+		# ok an entry exists, someone is trying to blacklist it again?
+		$r->log->error(
+			sprintf("re-blacklist attempt, user_id %s, url %s, ts %s, referer %s",
+			   $user_id, $r->pnotes('url'), $ary_ref->[1], $r->pnotes('referer')));
+		return _redirect($r, $user_id);
+	}
+
+	# not blacklisted yet, add it
 	my $sth = 
 		$dbh->prepare("INSERT INTO user_blacklist (user_id) values ( ? )");
 	$sth->bind_param(1,$user_id);
-	my $rv = $sth->execute;
-	unless ($rv) {
-		$r->log->error("Problem blacklisting $user_id ");
+	$rv = eval { $sth->execute };
+	if (!$rv or $@) {
+		# something is busted!
+		$r->log->error(
+			sprintf("blacklist attempt failed, uid %s, url %s, referer %s, err %s",
+				$user_id, $r->pnotes('url'), $r->pnotes('referer'), $@ ));
+		return _redirect($r, $user_id);
 	}
-	$r->headers_out->set(Location => $r->pnotes('referer'));
+
+	# blacklist happened ok
+	$r->log->debug(sprintf("%s blacklist successful for uid %s, referer %s",
+			$r->pnotes('url'), $user_id, $r->pnotes('referer')));
+	return _redirect($r, $user_id);
+}
+
+sub _redirect {
+	my ($r, $user_id) = @_;
+	if (($r->pnotes('referer') eq 'no_referer') or ( !$r->pnotes('referer'))) {
+		# if no referer redirect to the root url
+		$r->log->error(sprintf("no referer, uid %s sending to base domain %s",
+				$user_id, $r->construct_url));
+		$r->headers_out->set(Location => $r->construct_url);
+	} else {
+		# redirect back to the referring page
+		$r->headers_out->set(Location => $r->pnotes('referer'));
+	}
 	return Apache2::Const::REDIRECT;
 }
 
