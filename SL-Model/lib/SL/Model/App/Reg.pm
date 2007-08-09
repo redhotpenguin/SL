eac69acef704fce414675eb164f96a9aef8dabf9
Fred Moyer <fred@redhotpenguin.com>
2007-08-09 00:06:42 +0000

    Fix some bugs introduced by the static classes
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1164 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Model/lib/SL/Model/App/Reg.pm
+++ SL-Model/lib/SL/Model/App/Reg.pm
@@ -3,7 +3,231 @@ package SL::Model::App::Reg;
 use strict;
 use warnings;
 
-use base 'SL::Model::App';
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components("Core");
+__PACKAGE__->table("reg");
+__PACKAGE__->add_columns(
+  "reg_id",
+  {
+    data_type => "integer",
+    default_value => "nextval('reg_reg_id_seq'::regclass)",
+    is_nullable => 0,
+    size => 4,
+  },
+  "email",
+  {
+    data_type => "character varying",
+    default_value => "''::character varying",
+    is_nullable => 0,
+    size => 64,
+  },
+  "zipcode",
+  {
+    data_type => "character varying",
+    default_value => "''::character varying",
+    is_nullable => 1,
+    size => 10,
+  },
+  "firstname",
+  {
+    data_type => "character varying",
+    default_value => undef,
+    is_nullable => 1,
+    size => 32,
+  },
+  "lastname",
+  {
+    data_type => "character varying",
+    default_value => undef,
+    is_nullable => 1,
+    size => 32,
+  },
+  "description",
+  {
+    data_type => "text",
+    default_value => undef,
+    is_nullable => 1,
+    size => undef,
+  },
+  "street_addr",
+  {
+    data_type => "character varying",
+    default_value => undef,
+    is_nullable => 1,
+    size => 64,
+  },
+  "apt_suite",
+  {
+    data_type => "character varying",
+    default_value => undef,
+    is_nullable => 1,
+    size => 5,
+  },
+  "referer",
+  {
+    data_type => "character varying",
+    default_value => undef,
+    is_nullable => 1,
+    size => 32,
+  },
+  "phone",
+  {
+    data_type => "character varying",
+    default_value => undef,
+    is_nullable => 1,
+    size => 14,
+  },
+  "cts",
+  {
+    data_type => "timestamp without time zone",
+    default_value => "now()",
+    is_nullable => 1,
+    size => 8,
+  },
+  "mts",
+  {
+    data_type => "timestamp without time zone",
+    default_value => "now()",
+    is_nullable => 1,
+    size => 8,
+  },
+  "sponsor",
+  {
+    data_type => "character varying",
+    default_value => undef,
+    is_nullable => 1,
+    size => 64,
+  },
+  "street_addr2",
+  {
+    data_type => "character varying",
+    default_value => undef,
+    is_nullable => 1,
+    size => 64,
+  },
+  "city",
+  {
+    data_type => "character varying",
+    default_value => undef,
+    is_nullable => 1,
+    size => 64,
+  },
+  "state",
+  {
+    data_type => "character",
+    default_value => undef,
+    is_nullable => 1,
+    size => 2,
+  },
+  "active",
+  {
+    data_type => "boolean",
+    default_value => "true",
+    is_nullable => 1,
+    size => 1,
+  },
+  "report_email",
+  {
+    data_type => "character varying",
+    default_value => "''::character varying",
+    is_nullable => 1,
+    size => 64,
+  },
+  "password_md5",
+  {
+    data_type => "character varying",
+    default_value => undef,
+    is_nullable => 1,
+    size => 32,
+  },
+  "send_reports_daily",
+  {
+    data_type => "boolean",
+    default_value => "false",
+    is_nullable => 1,
+    size => 1,
+  },
+  "send_reports_weekly",
+  {
+    data_type => "boolean",
+    default_value => "false",
+    is_nullable => 1,
+    size => 1,
+  },
+  "send_reports_monthly",
+  {
+    data_type => "boolean",
+    default_value => "false",
+    is_nullable => 1,
+    size => 1,
+  },
+  "send_reports_quarterly",
+  {
+    data_type => "boolean",
+    default_value => "false",
+    is_nullable => 1,
+    size => 1,
+  },
+  "report_email_frequency",
+  {
+    data_type => "character varying",
+    default_value => "''::character varying",
+    is_nullable => 0,
+    size => 16,
+  },
+);
+__PACKAGE__->set_primary_key("reg_id");
+__PACKAGE__->has_many(
+  "ad_groups",
+  "SL::Model::App::AdGroup",
+  { "foreign.reg_id" => "self.reg_id" },
+);
+__PACKAGE__->has_many(
+  "ad_sls",
+  "SL::Model::App::AdSl",
+  { "foreign.reg_id" => "self.reg_id" },
+);
+__PACKAGE__->has_many(
+  "bugs",
+  "SL::Model::App::Bug",
+  { "foreign.reg_id" => "self.reg_id" },
+);
+__PACKAGE__->has_many(
+  "forgots",
+  "SL::Model::App::Forgot",
+  { "foreign.reg_id" => "self.reg_id" },
+);
+__PACKAGE__->has_many(
+  "reg__ad_groups",
+  "SL::Model::App::RegAdGroup",
+  { "foreign.reg_id" => "self.reg_id" },
+);
+__PACKAGE__->has_many(
+  "reg__reg_sec_reg_ids",
+  "SL::Model::App::RegReg",
+  { "foreign.sec_reg_id" => "self.reg_id" },
+);
+__PACKAGE__->has_many(
+  "reg__reg_first_reg_ids",
+  "SL::Model::App::RegReg",
+  { "foreign.first_reg_id" => "self.reg_id" },
+);
+__PACKAGE__->has_many(
+  "roots",
+  "SL::Model::App::Root",
+  { "foreign.reg_id" => "self.reg_id" },
+);
+__PACKAGE__->has_many(
+  "router__regs",
+  "SL::Model::App::RouterReg",
+  { "foreign.reg_id" => "self.reg_id" },
+);
+__PACKAGE__->has_many(
+  "urls",
+  "SL::Model::App::Url",
+  { "foreign.reg_id" => "self.reg_id" },
+);
 
 sub friends {
     my $self = shift;
@@ -84,7 +308,7 @@ sub process_ad_group {
       ->search( { ad_group_id => $ad_group->ad_group_id } )->count;
 
     # get routers for this reg
-    my @router_ids = map { $_->router_id } $self->routers;
+    my @router_ids = map { $_->router_id->router_id } $self->router__regs;
     if ( scalar(@router_ids) == 0 ) {
         $ad_group->{router_count} = 0;
         next;
@@ -96,7 +320,7 @@ sub process_ad_group {
             ad_group_id => $ad_group->ad_group_id,
             router_id   => { -in => \@router_ids }
         }
-      );
+      )->count;
 
     # Hack
     if ( $ad_group->template eq 'text_ad.tmpl' ) {
@@ -108,12 +332,32 @@ sub process_ad_group {
     return 1;
 }
 
-sub routers {
-    my ( $self, $args_ref ) = @_;
+sub process_router {
+  my ($self, $router) = @_;
+
+  $router->{location_count} = $router->router__locations->count;
+  $router->{ad_group_count} = $router->router__ad_groups->count;
+
+  return 1;
+}
+
+sub get_routers {
+    my ( $self, $ad_group_id ) = @_;
 
     my @routers = map { $_->router_id } $self->router__regs;
+    return unless (scalar(@routers) > 0);
+    $self->process_router($_) for @routers;
+    return @routers unless $ad_group_id;
 
-    return unless scalar(@routers) > 0;
+    # filter the routers which have this ad group
+    my @router_ids = map { $_->router_id } @routers;
+    my @filtered_routers = map { $_->router_id }
+        SL::Model::App->resultset('RouterAdGroup')->search(
+                            { ad_group_id => $ad_group_id,
+                              router_id => { -in => \@router_ids } });
+
+    return unless (scalar(@filtered_routers) > 0);
+    $self->process_router($_) for @routers;
     return @routers;
 }
 
