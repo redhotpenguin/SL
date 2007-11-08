a8c172396624ed4653e0283ea985c8fa6abe3105
Fred Moyer <fred@redhotpenguin.com>
2007-11-08 22:27:17 +0000

    splash page
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1324 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Model/lib/SL/Model/Proxy/Router.pm
+++ SL-Model/lib/SL/Model/Proxy/Router.pm
@@ -45,6 +45,29 @@ sub add_router_from_mac {
     return $router_id;
 }
 
+
+use constant SPLASH_PAGE_SQL => q{
+SELECT router.splash_href, router.splash_timeout
+FROM router, location, router__location
+WHERE location.location_id = ?
+AND router__location.location_id = location.location_id
+AND router.router_id = router__location.router_id
+};
+
+
+sub splash_page {
+  my ($class, $location_id) = @_;
+  my $sth = $class->connect->prepare_cached(SPLASH_PAGE_SQL);
+  $sth->bind_param(1, $location_id);
+  $sth->execute or return;
+  my $ary_ref = $sth->fetchrow_arrayref;
+  $sth->finish;
+  return unless (scalar(@{$ary_ref}) > 0);
+  return unless ((defined $ary_ref->[0]) && ($ary_ref->[0] ne ''));
+  return ($ary_ref->[0], $ary_ref->[1]);
+}
+
+
 use constant REPLACE_PORT_SQL => q{
 SELECT router.router_id, router.replace_port
 FROM router, location, router__location
---
+++ SL-Model/t/SL-Model-Proxy-Router.t
@@ -0,0 +1,22 @@
+use strict;
+use warnings FATAL => 'all';
+
+use Test::More tests => 2;
+
+my $pkg;
+
+BEGIN {
+    $pkg = 'SL::Model::Proxy::Router';
+    use_ok($pkg);
+}
+
+can_ok( $pkg, qw( replace_port add_router_from_mac splash_page ) );
+
+use SL::Model;
+
+my $dbh = SL::Model->connect or die 'no db connection!';
+my $location_id = $dbh->selectcol_arrayref("SELECT location_id from location limit 1")->[0];
+
+my $splash_href = SL::Model::Proxy::Router->splash_page($location_id);
+
+1;
