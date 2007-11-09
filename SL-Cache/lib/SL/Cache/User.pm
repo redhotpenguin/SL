fc5fc6fb294340a63d3a982b02bb8f62566df0ad
Fred Moyer <fred@redhotpenguin.com>
2007-11-09 00:03:47 +0000

    track users individually
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1328 bd012f14-6cfe-0310-9a23-91b8f1fede10

---
+++ SL-Cache/lib/SL/Cache/User.pm
@@ -0,0 +1,36 @@
+package SL::Cache::User;
+
+use strict;
+use warnings;
+
+use SL::Cache ();
+use base 'SL::Cache';
+
+our $CONFIG     = SL::Config->new();
+
+sub new {
+    my $class = shift;
+
+    my $self = $class->SUPER::new( type => 'raw');
+    return $self;
+}
+
+sub set_last_seen {
+    my ( $self, $user_id ) = @_;
+
+    # update the cache
+    $self->{cache}->set( join('|', 'user', $user_id) => time() );
+
+    return 1;
+}
+
+
+sub get_last_seen {
+    my ( $self, $user_id ) = @_;
+
+    my $last_seen = $self->{cache}->get(join('|', 'user', $user_id));
+    return unless $last_known;
+    return $last_seen;
+}
+
+1;
