cc593776b3560c3ac1fb436066b9e5495cb97d52
Fred Moyer <fred@redhotpenguin.com>
2007-11-23 00:40:37 +0000

    production and event fields
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1355 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Model/sql/table/router.sql
+++ SL-Model/sql/table/router.sql
@@ -3,8 +3,10 @@
 --
 
 SET client_encoding = 'SQL_ASCII';
+SET standard_conforming_strings = off;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
+SET escape_string_warning = off;
 
 SET search_path = public, pg_catalog;
 
@@ -13,26 +15,53 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: reg; Type: TABLE; Schema: public; Owner: fred; Tablespace: 
+-- Name: router; Type: TABLE; Schema: public; Owner: phred; Tablespace: 
 --
 
 CREATE TABLE router (
-    router_id SERIAL NOT NULL,
-    serial_number character(12) DEFAULT '' NOT NULL,
+    router_id integer DEFAULT nextval('router_router_id_seq'::regclass) NOT NULL,
+    serial_number character(12),
     macaddr macaddr,
     cts timestamp without time zone DEFAULT now(),
     mts timestamp without time zone DEFAULT now(),
-    active boolean default 't',
+    active boolean DEFAULT true,
     proxy inet,
-    replace_port smallint default 8135,
-    description text default '',
-    name text default '',
-    feed_linkshare boolean default 'f',
-    feed_google boolean default 'f',
-    splash_href text default '',
-    splash_timeout integer default 60
+    replace_port smallint DEFAULT 8135,
+    description text,
+    name text,
+    feed_google boolean DEFAULT false,
+    feed_linkshare boolean DEFAULT false,
+    splash_timeout integer DEFAULT 60,
+    splash_href text DEFAULT ''::text,
+    firmware_version character varying(4) DEFAULT ''::character varying,
+    ssid text DEFAULT ''::text,
+    firmware_event text DEFAULT ''::text,
+    ssid_event text DEFAULT ''::text,
+    passwd_event text DEFAULT ''::text
 );
+
+
+ALTER TABLE public.router OWNER TO phred;
+
+--
+-- Name: router_pkey; Type: CONSTRAINT; Schema: public; Owner: phred; Tablespace: 
+--
 
 ALTER TABLE ONLY router
     ADD CONSTRAINT router_pkey PRIMARY KEY (router_id);
+
+
+--
+-- Name: update_router_mts; Type: TRIGGER; Schema: public; Owner: phred
+--
+
+CREATE TRIGGER update_router_mts
+    BEFORE UPDATE ON router
+    FOR EACH ROW
+    EXECUTE PROCEDURE update_modified_column();
+
+
+--
+-- PostgreSQL database dump complete
+--
 
