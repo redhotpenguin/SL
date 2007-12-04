c96f9450d12be7fc04a2721c86147b850f57b1ae
Fred Moyer <fred@redhotpenguin.com>
2007-12-04 05:02:18 +0000

    default values for client
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1411 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Client/lib/SL/Client/HTTP.pm
+++ SL-Client/lib/SL/Client/HTTP.pm
@@ -57,7 +57,6 @@ if it encounters network communication problems.
 
 =cut
 
-
 use Params::Validate qw(validate HASHREF);
 use URI;
 use Net::HTTP;
@@ -65,69 +64,87 @@ use HTTP::Response;
 use HTTP::Headers;
 use Carp qw(croak);
 
+my %default_headers = (
+    'Accept-Encoding' => 'gzip,deflate',
+    'Accept-Charset'  => 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+    'Keep-Alive'      => 300,
+    'Connection'      => 'keep-alive',
+    'Pragma'          => 'no-cache',
+    'Cache-Control'   => 'no-cache',
+    'Accept-Lang'     => 'en-us,en;q=0.5',
+    'Accept'          =>
+'text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
+    'User-Agent' =>
+'Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; en-US; rv:1.8.0.2) Gecko/20060308 Firefox/1.5.0.2',
+);
+
 sub get {
     my $pkg  = shift;
-    my %args = validate(@_, { url  => 1,
-                              host => 1,
-                              port => 1,
-                              headers => 0,
-                            });
+    my %args = validate(
+        @_,
+        {
+            url     => 1,
+            host    => 1,
+            port    => 1,
+            headers => 0,
+        }
+    );
 
-    my $url = URI->new($args{url})
+    my $url = URI->new( $args{url} )
       or croak("Unable to parse url '$args{url}'.");
-    my $host = $args{host};
-    my $port = $args{port};
-    my $headers = $args{headers} || [];
+    my $host    = $args{host};
+    my $port    = $args{port};
+    my $headers = $args{headers} || \%default_headers;
 
     # convert headers to array-ref if a hash-ref is passed
-    $headers = [ %$headers ] if (ref $headers eq 'HASH');
-        
+    $headers = [%$headers] if ( ref $headers eq 'HASH' );
+
     # setup some default headers
     my %seen_keys;
-    for my $i (0 .. $#$headers) {
-        $seen_keys{$headers->[$i]}++ if (($i % 2) == 0);
+    for my $i ( 0 .. $#$headers ) {
+        $seen_keys{ $headers->[$i] }++ if ( ( $i % 2 ) == 0 );
     }
-    push @$headers, ('User-Agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.2) Gecko/20060308 Firefox/1.5.0.2')
-      unless $seen_keys{'User-Agent'};
 
-    my $http = Net::HTTP->new(Host => $url->host,
-                              PeerAddr => $host,
-                              PeerPort => $port) || die $@;
+    my $http = Net::HTTP->new(
+        Host     => $url->host,
+        PeerAddr => $host,
+        PeerPort => $port
+      )
+      || die $@;
 
     # reinforce the point (Net::HTTP adds PeerPort to host during
     # new())
-    $http->host($url->host);
+    $http->host( $url->host );
 
     # make the request
     my $req = $url->path_query || "/";
-    $http->write_request(GET => $req, @$headers);
+    $http->write_request( GET => $req, @$headers );
 
     # get the resulr code, message and response headers
-    my ($code, $mess, @headers_out) = $http->read_response_headers;
+    my ( $code, $mess, @headers_out ) = $http->read_response_headers;
 
     # read response body
     my $body = "";
     while (1) {
         my $buf;
-        my $n = $http->read_entity_body($buf, 10240);
+        my $n = $http->read_entity_body( $buf, 10240 );
         die "read failed: $!" unless defined $n;
-        last unless $n;
+        last                  unless $n;
         $body .= $buf;
     }
 
-    my $response = _build_response($code, $mess, \@headers_out, \$body);
+    my $response = _build_response( $code, $mess, \@headers_out, \$body );
     return $response;
 }
 
 # turns data returned by Net::HTTP into a HTTP::Response object
 sub _build_response {
-    my ($code, $mess, $header_list, $body_ref) = @_;
+    my ( $code, $mess, $header_list, $body_ref ) = @_;
 
     my $header = HTTP::Headers->new(@$header_list);
 
-    my $response = HTTP::Response->new($code, $mess, $header, $$body_ref);
+    my $response = HTTP::Response->new( $code, $mess, $header, $$body_ref );
     return $response;
 }
-
 
 1;
