5495ff8e6ff2da2b4f06665e35030f350245e26f
Fred Moyer <fred@redhotpenguin.com>
2007-12-08 00:42:54 +0000

    exec instead of system call now.  ssid, password, reboot, and halt are working, still need to test firmware update
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1445 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Linksys/imagebuilder/files/usr/bin/change_ssid.sh
+++ SL-Linksys/imagebuilder/files/usr/bin/change_ssid.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+# echo "setting wl0_ssid\n"
 /usr/sbin/nvram set wl0_ssid="$1"
 
+# echo "committing nvram\n"
 /usr/sbin/nvram commit
 
-/sbin/wifi
+# echo "calling wifi\n"
+/sbin/wifi &
 
+# echo "exiting\n"
+exit 0
