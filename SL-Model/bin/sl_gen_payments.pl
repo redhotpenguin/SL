aacfc18f5ef1b52bcfc3edca503e05f54cee591a
Fred Moyer <fred@redhotpenguin.com>
2008-01-05 21:48:22 +0000

    initial payment generator
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1546 bd012f14-6cfe-0310-9a23-91b8f1fede10

---
+++ SL-Model/bin/sl_gen_payments.pl
@@ -0,0 +1,10 @@
+#!perl
+
+use strict;
+use warnings;
+
+# cron based payment generator
+# look for previous payments with this account
+# compute the number of ad views seen since then
+# create a new payment
+
