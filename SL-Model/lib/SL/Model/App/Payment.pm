8cc8d5ecaca110869a12f9135c316f3e6687f8b1
Fred Moyer <fred@redhotpenguin.com>
2008-01-05 21:42:27 +0000

    handle broken payment scenario
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1544 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Model/lib/SL/Model/App/Payment.pm
+++ SL-Model/lib/SL/Model/App/Payment.pm
@@ -8,83 +8,103 @@ use base 'DBIx::Class';
 __PACKAGE__->load_components("Core");
 __PACKAGE__->table("payment");
 __PACKAGE__->add_columns(
-  "payment_id",
-  {
-    data_type => "integer",
-    default_value => "nextval('payment_payment_id_seq'::regclass)",
-    is_nullable => 0,
-    size => 4,
-  },
-  "reg_id",
-  { data_type => "integer", default_value => undef, is_nullable => 0, size => 4 },
-  "cts",
-  {
-    data_type => "timestamp without time zone",
-    default_value => "now()",
-    is_nullable => 1,
-    size => 8,
-  },
-  "approved_ts",
-  {
-    data_type => "timestamp without time zone",
-    default_value => undef,
-    is_nullable => 1,
-    size => 8,
-  },
-  "approved",
-  {
-    data_type => "boolean",
-    default_value => "false",
-    is_nullable => 1,
-    size => 1,
-  },
-  "approved_reg_id",
-  { data_type => "integer", default_value => 1, is_nullable => 0, size => 4 },
-  "num_views",
-  { data_type => "integer", default_value => undef, is_nullable => 0, size => 4 },
-  "cpm",
-  { data_type => "money", default_value => undef, is_nullable => 0, size => 4 },
-  "amount",
-  { data_type => "money", default_value => undef, is_nullable => 0, size => 4 },
-  "pp_timestamp",
-  {
-    data_type => "timestamp with time zone",
-    default_value => undef,
-    is_nullable => 1,
-    size => 8,
-  },
-  "pp_correlation_id",
-  {
-    data_type => "text",
-    default_value => "''::text",
-    is_nullable => 1,
-    size => undef,
-  },
-  "pp_version",
-  {
-    data_type => "text",
-    default_value => "''::text",
-    is_nullable => 1,
-    size => undef,
-  },
-  "pp_build",
-  {
-    data_type => "text",
-    default_value => "''::text",
-    is_nullable => 1,
-    size => undef,
-  },
-  "paid",
-  {
-    data_type => "boolean",
-    default_value => "false",
-    is_nullable => 1,
-    size => 1,
-  },
+    "payment_id",
+    {
+        data_type     => "integer",
+        default_value => "nextval('payment_payment_id_seq'::regclass)",
+        is_nullable   => 0,
+        size          => 4,
+    },
+    "reg_id",
+    {
+        data_type     => "integer",
+        default_value => undef,
+        is_nullable   => 0,
+        size          => 4
+    },
+    "cts",
+    {
+        data_type     => "timestamp without time zone",
+        default_value => "now()",
+        is_nullable   => 1,
+        size          => 8,
+    },
+    "approved_ts",
+    {
+        data_type     => "timestamp without time zone",
+        default_value => undef,
+        is_nullable   => 1,
+        size          => 8,
+    },
+    "approved",
+    {
+        data_type     => "boolean",
+        default_value => "false",
+        is_nullable   => 1,
+        size          => 1,
+    },
+    "approved_reg_id",
+    { data_type => "integer", default_value => 1, is_nullable => 0, size => 4 },
+    "num_views",
+    {
+        data_type     => "integer",
+        default_value => undef,
+        is_nullable   => 0,
+        size          => 4
+    },
+    "cpm",
+    {
+        data_type     => "money",
+        default_value => undef,
+        is_nullable   => 0,
+        size          => 4
+    },
+    "amount",
+    {
+        data_type     => "money",
+        default_value => undef,
+        is_nullable   => 0,
+        size          => 4
+    },
+    "pp_timestamp",
+    {
+        data_type     => "timestamp with time zone",
+        default_value => undef,
+        is_nullable   => 1,
+        size          => 8,
+    },
+    "pp_correlation_id",
+    {
+        data_type     => "text",
+        default_value => "''::text",
+        is_nullable   => 1,
+        size          => undef,
+    },
+    "pp_version",
+    {
+        data_type     => "text",
+        default_value => "''::text",
+        is_nullable   => 1,
+        size          => undef,
+    },
+    "pp_build",
+    {
+        data_type     => "text",
+        default_value => "''::text",
+        is_nullable   => 1,
+        size          => undef,
+    },
+    "paid",
+    {
+        data_type     => "boolean",
+        default_value => "false",
+        is_nullable   => 1,
+        size          => 1,
+    },
 );
 __PACKAGE__->set_primary_key("payment_id");
-__PACKAGE__->belongs_to("reg_id", "SL::Model::App::Reg", { reg_id => "reg_id" });
-
+__PACKAGE__->belongs_to( "reg_id", "SL::Model::App::Reg",
+    { reg_id => "reg_id" } );
 
 # Created by DBIx::Class::Schema::Loader v0.04002 @ 2008-01-05 11:51:52
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:9Y+2z08mE3nnwZ5LI97I0Q
@@ -150,11 +170,12 @@ sub pay_payments {
     my $self = shift;
 
     # grab approved payments that haven't been paid
-    my @sl_payments =
-      SL::Model::App->resultset('Payment')
-      ->search( { approved => 1,
-                  paid => 0,
-    } );
+    my @sl_payments = SL::Model::App->resultset('Payment')->search(
+        {
+            approved => 1,
+            paid     => 0,
+        }
+    );
 
     die "no payments to make!\n" unless @sl_payments;
 
@@ -223,23 +244,37 @@ NOTE
 }
 
 sub approve {
-  my ($self, $approver_reg) = @_;
+    my ( $self, $approver_reg ) = @_;
 
-  if ($self->paid == 1) {
-    die sprintf("payment id %d already paid!\n", $self->payment_id);
-  }
+    if ( $self->paid == 1 ) {
+        die sprintf( "payment id %d already paid!\n", $self->payment_id );
+    }
 
-  if (($self->approved == 1) and
-      ($self->approved_reg_id != 1) and
-      (defined $self->approved_ts)) {
-    die sprintf("payment id %d already approved!\n", $self->payment_id);
-  }
+    if (    ( $self->approved == 1 )
+        and ( $self->approved_reg_id != 1 )
+        and ( defined $self->approved_ts ) )
+    {
+        die sprintf( "payment id %d already approved!\n", $self->payment_id );
+    }
 
-  # ok approve the payment
-  $self->approved_reg_id($approver_reg->reg_id);
-  $self->approved_ts(DateTime::Format::Pg->format_datetime(DateTime->now));
-  $self->approved(1);
-  return $self->update;
+    if (
+        ( $self->approved == 1 )
+        and (
+            ( $self->approved_reg_id == 1 ) or    #broken
+            ( !defined $self->approved_ts )
+        )
+      )
+    {
+        die sprintf( "payment id %d is BROKEN, please investigate!\n",
+            $self->payment_id );
+    }
+
+    # ok approve the payment
+    $self->approved_reg_id( $approver_reg->reg_id );
+    $self->approved_ts(
+        DateTime::Format::Pg->format_datetime( DateTime->now ) );
+    $self->approved(1);
+    return $self->update;
 }
 
 sub send_receipt {
