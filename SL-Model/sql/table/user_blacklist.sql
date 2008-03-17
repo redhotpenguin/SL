6a54c4d42af170b826ac92acd0940dc08e16429d
Fred Moyer <fred@redhotpenguin.com>
2008-03-17 02:31:58 +0000

    Add expired column to ratelimit for kharma setup.  This allows the close button actions to expire after a certain amount of time for setups like the mesh network where we can't determine exactly what user caused the action.
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1627 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Model/sql/table/user_blacklist.sql
+++ SL-Model/sql/table/user_blacklist.sql
@@ -2,5 +2,6 @@
 CREATE TABLE user_blacklist (
     -- user_id must be long enough for 1 ip, a domain, and a long user-agent
     user_id varchar(256) PRIMARY KEY,
+    expired boolean NOT NULL default 'f',
     ts timestamp without time zone DEFAULT now()
 );
