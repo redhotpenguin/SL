8cd38e5cbf3f17695b70f6e56c632f480c362a2f
Fred Moyer <fred@redhotpenguin.com>
2008-05-12 03:50:54 +0000

    cleanup
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1699 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL/lib/SL/Apache/Proxy/PostReadRequestHandler.pm
+++ SL/lib/SL/Apache/Proxy/PostReadRequestHandler.pm
@@ -5,44 +5,45 @@ use warnings;
 
 use Apache2::RequestRec ();
 use Apache2::Const -compile => qw( OK DONE );
-use Apache2::Log ();
-use APR::Table ();
+use Apache2::Log         ();
+use APR::Table           ();
 use Apache2::RequestUtil ();
 
-use constant DEBUG  => $ENV{SL_DEBUG} || 0;
-use constant TIMING => $ENV{SL_TIMING} || 0;
+use constant DEBUG      => $ENV{SL_DEBUG}      || 0;
+use constant TIMING     => $ENV{SL_TIMING}     || 0;
 use constant REQ_TIMING => $ENV{SL_REQ_TIMING} || 0;
 
 my $TIMER;
-if (TIMING or REQ_TIMING) {
-  require RHP::Timer;
-  $TIMER = RHP::Timer->new();
+if ( TIMING or REQ_TIMING ) {
+    require RHP::Timer;
+    $TIMER = RHP::Timer->new();
 }
 
 sub handler {
-	my $r = shift;
- 
-	my $ua      = $r->headers_in->{'user-agent'};
-	unless ($ua) {
-		$ua = 'none';
-	}
-	$r->pnotes('ua'      => $ua);
+    my $r = shift;
 
-    if ( length($ua) > 25) {
-        my $potential_dummy = substr($ua, (length($ua)-27), length($ua));
+    my $ua = $r->headers_in->{'user-agent'};
+    unless ($ua) {
+        $r->log->error("$$ no user agent, request " . $r->as_string);
+        $ua = 'none';
+    }
+    $r->pnotes( 'ua' => $ua );
 
-	    if ($potential_dummy eq  '(internal dummy connection)') {
+    if ( length($ua) > 25 ) {
+        my $potential_dummy = substr( $ua, ( length($ua) - 27 ), length($ua) );
+
+        if ( $potential_dummy eq '(internal dummy connection)' ) {
             $r->log->debug("$$ dummy connection") if DEBUG;
 
-		    $r->subprocess_env(SL_URL => 'sl_dummy');
-            $r->set_handlers( PerlResponseHandler => [] );
-		    return Apache2::Const::DONE;
-	    }
+            $r->subprocess_env( SL_URL => 'sl_dummy' );
+            $r->set_handlers( PerlResponseHandler => undef );
+            return Apache2::Const::DONE;
+        }
     }
 
-    $TIMER->start('global_request_timer') if (TIMING or REQ_TIMING);
-	$r->pnotes('global_request_timer' => $TIMER) if (TIMING or REQ_TIMING);
-	return Apache2::Const::DECLINED;
+    $TIMER->start('global_request_timer') if ( TIMING or REQ_TIMING );
+    $r->pnotes( 'global_request_timer' => $TIMER ) if ( TIMING or REQ_TIMING );
+    return Apache2::Const::DECLINED;
 }
 
 1;
