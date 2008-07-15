7f4623c21524fd30bda3585a923e1c7928814b00
Fred Moyer <fred@redhotpenguin.com>
2008-07-15 17:41:49 +0000

    fix up
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1771 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Model/bin/sl_mail_user_reports.pl
+++ SL-Model/bin/sl_mail_user_reports.pl
@@ -7,7 +7,7 @@ use warnings;
 
 =head1 SYNOPSIS
 
- perl sl_mail_user_reports.pl --to=specific_user_email --interval=daily --interval=weekly --interval=monthly --interval=quarterly
+ perl sl_mail_user_reports.pl --to=specific_user_email --interval=daily --interval=weekly --interval=monthly --interval=quarterly --interval biannually --interval annually
 
  perl sl_mail_user_reports.pl --help
 
@@ -18,20 +18,20 @@ use warnings;
 use Getopt::Long;
 use Pod::Usage;
 
-my (@intervals, $to);
-my ($help, $man);
+my ( @intervals, $to );
+my ( $help,      $man );
 
 pod2usage(1) unless @ARGV;
 GetOptions(
-    'to=s'    => \$to,
+    'to=s'       => \$to,
     'interval=s' => \@intervals,
-    'help' => \$help,
-    'man' => \$man,
+    'help'       => \$help,
+    'man'        => \$man,
 ) or pod2usage(2);
 
-pod2usage(1) unless ($intervals[0]);
+pod2usage(1) unless ( $intervals[0] );
 pod2usage(1) if $help;
-pod2usage( -verbose => 2) if $man;
+pod2usage( -verbose => 2 ) if $man;
 
 use MIME::Lite;
 use SL::Model::App;
@@ -41,26 +41,27 @@ my $SUBJECT = "SilverLining Report Graphs";
 my @users;
 
 if ($to) {
-    @users = SL::Model::App->resultset('Reg')->search({ email => $to });
+    @users = SL::Model::App->resultset('Reg')->search( { email => $to } );
     unless (@users) {
         print "Sorry we couldn't find $to in our database\n";
         exit(1);
     }
-} else {
-	my %where;
-    @users = SL::Model::App->resultset('Reg')->search( { active => 't'} );
+}
+else {
+    my %where;
+    @users = SL::Model::App->resultset('Reg')->search( { active => 't' } );
 }
 
 foreach my $user (@users) {
-	my $dir     = $user->account_id->report_dir_base;
-    opendir(DIR, $dir) || die "could not open $dir: $!";
-    my $has_pngs = () = grep { $_ =~ m/\.png$/  } readdir(DIR);
+    my $dir = $user->account_id->report_dir_base;
+    opendir( DIR, $dir ) || die "could not open $dir: $!";
+    my $has_pngs = () = grep { $_ =~ m/\.png$/ } readdir(DIR);
     closedir(DIR);
     next unless $has_pngs;
 
-	my $email = $user->email;
+    my $email = $user->email;
 
-	my $data = <<DATA;
+    my $data = <<DATA;
 Hi $email,
 
 Attached are the reporting graphs for your Silver Lining routers.
@@ -70,32 +71,31 @@ To edit your report delivery settings visit the Silver Lining dashboard:
 https://app.silverliningnetworks.com/sl/app/home/index
 DATA
 
-	my $msg = MIME::Lite->new(
+    my $msg = MIME::Lite->new(
         From    => $FROM,
         To      => $user->email,
         Subject => $SUBJECT,
         Type    => 'TEXT',
         Data    => $data,
     );
-   
-	my $attached_something = 0;	
-    foreach my $temporal ( @intervals ) {
 
-        foreach my $type qw( views ) { # clicks rates ads ) {
-          my $filename = "$type\_$temporal.png";
-		  next unless -e "$dir/$filename";
-		  
-		  $msg->attach(
+    my $attached_something = 0;
+    foreach my $temporal (@intervals) {
+
+        foreach my $type qw( views ) {    # clicks rates ads ) {
+            my $filename = "$type\_$temporal.png";
+            next unless -e "$dir/$filename";
+
+            $msg->attach(
                 Type     => 'image/png',
                 Path     => "$dir/$filename",
                 Filename => $filename,
             );
-			$attached_something = 1;
+            $attached_something = 1;
         }
     }
-	
-	next unless $attached_something; # no reports
+
+    next unless $attached_something;    # no reports
     $msg->send;
 }
-
 
