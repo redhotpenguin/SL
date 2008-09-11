3f32989d75cde3e63d30193f57db2a592fdb5db3
Fred Moyer <fred@redhotpenguin.com>
2008-09-11 00:11:14 +0000

    auth time for splash page enforcement
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1902 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-User/lib/SL/User.pm
+++ SL-User/lib/SL/User.pm
@@ -8,12 +8,12 @@ our $VERSION = 0.01;
 use SL::Cache ();
 use base 'SL::Cache';
 
-our $CONFIG     = SL::Config->new();
+our $CONFIG = SL::Config->new();
 
 sub new {
     my $class = shift;
 
-    my $self = $class->SUPER::new( type => 'raw');
+    my $self = $class->SUPER::new( type => 'raw' );
     return $self;
 }
 
@@ -21,18 +21,36 @@ sub set_last_seen {
     my ( $self, $user_id ) = @_;
 
     # update the cache
-    $self->{cache}->set( join('|', 'user', $user_id) => time() );
+    $self->{cache}->set( join( '|', 'user', 'last_seen', $user_id ) => time() );
 
     return 1;
 }
 
-
 sub get_last_seen {
     my ( $self, $user_id ) = @_;
 
-    my $last_seen = $self->{cache}->get(join('|', 'user', $user_id));
+    my $last_seen =
+      $self->{cache}->get( join( '|', 'user', 'last_seen', $user_id ) );
     return unless $last_seen;
     return $last_seen;
+}
+
+sub set_last_auth {
+    my ( $self, $user_id ) = @_;
+
+    # update the cache
+    $self->{cache}->set( join( '|', 'user', 'last_auth', $user_id ) => time() );
+
+    return 1;
+}
+
+sub get_last_auth {
+    my ( $self, $user_id ) = @_;
+
+    my $last_auth =
+      $self->{cache}->get( join( '|', 'user', 'last_auth', $user_id ) );
+    return unless $last_auth;
+    return $last_auth;
 }
 
 1;
--- SL-User/t/SL-User.t
+++ SL-User/t/SL-User.t
@@ -3,6 +3,13 @@
 use strict;
 use warnings;
 
-use Test::More tests => 1;
-BEGIN { use_ok('SL::User') }
+use Test::More tests => 2;
+my $pkg;
+
+BEGIN {
+    $pkg = 'SL::User';
+    use_ok($pkg);
+}
+can_ok( $pkg,
+    qw( set_last_auth get_last_auth set_last_seen get_last_seen new ) );
 
