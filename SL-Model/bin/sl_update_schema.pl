fe8f675313f76319c16c5dbda74b302b84220857
Fred Moyer <fred@redhotpenguin.com>
2008-09-24 23:14:55 +0000

    More ad zones.
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1923 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Model/bin/sl_update_schema.pl
+++ SL-Model/bin/sl_update_schema.pl
@@ -27,12 +27,23 @@ $dbh->do("alter table ad_size rename column height to bug_height;");
 $dbh->do("alter table ad_size rename column width to bug_width;");
 
 $dbh->do("update ad_size set bug_width='200' where ad_size_id = 1");
+$dbh->do("update ad_size set css_url='http://www.silverliningnetworks.com/resources/css/sl_leaderboard.css' where ad_size_id = 1");
+
 $dbh->do("update ad_size set bug_width='200' where ad_size_id = 2");
+$dbh->do("update ad_size set css_url='http://www.silverliningnetworks.com/resources/css/sl_fullbanner.css' where ad_size_id = 2");
 
 $dbh->do("update ad_size set bug_width='120' where ad_size_id = 3");
+$dbh->do("update ad_size set css_url='http://www.silverliningnetworks.com/resources/css/sl_textad.css' where ad_size_id = 3");
+
 $dbh->do("update ad_size set bug_height='90' where ad_size_id = 4");
+$dbh->do("update ad_size set css_url='http://www.silverliningnetworks.com/resources/css/sl_textad.css' where ad_size_id = 4");
+
 $dbh->do("update ad_size set bug_height='90' where ad_size_id = 5");
+$dbh->do("update ad_size set css_url='http://www.silverliningnetworks.com/resources/css/sl_wide_skyscraper.css' where ad_size_id = 5");
+
 $dbh->do("update ad_size set bug_height='90' where ad_size_id = 6");
+$dbh->do("update ad_size set css_url='http://www.silverliningnetworks.com/resources/css/sl_halfpage.css' where ad_size_id = 6");
+
 
 # set the double wide skyscraper to normal wide skyscraper
 $dbh->do("update ad_zone set ad_size_id = 5 where ad_size_id = 7");
@@ -40,9 +51,16 @@ $dbh->do("update ad_zone set ad_size_id = 5 where ad_size_id = 7");
 $dbh->do("update ad_size set name='Full Banner & Skyscraper' where ad_size_id = 7");
 $dbh->do("update ad_size set bug_height='60' where ad_size_id = 7");
 $dbh->do("update ad_size set bug_width='120' where ad_size_id = 7");
-$dbh->do("update ad_size set css_url='http://www.silverliningnetworks.com/css/sl_full_banner__skyscraper.css' where ad_size_id = 7");
+$dbh->do("update ad_size set css_url='http://www.silverliningnetworks.com/resources/css/sl_double_fb_ss.css' where ad_size_id = 7");
 
-$dbh->do("insert into ad_size (ad_size_id, name, css_url, bug_height, bug_width) values (8, 'Leaderboard & Skyscraper', 'http://www.silverliningnetworks.com/css/sl_leaderboard__skyscraper.css', 90, 120) ");
+$dbh->do("insert into ad_size (ad_size_id, name, css_url, bug_height, bug_width) values (8, 'Leaderboard & Skyscraper', 'http://www.silverliningnetworks.com/resources/css/sl_double_lb_ss.css', 90, 120) ");
 
-$dbh->do("insert into ad_size (ad_size_id, name, css_url, bug_height, bug_width) values (9, 'Leaderboard & Wide Skyscraper', 'http://www.silverliningnetworks.com/css/sl_leaderboard__wide_skyscraper.css', 90, 160) ");
+
+$dbh->do("insert into ad_size (ad_size_id, name, css_url, bug_height, bug_width) values (9, 'Leaderboard & Wide Skyscraper', 'http://www.silverliningnetworks.com/resources/css/sl_double_lb_wss.css', 90, 160) ");
+
+
+$dbh->do("insert into ad_size (ad_size_id, name, css_url, bug_height, bug_width) values (10, 'Floating Leaderboard', 'http://www.silverliningnetworks.com/resources/css/sl_floating_leaderboard.css', 90, 200) ");
+
+$dbh->do("insert into ad_size (ad_size_id, name, css_url, bug_height, bug_width) values (11, 'Floating Full Banner', 'http://www.silverliningnetworks.com/resources/css/sl_floating_full_banner.css', 60, 200) ");
+
 
