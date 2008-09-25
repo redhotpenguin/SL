62e1c4257807c89e3d42003b9620f3dfc7e688e5
Fred Moyer <fred@redhotpenguin.com>
2008-09-25 15:22:08 +0000

    Updates for ad bar v2
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1925 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL/lib/SL/Apache/Proxy/SplashHandler.pm
+++ SL/lib/SL/Apache/Proxy/SplashHandler.pm
@@ -57,11 +57,7 @@ sub handler {
         $separator = '?';
     }
 
-    my $location = $splash_url
-      . $separator . 'url='
-      . $r->pnotes('url')
-      . '&auth_param='
-      . int( rand(1_000_000_000) );
+    my $location = $splash_url . $separator . 'url=' . $r->pnotes('url');
     $r->log->debug("splash page redirecting to $location") if DEBUG;
 
     $r->headers_out->set( Location => $location );
