d799bf11825c16657bbde318ccaac78e03a881be
Fred Moyer <fred@redhotpenguin.com>
2008-09-30 04:24:34 +0000

    committed and uploaded to typepad
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@1962 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Resources/resources/js/horizontal.js
+++ SL-Resources/resources/js/horizontal.js
@@ -1,4 +1,7 @@
 function silverLiningClose() {
 var container = document.getElementById('silver_lining_ad_horizontal');
-container.setAttribute("style", "display:none;");
+var leaderboard = document.getElementById('silver_lining_leaderboard');
+var hideitem = "display:none; !important";
+container.style.cssText = hideitem;
+leaderboard.sstyle.cssText = hideitem;
 };
