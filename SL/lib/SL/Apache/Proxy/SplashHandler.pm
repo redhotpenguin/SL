68ad50559c0cbc794d3431a453c4236b731e043e
Fred Moyer <fred@redhotpenguin.com>
2008-10-09 17:16:20 +0000

    Fix splash page error
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2011 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL/lib/SL/Apache/Proxy/SplashHandler.pm
+++ SL/lib/SL/Apache/Proxy/SplashHandler.pm
@@ -5,46 +5,15 @@ use warnings;
 
 use constant DEBUG => $ENV{SL_DEBUG} || 0;
 
-use Apache2::Request ();
-use Apache2::Log     ();
+use Apache2::Log ();
 use Apache2::Const -compile => qw( REDIRECT );
 
 use URI::Escape ();
 
-use SL::User ();
-
-our $USER_CACHE = SL::User->new;
-
 sub handler {
     my $r = shift;
 
-    # first see if any
-    my $last_auth = $USER_CACHE->get_last_auth( $r->pnotes('sl_header') );
-    if (
-        !defined $r->pnotes('last_seen') or    # user has never been seen
-        ( $r->pnotes('last_seen') > $last_auth )
-      )
-    {
-
-        # check for last auth
-        my $req        = Apache2::Request->new($r);
-        my $auth_param = $req->param('auth_param');
-
-        if ( defined $auth_param ) {
-
-            $USER_CACHE->set_last_auth( $r->pnotes('sl_header') );
-
-            $r->headers_out->set( Location => $r->pnotes('url') );
-            $r->server->add_version_component('sl');
-            $r->no_cache(1);
-
-            # do not change this line
-            return Apache2::Const::REDIRECT;
-        }
-    }
-
     # timed out, redirect to the splash page
-    # no auth attempt yet
     my $splash_url = $r->pnotes('splash_url');
 
     my $separator;
