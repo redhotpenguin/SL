3c2c315a3aec4d6f1a726ce0823c155a4f8b0189
Fred Moyer <fred@redhotpenguin.com>
2008-10-31 08:48:34 +0000

    credit card form being delivered
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2082 bd012f14-6cfe-0310-9a23-91b8f1fede10

---
+++ SL-App/lib/SL/Apache/App/CP.pm
@@ -0,0 +1,48 @@
+package SL::Apache::App::CP;
+
+use strict;
+use warnings;
+
+
+use Apache2::Const -compile => qw(OK SERVER_ERROR NOT_FOUND M_GET M_POST);
+use Apache2::Log        ();
+use Apache2::SubRequest ();
+use Data::FormValidator ();
+use Apache2::Request    ();
+use Apache2::SubRequest ();
+
+use base 'SL::Apache::App';
+
+use SL::App::Template ();
+our $Tmpl = SL::App::Template->template();
+
+# this specific template logic
+use Data::Dumper;
+use SL::Model;
+use SL::Model::App;    # works for now
+
+sub auth {
+    my ($class, $r) = @_;
+
+    my %tmpl_data;
+
+    my $output;
+    my $ok = $Tmpl->process('auth/index.tmpl', \%tmpl_data, \$output, $r);
+    $ok ? return $class->ok($r, $output)
+        : return $class->error($r, "Template error: " . $Tmpl->error());
+}
+
+
+sub paid {
+    my ($class, $r) = @_;
+
+    my %tmpl_data;
+
+    my $output;
+    my $ok = $Tmpl->process('auth/paid.tmpl', \%tmpl_data, \$output, $r);
+    $ok ? return $class->ok($r, $output)
+        : return $class->error($r, "Template error: " . $Tmpl->error());
+
+}
+
+1;
