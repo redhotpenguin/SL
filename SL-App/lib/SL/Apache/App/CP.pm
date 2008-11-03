d7e2bc9cc436828dbe3d86bbfa8655a097a91e1a
Fred Moyer <fred@redhotpenguin.com>
2008-11-03 00:33:15 +0000

    server side missing form validation working
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2088 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-App/lib/SL/Apache/App/CP.pm
+++ SL-App/lib/SL/Apache/App/CP.pm
@@ -3,11 +3,12 @@ package SL::Apache::App::CP;
 use strict;
 use warnings;
 
-
 use Apache2::Const -compile => qw(OK SERVER_ERROR NOT_FOUND M_GET M_POST);
 use Apache2::Log        ();
 use Apache2::SubRequest ();
 use Data::FormValidator ();
+use Data::FormValidator::Constraints qw(:closures);
+
 use Apache2::Request    ();
 use Apache2::SubRequest ();
 
@@ -22,31 +23,91 @@ use SL::Model;
 use SL::Model::App;    # works for now
 
 sub auth {
-    my ($class, $r) = @_;
+    my ( $class, $r ) = @_;
 
     my %tmpl_data;
 
     my $output;
-    my $ok = $Tmpl->process('auth/index.tmpl', \%tmpl_data, \$output, $r);
-    $ok ? return $class->ok($r, $output)
-        : return $class->error($r, "Template error: " . $Tmpl->error());
+    my $ok = $Tmpl->process( 'auth/index.tmpl', \%tmpl_data, \$output, $r );
+    $ok
+      ? return $class->ok( $r, $output )
+      : return $class->error( $r, "Template error: " . $Tmpl->error() );
 }
 
+sub valid_plan {
+    return sub {
+        my $dfv = shift;
+        my $val = $dfv->get_current_constraint_value;
+
+        return $val if ( $val =~ m/(?:day|month|hour)/ );
+        return;
+      }
+}
 
 sub paid {
-    my ($class, $r) = @_;
+    my ( $class, $r, $args_ref ) = @_;
 
-    my $req = Apache2::Request->new($r);
+    my $req = $args_ref->{req} || Apache2::Request->new($r);
 
+    # plan passed on GET
+    my %tmpl_data = (
+        plan   => $req->param('plan'),
+        errors => $args_ref->{errors},
+        req    => $req,
+    );
 
-    my %tmpl_data = ( plan => $req->param('plan'), );
+    if ( $r->method_number == Apache2::Const::M_GET ) {
 
+        my $output;
+        my $ok = $Tmpl->process( 'auth/paid.tmpl', \%tmpl_data, \$output, $r );
 
+        return $class->ok( $r, $output ) if $ok;
+        return $class->error( $r, "Template error: " . $Tmpl->error() );
+    }
+    elsif ( $r->method_number == Apache2::Const::M_POST ) {
 
-    my $output;
-    my $ok = $Tmpl->process('auth/paid.tmpl', \%tmpl_data, \$output, $r);
-    $ok ? return $class->ok($r, $output)
-        : return $class->error($r, "Template error: " . $Tmpl->error());
+        ## processing a payment, here we go
+
+        # reset method to get for redirect
+        $r->method_number(Apache2::Const::M_GET);
+
+        my %payment_profile = (
+            required => [
+                qw( first_name last_name card_type card_number cvc
+                  month year street city zip state email plan )
+            ],
+            constraint_methods => {
+                email       => email(),
+                zip         => zip(),
+                card_expiry => cc_exp(),
+                card_type   => cc_type(),
+                card_number => cc_number( { fields => ['card_type'] } ),
+                plan        => valid_plan(),
+            }
+        );
+        my $results = Data::FormValidator->check( $req, \%payment_profile );
+
+        # handle form errors
+        if ( $results->has_missing or $results->has_invalid ) {
+            my $errors = $class->SUPER::_results_to_errors($results);
+            return $class->paid(
+                $r,
+                {
+                    errors => $errors,
+                    req    => $req
+                }
+            );
+        }
+    }
+
+    #  my $output;
+    #  my $ok = $Tmpl->process('auth/paid.tmpl', \%tmpl_data, \$output, $r);
+    my $ok = 1;
+
+    my $output = 'yep it worked';
+    $ok
+      ? return $class->ok( $r, $output )
+      : return $class->error( $r, "Template error: " . $Tmpl->error() );
 
 }
 
