2203982a46594ba495e7390d5bb93b5602e777d8
Fred Moyer <fred@redhotpenguin.com>
2008-11-13 07:20:15 +0000

    code to production to test!
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2102 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Payment/lib/SL/Payment.pm
+++ SL-Payment/lib/SL/Payment.pm
@@ -18,13 +18,33 @@ BEGIN {
     $Config = SL::Config->new;
 }
 
-sub make_payment {
+sub process {
     my ( $class, $args ) = @_;
+
+    die "missing args"
+      unless ( defined $args->{account_id}
+        && defined $args->{email}
+        && defined $args->{mac}
+        && defined $args->{card_number}
+        && defined $args->{card_type}
+        && defined $args->{card_exp}
+        && defined $args->{zip}
+        && defined $args->{first_name}
+        && defined $args->{last_name}
+        && defined $args->{amount}
+        && defined $args->{cvv2}
+        && defined $args->{ip}
+        && defined $args->{street}
+        && defined $args->{city}
+        && defined $args->{state}
+        && defined $args->{referer}
+        && defined $args->{plan}
+ );
 
     my ($last_four) = $arg->{card_number} =~ m/(\d{4})$/;
 
-    my $duration = delete $args->{duration} . 's';
-    die "bad duration: $duration" unless $duration =~ m/(?:month|day|hour)/;
+    my $plan = delete $args->{plan} . 's';
+    die "bad plan: $plan" unless $duration =~ m/(?:month|day|hour)/;
 
     my $stop =
       DateTime::Format::Pg->format_datetime(
@@ -37,9 +57,10 @@ sub make_payment {
             mac        => $args->{mac},
             amount     => $args->{amount},
             stop       => $stop,
-            email      => $args->{email}
+            email      => $args->{email},
             last_four  => $last_four,
             type       => $args->{card_type},
+            ip         => $args->{ip},
         }
     );
 
@@ -47,15 +68,38 @@ sub make_payment {
 
     # munge transaction args
     my $email = delete $args->{email};
-    $args->{description}    = "$duration transaction payment for $email";
+    $args->{description}    = "$plan transaction payment for $email";
     $args->{invoice_number} = $payment->payment_id;
     $args->{customer_id}    = sprintf(
-        "user %u account %u",
-        delete $args->{usr_id},
+        "macaddress %u account %u",
+        delete $args->{mac},
         delete $args->{account_id}
     );
 
-    my $tx = $class->_make_transaction($args);
+    # authorize
+    my $tx = Business::OnlinePayment->new('AuthorizeNet');
+
+    $tx->content(
+        login          => $Config->login,
+        password       => $Config->key,
+        action         => 'Normal Authorization',
+        description    => $args->{description},
+        amount         => $args->{amount},
+        invoice_number => $args->{invoice_number},
+        customer_id    => $args->{customer_id},
+        first_name     => $args->{first_name},
+        last_name      => $args->{last_name},
+        address        => $args->{address},
+        city           => $args->{city},
+        state          => $args->{state},
+        zip            => $args->{zip},
+        card_number    => $args->{card_number},
+        expiration     => $args->{card_exp},
+        type           => $args->{card_type},
+        cvv2           => $args->{cvv2},
+        referer        => $args->{referer},
+    );
+    $tx->submit;
 
     if ( $tx->is_success ) {
         warn "Card processed successfully: " . $tx->authorization if DEBUG;
@@ -68,7 +112,7 @@ sub make_payment {
         $payment->error_message( $tx->error_message );
         $payment->approved('f');
         $payment->update;
-        return;
+        die $tx->error_message;
     }
 
     # send the email receipt
@@ -88,7 +132,7 @@ sub make_payment {
 Hi $to,
 
 Thank you for purchasing wifi access with Silver Lining Networks for the period of
-one $duration.  Your confirmation number is $authorization.
+one $plan.  Your confirmation number is $authorization.
 
 Please contact us at support\@silverliningnetworks.com if have any questions.
 
@@ -102,37 +146,6 @@ MAIL
 
       return 1;
 
-}
-
-sub _make_transaction {
-    my ( $class, $args ) = @_;
-
-    # authorize
-    my $tx = Business::OnlinePayment->new('AuthorizeNet');
-
-    $tx->content(
-        type           => $args->{card_type},
-        login          => $Config->login,
-        password       => $Config->key,
-        action         => 'Normal Authorization',
-        description    => $args->{description},
-        amount         => $args->{amount},
-        invoice_number => $args->{invoice_number},
-        customer_id    => $args->{customer_id},
-        first_name     => $args->{first_name},
-        last_name      => $args->{last_name},
-        address        => $args->{address},
-        city           => $args->{city},
-        state          => $args->{state},
-        zip            => $args->{zip},
-        card_number    => $args->{card_number},
-        expiration     => $args->{expiration},
-        cvv2           => $args->{cvv2},
-        referer        => $args->{referer},
-    );
-    $tx->submit;
-
-    return $tx;
 }
 
 1;
