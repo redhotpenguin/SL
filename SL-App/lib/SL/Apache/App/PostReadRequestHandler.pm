8e2cd729b8f832f48a548a4c5d8c339eda7e9c46
Fred Moyer <fred@redhotpenguin.com>
2008-11-13 07:47:52 +0000

    fixup the remote ip
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2106 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-App/lib/SL/Apache/App/PostReadRequestHandler.pm
+++ SL-App/lib/SL/Apache/App/PostReadRequestHandler.pm
@@ -3,8 +3,17 @@ package SL::Apache::App::PostReadRequestHandler;
 use strict;
 use warnings;
 
-    if ( defined $r->headers_in->{'X-Forwarded-For'} ) {
-                $r->connection->remote_ip( $r->headers_in->{'X-Forwarded-For'} );
-		            delete $r->headers_in->{'X-Forwarded-For'};
-			        }
+use Apache2::Const->compile => qw( OK );
+use Apache2::RequestRec ();
+use Apache2::Connection ();
 
+sub handler {
+    my $r = shift;
+
+    if ( defined $r->headers_in->{'X-Forwarded-For'} ) {
+        $r->connection->remote_ip( $r->headers_in->{'X-Forwarded-For'} );
+        delete $r->headers_in->{'X-Forwarded-For'};
+    }
+
+    return Apache2::Const::OK;
+  }
