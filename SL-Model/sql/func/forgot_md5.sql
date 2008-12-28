200c25ac90ba36307e1e627b89bec8b774aa616a
Fred Moyer <fred@redhotpenguin.com>
2008-12-28 19:32:09 +0000

    fix dying function for forgot password token
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2235 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Model/sql/func/forgot_md5.sql
+++ SL-Model/sql/func/forgot_md5.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION forgot_md5() RETURNS trigger as $_$
     BEGIN
     UPDATE forgot SET expired = 't' WHERE reg_id = NEW.reg_id AND expired = 'f';
-    NEW.link_md5 = md5(NEW.reg_id || NEW.ts || random());
+    NEW.link_md5 = md5(' || NEW.reg_id || NEW.ts || random() || ');
     RETURN NEW;
 END;
 $_$
