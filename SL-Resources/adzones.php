9aa9b0c48d5ad709bae6966458ea3ae46433ccac
Alex Wolfe <thewolfe@gmail.com>
2009-01-06 19:55:15 +0000

    Forgot one file for New Home page beta
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2263 bd012f14-6cfe-0310-9a23-91b8f1fede10

---
+++ SL-Resources/adzones.php
@@ -0,0 +1,30 @@
+<?php include 'includes/header.php' ?> 
+<body id="sln_adzones">
+<?php include 'includes/masthead.php' ?> 
+<?php include 'includes/navigation.php'?> 
+
+<div id="content">
+	<h2>Advertisement Zones</h2>
+	<ul>
+	    <li><a href="adzones/horizontal_full_banner.html">Full Banner</a></li>
+	    <li><a href="adzones/horizontal_leaderboard.html">Leaderboard</a></li>
+	    <li><a href="adzones/horizontal_textad.html">Text Ad</a></li>
+	    <li><a href="adzones/vertical_half_page.html">Half Page Skyscraper</a></li>
+	    <li><a href="adzones/vertical_skyscraper.html">Skyscraper</a></li>
+	    <li><a href="adzones/vertical_wide_skyscraper.html">Wide Skyscraper</a></li>
+	    <li><a href="adzones/double_fullbanner_skyscraper.html">Double Full Banner Skyscraper</a></li>
+	    <li><a href="adzones/double_leaderboard_skyscraper.html">Double Leaderboard Skyscraper</a></li>
+	    <li><a href="adzones/double_leaderboard_wideskyscraper.html">Double Leaderboard Wide Skyscraper</a></li>
+	    <li><a href="adzones/floating_full_banner.html">Floating Full Banner</a></li>
+	    <li><a href="adzones/floating_leaderboard.html">Floating Leaderboard</a></li>
+	    <li><a href="adzones/floating_footer_leaderboard.html">Floating Footer (Leaderboard)</a></li>
+	    <li><a href="adzones/floating_footer_full_banner.html">Floating Footer (Full Banner)</a></li>
+	    <li><a href="adzones/floating_footer_fb_db.html">Floating Footer (FB Double)</a></li>
+	    <li><a href="adzones/floating_cloud.html">Floating Cloud</a></li>
+	    <li><a href="adzones/purchase_now.html">Purchase Now</a></li>
+    </ul>
+</div>
+
+<?php include 'includes/sidebar.php'?>
+</body>
+</html>
