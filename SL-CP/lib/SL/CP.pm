a0142f01ddc377acb3f5cdcd1406d39d3a2a3b3b
Fred Moyer <fred@redhotpenguin.com>
2009-01-09 00:35:53 +0000

    tidy up portal code
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2267 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-CP/lib/SL/CP.pm
+++ SL-CP/lib/SL/CP.pm
@@ -6,11 +6,11 @@ use warnings;
 use Apache2::RequestRec ();
 use Apache2::Connection ();
 use Apache2::Const -compile => qw( NOT_FOUND OK REDIRECT SERVER_ERROR );
-use Apache2::Log ();
+use Apache2::Log         ();
 use Apache2::RequestUtil ();
-use Apache2::Request ();
-use Apache2::URI ();
-use APR::Table ();
+use Apache2::Request     ();
+use Apache2::URI         ();
+use APR::Table           ();
 
 use URI::Escape ();
 
@@ -24,90 +24,86 @@ our $VERSION = 0.01;
 our ( $Config, $Lease_file, $Auth_url );
 
 BEGIN {
-    $Config             = SL::Config->new;
-    $Auth_url           = $Config->sl_cp_auth_url || die 'oops';
-    $Lease_file         = $Config->sl_dhcp_lease_file || die 'oops';
+    $Config     = SL::Config->new;
+    $Auth_url   = $Config->sl_cp_auth_url || die 'oops';
+    $Lease_file = $Config->sl_dhcp_lease_file || die 'oops';
 }
 
 sub handler {
     my $r = shift;
 
-    $r->log->debug("$$ handling new request for " . $r->connection->remote_ip)
-	if DEBUG;
+    $r->log->debug( "$$ handling new request for " . $r->connection->remote_ip )
+      if DEBUG;
 
-    my ($mac, $ip) = _mac_from_ip($r);
+    my ( $mac, $ip ) = _mac_from_ip($r);
     return Apache2::Const::NOT_FOUND unless $mac;
 
     my $dest_url = $r->construct_url( $r->unparsed_uri );
 
-
     # check to see if this mac has been paid for
-    my $paid_code = eval { SL::CP::IPTables->check_for_paid_mac($mac, $ip); };
+    my $paid_code = eval { SL::CP::IPTables->check_for_paid_mac( $mac, $ip ); };
     if ($@) {
-	$r->log->error("$$ Error checking paid mac $mac, $@");
-	return Apache2::Const::SERVER_ERROR;
+        $r->log->error("$$ Error checking paid mac $mac, $@");
+        return Apache2::Const::SERVER_ERROR;
     }
 
-    if ($paid_code == 1) {
+    if ( $paid_code == 1 ) {
 
-	$r->log->info("$$ valid mac $mac found, redirecting to $dest_url");
-	$r->headers_out->set( Location => $dest_url );
-	$r->no_cache(1);
-	return Apache2::Const::REDIRECT;
+        $r->log->info("$$ valid mac $mac found, redirecting to $dest_url");
+        $r->headers_out->set( Location => $dest_url );
+        $r->no_cache(1);
+        return Apache2::Const::REDIRECT;
     }
-
 
     # existing ads plan for this mac?
-    my $ads_code = eval { SL::CP::IPTables->check_for_ads_mac($mac, $ip); };
+    my $ads_code = eval { SL::CP::IPTables->check_for_ads_mac( $mac, $ip ); };
 
     if ($@) {
-	$r->log->error("$$ Error checking ads mac $mac, $@");
-	return Apache2::Const::SERVER_ERROR;
+        $r->log->error("$$ Error checking ads mac $mac, $@");
+        return Apache2::Const::SERVER_ERROR;
     }
 
+    if ( $ads_code == 1 ) {
 
-    if ($ads_code == 1) {
-
-	$r->log->info("$$ valid mac $mac found, redirecting to $dest_url");
-	$r->headers_out->set( Location => $dest_url );
-	$r->no_cache(1);
-	return Apache2::Const::REDIRECT;
+        $r->log->info("$$ valid mac $mac found, redirecting to $dest_url");
+        $r->headers_out->set( Location => $dest_url );
+        $r->no_cache(1);
+        return Apache2::Const::REDIRECT;
     }
-
-
 
     # at this point the mac obviously has not been put into a rule chain
     # so redirect to the auth server
     $dest_url = URI::Escape::uri_escape($dest_url);
-    my $esc_mac = URI::Escape::uri_escape($mac);
+    my $esc_mac  = URI::Escape::uri_escape($mac);
     my $location = "$Auth_url?mac=$esc_mac&url=$dest_url";
-    $location .= "&expired=1" if ($paid_code == 401);
-    
+    $location .= "&expired=1" if ( $paid_code == 401 );
+
     $r->log->info("$$ unknown $mac found, redirecting to $location");
     $r->headers_out->set( Location => $location );
     $r->no_cache(1);
     return Apache2::Const::REDIRECT;
 }
 
-
 sub ads {
     my ( $class, $r ) = @_;
 
-    my ($mac, $ip) = _mac_from_ip($r);
+    my ( $mac, $ip ) = _mac_from_ip($r);
     return Apache2::Const::NOT_FOUND unless $mac;
 
-    my $req = Apache2::Request->new($r);
-    my $url = $req->param('url');
+    my $req     = Apache2::Request->new($r);
+    my $url     = $req->param('url');
     my $req_mac = $req->param('mac');
-    my $token = $req->param('token');
+    my $token   = $req->param('token');
 
     # urls had better match up
-    unless ($req_mac eq $mac) {
-	$r->log->error("$$ auth macs didn't match up, mac $mac, req mac $req_mac");
-	return Apache2::Const::SERVER_ERROR;
+    unless ( $req_mac eq $mac ) {
+        $r->log->error(
+            "$$ auth macs didn't match up, mac $mac, req mac $req_mac");
+        return Apache2::Const::SERVER_ERROR;
     }
 
-    my $added = eval { SL::CP::IPTables->add_to_ads_chain($mac, $ip, $token); };
+    my $added =
+      eval { SL::CP::IPTables->add_to_ads_chain( $mac, $ip, $token ); };
 
     if ($@) {
 
@@ -115,15 +111,17 @@ sub ads {
         return Apache2::Const::SERVER_ERROR;
     }
 
+    if ( ( $added == 401 ) or ( $added == 404 ) ) {
 
-    if (($added == 401) or ($added == 404)) {
-	# must be a 404
-	my $dest_url = URI::Escape::uri_escape($url);
-	my $esc_mac = URI::Escape::uri_escape($mac);
+        # must be a 404
+        my $dest_url = URI::Escape::uri_escape($url);
+        my $esc_mac  = URI::Escape::uri_escape($mac);
         my $location = "$Auth_url?mac=$esc_mac&url=$dest_url";
-	$location .= "&expired=1" if ($added == 401);
-    
-        $r->log->info("$$ expired mac address $mac found, code $added, redirecting to $location");
+        $location .= "&expired=1" if ( $added == 401 );
+
+        $r->log->info(
+"$$ expired mac address $mac found, code $added, redirecting to $location"
+        );
         $r->headers_out->set( Location => $location );
         $r->no_cache(1);
         return Apache2::Const::REDIRECT;
@@ -142,36 +140,41 @@ sub ads {
 sub paid {
     my ( $class, $r ) = @_;
 
-    my ($mac, $ip) = _mac_from_ip($r);
+    my ( $mac, $ip ) = _mac_from_ip($r);
     return Apache2::Const::NOT_FOUND unless $mac;
 
-    my $req = Apache2::Request->new($r);
-    my $url = $req->param('url');
+    my $req     = Apache2::Request->new($r);
+    my $url     = $req->param('url');
     my $req_mac = $req->param('mac');
-    my $token = $req->param('token');
+    my $token   = $req->param('token');
 
     # urls had better match up
-    unless ($req_mac eq $mac) {
-	$r->log->error("$$ auth macs didn't match up, mac $mac, req mac $req_mac");
-	return Apache2::Const::SERVER_ERROR;
+    unless ( $req_mac eq $mac ) {
+        $r->log->error(
+            "$$ auth macs didn't match up, mac $mac, req mac $req_mac");
+        return Apache2::Const::SERVER_ERROR;
     }
 
-    my $added = eval { SL::CP::IPTables->add_to_paid_chain($mac, $ip, $token); };
+    my $added =
+      eval { SL::CP::IPTables->add_to_paid_chain( $mac, $ip, $token ); };
 
-   if ($@) {
+    if ($@) {
 
         $r->log->error("$$ error adding mac $mac to paid chain: $@");
         return Apache2::Const::SERVER_ERROR;
     }
 
-    if (($added == 401) or ($added == 404)) {
-	# must be a 404
-	my $dest_url = URI::Escape::uri_escape($url);
-	my $esc_mac = URI::Escape::uri_escape($mac);
+    if ( ( $added == 401 ) or ( $added == 404 ) ) {
+
+        # must be a 404
+        my $dest_url = URI::Escape::uri_escape($url);
+        my $esc_mac  = URI::Escape::uri_escape($mac);
         my $location = "$Auth_url?mac=$esc_mac&url=$dest_url";
-	$location .= "&expired=1" if ($added == 401);
-    
-        $r->log->info("$$ expired mac address $mac found, code " . $added->code . ", redirecting to $location");
+        $location .= "&expired=1" if ( $added == 401 );
+
+        $r->log->info( "$$ expired mac address $mac found, code "
+              . $added->code
+              . ", redirecting to $location" );
         $r->headers_out->set( Location => $location );
         $r->no_cache(1);
         return Apache2::Const::REDIRECT;
@@ -209,9 +212,9 @@ sub _mac_from_ip {
         return;
     }
 
-	$r->log->info("$$ found mac address $mac for ip $ip");
+    $r->log->info("$$ found mac address $mac for ip $ip");
 
-    return ($mac, $ip);
+    return ( $mac, $ip );
 }
 
 1;
