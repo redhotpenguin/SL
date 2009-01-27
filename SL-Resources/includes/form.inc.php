96d2014de60d3ffd60559711868cd83cf1a64289
Alex Wolfe <thewolfe@gmail.com>
2009-01-27 07:00:50 +0000

    Some serious updates for the header of the new CMS. It's looking sweet!
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2314 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Resources/includes/form.inc.php
+++ SL-Resources/includes/form.inc.php
@@ -5,12 +5,20 @@
 			<label for="router_name">Create a name <strong>*</strong></label>
 			<input type="text" name="router_name" value="" />
 		</li>
-		<li class="one_third">
+		<li class="one_third ">
 			<label class="left" for="mac_address">MAC address <strong>*</strong></label>
 			<a class="right hint" href="#">what's this?</a>
 			<input class="clear" type="text" name="mac_address" value="" />
 			<label class="tip">example: 00:16:a3:78:32:60</label>
 		</li>
+		<li class="one_third ">
+			<label class="left" for="ad_campaign">Advertisement Campaign <strong>*</strong></label>
+			<a class="right hint" href="#">what's this?</a>
+			<select name="ad_campaign" class="clear">
+				<option value="Silver Lining (default)">Silver Lining (default)</option>
+				<option value="Bling Bling">Bling Bling</option>
+			</select>
+		</li>		
 	</ul>
 	<div class="submit_form">
 		<input type="submit" value="Add Router" name="submit" />
