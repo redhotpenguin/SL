3581d28a540577bfea1c55b1e73d194ca3828119
Fred Moyer <fred@redhotpenguin.com>
2009-02-04 06:54:56 +0000

    open-mesh.com checkin program for WRT54GL routers.  Good base for our new firmware
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2349 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Linksys/imagebuilder/files/usr/bin/openmesh_checkin.pl
+++ SL-Linksys/imagebuilder/files/usr/bin/openmesh_checkin.pl
@@ -15,24 +15,26 @@ my ($macaddr) = $ifconfig =~ m/(\w{2}\:\w{2}\:\w{2}\:\w{2}\:\w{2}\:\w{2})/;
 my $data = "ip=$wan_gateway&mac=$macaddr&robin=1500&batman=0.5.6-r3&memfree=$mem";
 $data .= "&ssid=argenta&pssid=-none-&users=0&kbup=0&kbdown=0&top_users=-none-";
 
-# get the uptime
+# get the uptime, and extract the date components
 my ($hours, $min, $sec, $days) = `/usr/bin/uptime`
     =~ m/(\d{2})\:(\d{2})\:(\d{2})\D+(\d+)\s/;
 
+# hrm uptime is broken
 my $uptime = sprintf("%0dd:%0dh:%02dm", $days, $hours, $min);
 
-# some other random stuff
-$data .= "&uptime=$uptime&NTR=0&gateway=self&gw-qual=0&routes=-none-";
-$data .= "&hops=0&RTT=0&nbs=0&nodes=0&rssi=0&rssi=0";
+# gw-qual to make the outage bar green, 1 hop for gateway
+$data .= "&uptime=$uptime&NTR=0&gateway=self&gw-qual=255&routes=-none-";
+$data .= "&hops=1&RTT=0&nbs=0&nodes=0&rssi=20&rssi=20";
 
 # hostname
 my $hostname = 'checkin.open-mesh.com';
 
-# compile the wget
+# build the url
 my $str = "https://$hostname/checkin-batman.php?$data";
 
-my $get = "/usr/bin/curl -k '$str'";
 # log it for troubleshooting
-# warn("get is $get");
+# warn("url is $str");
+
+# send stats to open-mesh
 exec( "/usr/bin/curl",  "-k",  "$str");
 
