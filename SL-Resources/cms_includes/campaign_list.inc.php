dc2400ebfe5fbfb526975cd2c8f2914492cde2de
Alex Wolfe <thewolfe@gmail.com>
2009-02-08 23:51:09 +0000

    Updated Leaderboard adzone to have an  advertise here link and created a Text Leaderboard Floating Footer.
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2361 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Resources/cms_includes/campaign_list.inc.php
+++ SL-Resources/cms_includes/campaign_list.inc.php
@@ -1,59 +1,59 @@
-<table class="router_table">
+<table class="campaign_table">
 	<thead>
 		<tr>
 			<th class="status">status</th>
-			<th class="router_name">router name</th>
-			<th class="mac_address">activate/deactivate</th>
+			<th class="router_name">Campaign</th>
+			<th class="mac_address">turn on/off</th>
 			<th class="delete">delete</th>
 		</tr>
 	</thead>
 	<tbody>
 		<tr>
 			<td class="status"><strong class="active">active</strong></td>
-			<td class="router_name"><a href="#">Starbucks Coffee Router</a></td>
-			<td class="campaign_activate"><a href="#">deactivate campaign</a></td>
+			<td class="router_name"><a href="#">Silver Lining Campaign</a></td>
+			<td class="campaign_activate"><a href="#">turn off</a></td>
 			<td class="delete"><a href="#">delete</a></td>
 		</tr>
 		<tr>
 			<td class="status"><strong class="inactive">inactive</strong></td>
 			<td class="router_name"><a href="#">Pete's Coffee Router</a></td>
-			<td class="campaign_activate"><a href="#">deactivate campaign</a></td>
+			<td class="campaign_activate"><a href="#">turn off</a></td>
 			<td class="delete"><a href="#">delete</a></td>
 		</tr>
 		<tr>
 			<td class="status"><strong class="active">active</strong></td>
-			<td class="router_name"><a href="#">Starbucks Coffee Router</a></td>
-			<td class="campaign_activate"><a href="#">deactivate campaign</a></td>
+			<td class="router_name"><a href="#">Silver Lining Campaign</a></td>
+			<td class="campaign_activate"><a href="#">turn off</a></td>
 			<td class="delete"><a href="#">delete</a></td>
 		</tr>
 		<tr>
 			<td class="status"><strong class="active">active</strong></td>
-			<td class="router_name"><a href="#">Starbucks Coffee Router</a></td>
-			<td class="campaign_activate"><a href="#">deactivate campaign</a></td>
+			<td class="router_name"><a href="#">Silver Lining Campaign</a></td>
+			<td class="campaign_activate"><a href="#">turn off</a></td>
 			<td class="delete"><a href="#">delete</a></td>
 		</tr>
 		<tr>
 			<td class="status"><strong class="active">active</strong></td>
-			<td class="router_name"><a href="#">Starbucks Coffee Router</a></td>
-			<td class="campaign_activate"><a href="#">deactivate campaign</a></td>
+			<td class="router_name"><a href="#">Silver Lining Campaign</a></td>
+			<td class="campaign_activate"><a href="#">turn off</a></td>
 			<td class="delete"><a href="#">delete</a></td>
 		</tr>
 		<tr>
 			<td class="status"><strong class="active">active</strong></td>
-			<td class="router_name"><a href="#">Starbucks Coffee Router</a></td>
-			<td class="campaign_activate"><a href="#">deactivate campaign</a></td>
+			<td class="router_name"><a href="#">Silver Lining Campaign</a></td>
+			<td class="campaign_activate"><a href="#">turn off</a></td>
 			<td class="delete"><a href="#">delete</a></td>
 		</tr>
 		<tr>
 			<td class="status"><strong class="active">active</strong></td>
-			<td class="router_name"><a href="#">Starbucks Coffee Router</a></td>
-			<td class="campaign_activate"><a href="#">deactivate campaign</a></td>
+			<td class="router_name"><a href="#">Silver Lining Campaign</a></td>
+			<td class="campaign_activate"><a href="#">turn off</a></td>
 			<td class="delete"><a href="#">delete</a></td>
 		</tr>
 		<tr>
 			<td class="status"><strong class="active">active</strong></td>
-			<td class="router_name"><a href="#">Starbucks Coffee Router</a></td>
-			<td class="campaign_activate"><a href="#">deactivate campaign</a></td>
+			<td class="router_name"><a href="#">Silver Lining Campaign</a></td>
+			<td class="campaign_activate"><a href="#">turn off</a></td>
 			<td class="delete"><a href="#">delete</a></td>
 		</tr>
 		
