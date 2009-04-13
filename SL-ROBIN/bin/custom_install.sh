ff9904ad6afbe3838f223ffcf2728c021b8f136a
Fred Moyer <fred@redhotpenguin.com>
2009-04-13 20:37:36 +0000

    Updates to do more verbose debugging, and reflect the new file layout for md5sums.  Thanks to Leo for the great start on this file.
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2498 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-ROBIN/bin/custom_install.sh
+++ SL-ROBIN/bin/custom_install.sh
@@ -1,23 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-kmod-sln_file = kmod-sln_2.6.23.17+0.20-atheros-5_mips.ipk
+VERSION=0.01
+LICENSE="Copyright 2009 Silver Lining Networks, Inc."
 
-sln_file = sln_0.20-5_mips.ipk
-
-microperl_file = microperl_5.10.0-1_mips.ipk
-
-kmod-sln_md5_file = kmod-sln_md5sum.txt
-
-sln_md5_file = sln_md5sum.txt
-
-microperl_md5_file = microperl_md5sum.txt
-
-URL1=https://app.silverliningnetworks.com/firmware/LATEST/SL-ROBIN
-
-URL2=https://app.silverliningnetworks.com/firmware/LATEST/SL-ROBIN
+MICROPERL_FILE=microperl_5.10.0-1_mips.ipk
+KMOD_SLN_FILE=kmod-sln_2.6.23.17+0.20-atheros-5_mips.ipk
+SLN_FILE=sln_0.20-5_mips.ipk
 
 
-[ -e $microperl_file  ] && rm -f $microperl_file 
+URL_MICROPERL=http://fw.slwifi.com/SL-ROBIN/perl/$MICROPERL_FILE
+URL_KMOD_SLN=http://fw.slwifi.com/SL-ROBIN/sln/0.20-5_mips/$KMOD_SLN_FILE
+URL_SLN=http://fw.slwifi.com/SL-ROBIN/sln/0.20-5_mips/$SLN_FILE
+
+
 [ -e $kmod-sln_file   ] && rm -f $kmod-sln_file 
 [ -e $sln_file   ] && rm -f $sln_file
 
@@ -25,69 +20,54 @@ URL2=https://app.silverliningnetworks.com/firmware/LATEST/SL-ROBIN
 [ -e $kmod-sln_md5_file ] && rm -f $kmod-sln_md5_file
 [ -e $sln_md5_file ] && rm -f $sln_md5_file
 
-
-wget "$URL1/$kmod-sln_file"
-wget "$URL1/$sln_file"
-wget "$URL1/$microperl_file"
-
-wget "$URL2/$kmod-sln_md5_file"
-wget "$URL2/$sln_md5_file"
-wget "$URL2/$microperl_md5_file"
-
-kmod-sln_md5 = $(md5sum $kmod-sln_file | head -c 32)
-
-sln_md5 = $(md5sum $sln_file | head -c 32)
-
-microperl_md5 = $(md5sum $microperl_file | head -c 32)
-
-kmod-sln_md5_expected = $(cat $kmod-sln_md5_file | head -c 32)
-
-sln_md5_expected = $(cat $sln_md5_file | head -c 32)
-
-microperl_md5_expected = $(cat $microperl_md5_file | head -c 32)
-
-if [ "$kmod-sln_md5" != "$kmod-sln_md5_expected" ] ; then
-
-     echo $kmod-sln_md5
-
-     echo $kmod-sln_md5_expected
-
-     stop
-fi
-
-if [ "$sln_md5" != "$sln_md5_expected" ] ; then
-
-     echo $sln_md5
-
-     echo $sln_md5_expected
-
-     stop
-fi
-
-if [ "$microperl_md5" != "$microperl_md5_expected" ] ; then
-
-     echo $microperl_md5
-
-     echo $microperl_md5_expected
-
-     stop
-fi
-
-
+# shut down cron
 /etc/init.d/cron stop
 
-ipkg install $microperl_file
+################################
+# install microperl first
 
-ipkg install $kmod-sln_file
+# remove existing files
+echo "removing old files"
+[ -e $MICROPERL_FILE ] && rm -f $MICROPERL_FILE
+[ -e $MICROPERL_FILE.md5 ] && rm -f $MICROPERL_FILE.md5
 
-ipkg install $sln_file
+# is microperl installed?  remove it
+echo "checking for old microperl install"
+[ "$(/usr/bin/ipkg list_installed microperl)" != 'Done.' ] && `/usr/bin/ipkg remove -force-depends microperl`
+
+# grab the new microperl
+echo "grabbing new microperl files"
+wget "$URL_MICROPERL"
+wget "$URL_MICROPERL.md5"
+
+# check the md5
+echo "checking md5s"
+
+MICROPERL_DL_MD5=$(/usr/bin/md5sum $MICROPERL_FILE | head -c 32)
+MICROPERL_MD5=$(/bin/cat $MICROPERL_FILE.md5 | head -c 32);
+echo "calculated md5 is $MICROPERL_DL_MD5"
+echo "expected md5 is   $MICROPERL_MD5"
+
+if [ $MICROPERL_MD5 != $MICROPERL_DL_MD5 ] ; then
+
+    echo "md5sum mismatch installing $URL_MICROPERL"
+
+    echo "Expected md5sum - $MICROPERL_MD5"
+
+    echo "Calculated md5sum - $MICROPERL_DL_MD5"
+
+    /etc/init.d/cron start
+
+    exit 1
+fi
+
+# md5s check out, install the new ipkg
+echo "installing new package $MICROPERL_FILE"
+
+INSTALLED=$(/usr/bin/ipkg -V3 install "$MICROPERL_FILE")
+
+echo "$MICROPERL_FILE installed ok - $INSTALLED"
 
 /etc/init.d/cron start
 
-
-
-
-
-
-
-
+exit 0
