f3b1f2ea8d007885631ac4c7d06dc095c69b5ccd
Fred Moyer <fred@redhotpenguin.com>
2009-04-16 05:38:32 +0000

    Add device type
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2521 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Model/lib/SL/Model/App/Router.pm
+++ SL-Model/lib/SL/Model/App/Router.pm
@@ -59,6 +59,13 @@ __PACKAGE__->add_columns(
     is_nullable => 1,
     size => undef,
   },
+  "device",
+  {
+    data_type => "text",
+    default_value => undef,
+    is_nullable => 1,
+    size => undef,
+  },
   "name",
   {
     data_type => "text",
