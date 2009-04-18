cc974990ad6972dfcdd074bd2a6e1bfbde5f4025
Fred Moyer <fred@redhotpenguin.com>
2009-04-18 06:22:38 +0000

    moved to static serfvefrz
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2546 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL/htdocs/resources/js/horizontal.js
+++
@@ -1,7 +0,0 @@
-function silverLiningClose() {
-var container = document.getElementById('silver_lining_ad_horizontal');
-var leaderboard = document.getElementById('silver_lining_leaderboard');
-var hideitem = "display:none; !important";
-container.style.cssText = hideitem;
-leaderboard.sstyle.cssText = hideitem;
-};
