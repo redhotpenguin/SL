d4d990778f6632c17952f91ba9e489a2757d664f
Fred Moyer <fred@redhotpenguin.com>
2009-05-20 14:18:27 +0000

    custom.sh to remove sln
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2648 bd012f14-6cfe-0310-9a23-91b8f1fede10

---
+++ SL-ROBIN/bin/deactivate/custom.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+VERSION=0.01
+LICENSE="Copyright 2009 Silver Lining Networks, Inc."
+DESCRIPTION="This program removes the Silver Lining ipkg onto open-mesh.com ROBIN enabled devices"
+
+IPKG=/usr/bin/ipkg
+echo "removing sln"
+$($IPKG -noaction remove sln)
+
+echo "removing kmod-sln"
+$($IPKG -noaction remove kmod-sln)
+
+echo "removing microperl"
+$($IPKG -noaction remove microperl)
+
+echo "rebooting"
+/sbin/reboot
