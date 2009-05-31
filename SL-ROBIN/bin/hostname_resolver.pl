baf1a50d7674f2be21007118e3bece52cdc3a4c1
Leo Portnoff <leoportnoff@gmail.com>
2009-05-31 16:33:45 +0000

    Reading hosts from skips_hosts.txt
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2728 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-ROBIN/bin/hostname_resolver.pl
+++ SL-ROBIN/bin/hostname_resolver.pl
@@ -7,8 +7,44 @@ use Net::DNS;
 
 use Data::Dumper;
 
-  my @hostname_list = qw(mail.google.com ad.afy11.net);
+  my @skips_hosts_array;
 
+  my @hostname_list_test;
+
+  my $hostname_test; 
+
+  unless (open(MYFILE, "skips_hosts.txt")){
+
+  die ("cannot open input file skips_hosts.txt\n");
+
+  }
+
+  @skips_hosts_array = <MYFILE>;
+
+  chomp @skips_hosts_array;
+
+
+  foreach  $hostname_test (@skips_hosts_array) {
+
+  if ($hostname_test =~ m/com/ or $hostname_test =~ m/net/) {
+
+     push(@hostname_list_test, $hostname_test);
+
+  }
+  }
+
+  print " HOSTNAME_LIST_TEST ARRAY \n ";
+
+  print @hostname_list_test;
+
+  print "\n";
+
+
+
+  #my @hostname_list = qw(112.2o7.net mail.google.com ad.afy11.net 0.channel13.facebook.com 02268001106.channel09.facebook.com ad-yt-bfp.doubleclick.net);
+
+  my @hostname_list = @hostname_list_test;
+ 
 
   our @nameservers_list = qw(207.69.188.172 207.69.188.171);
 
@@ -51,6 +87,11 @@ use Data::Dumper;
 
   print %results . "\n";
 
+  
+  }
+
+  }
+
   my $key;
   my $value;
 
@@ -61,7 +102,3 @@ use Data::Dumper;
 
   print Dumper(\%results);
 
-
-  }
-
-  }
