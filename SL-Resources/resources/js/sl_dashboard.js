382bde61624e5069cb99bb1e081f5ce4a93fca95
Alex Wolfe <thewolfe@gmail.com>
2009-06-03 06:27:45 +0000

    updates to form styles.
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2767 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Resources/resources/js/sl_dashboard.js
+++ SL-Resources/resources/js/sl_dashboard.js
@@ -20,19 +20,16 @@ $(document).ready(function(){
 	});
 
 
-    // readonly for determining fields
+    // readonly for deterin fields
     function setDeviceForm() {
 
         var device = $("#device").val();
-
         if ( device == 'mr3201a' || !device ) {
-
 
             $("#name").attr("readonly", true);
             $("#device").attr("readonly", true);
             $("#macaddr").attr("readonly", true);
             $("#notes").attr("readonly", true);
-
             $("#wrt54gl").css("display","none");
 
         } else if (device == 'wrt54gl' ) {
@@ -41,7 +38,6 @@ $(document).ready(function(){
             $("#name").attr("readonly", false);
             $("#macaddr").attr("readonly", false);
             $("#notes").attr("readonly", false);
-
             $("#wrt54gl").css("display","block");
 
         }
@@ -52,34 +48,6 @@ $(document).ready(function(){
     setDeviceForm();
 
     //find all form with class jqtransform and apply the plugin
-    $("form.form").jqTransform();	
-
-    // clear checkboxes for twitter or text message radio selection
-    $("#twitter").change(updateZones);
-    $("#text").change(updateZones);
-
-    $("INPUT[type='checkbox']").click(
-            function()
-             {
-                 if ($('#iab').attr('checked', false)) {
-                         $('#iab').attr('checked', true);
-                     }
-              }
-    )
-
-
-    function updateIab() {
-
-        if ($("#iab").attr('checked', false) ) {
-            $("#iab").attr('checked', true);
-        }
-    }
-    
-   function updateZones() {
-       adzones=document.getElementsByName('ad_zone');
-       for (var i=0;i<adzones.length-1;i++) {
-           adzones[i].checked=false;
-       }	
-   }
-
+    $(".form").jqTransform();	
+	
 });
