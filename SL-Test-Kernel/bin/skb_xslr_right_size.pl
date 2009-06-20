9635ac9511d660cd6d57862457649cb86e03e3de
Fred Moyer <fred@redhotpenguin.com>
2009-06-20 03:03:07 +0000

    forgot to commit these
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2864 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Test-Kernel/bin/skb_xslr_right_size.pl
+++ SL-Test-Kernel/bin/skb_xslr_right_size.pl
@@ -17,6 +17,7 @@ $DB::single = 1;
 my $url = 'http://www.google.com/mail/channel/bind?at=xn3j2v9hgz1alnhx0otznbju9f8xs7&VER=6&it=12&SID=C0430DDD724F9434&RID=48959&zx=klug2zrrl4rd&t=1';
 
 my	@headers = (
+'X-SLR', 'zzzzzzzz|0016b6493d60',
 'User-Agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en-US; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14',
 'Accept', 'text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
 'Accept-Language', 'en-us,en;q=0.5',
