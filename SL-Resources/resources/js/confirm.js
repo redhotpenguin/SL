5318b8697a6745570167659ac6b75a1e2be3d5bd
Alex Wolfe <thewolfe@gmail.com>
2009-06-22 04:16:58 +0000

    New files and images for updated Network Device page
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2868 bd012f14-6cfe-0310-9a23-91b8f1fede10

---
+++ SL-Resources/resources/js/confirm.js
@@ -0,0 +1,49 @@
+/*
+ * SimpleModal Confirm Modal Dialog
+ * http://www.ericmmartin.com/projects/simplemodal/
+ * http://code.google.com/p/simplemodal/
+ *
+ * Copyright (c) 2009 Eric Martin - http://ericmmartin.com
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Revision: $Id: confirm.js 185 2009-02-09 21:51:12Z emartin24 $
+ *
+ */
+
+$(document).ready(function () {
+	$('.confirm').click(function (e) {
+		e.preventDefault();
+		
+		var baseUrl = $('.confirm').attr('rel');
+
+		// example of calling the confirm function
+		// you must use a callback function to perform the "yes" action
+		confirm("Are you sure you want to remove this device?", function () {
+			window.location.href = '/app/router/deactivate';
+		});
+	});
+});
+
+function confirm(message, callback) {
+	$('#confirm').modal({
+		close:false,
+		position: ["20%",],
+		overlayId:'confirmModalOverlay',
+		containerId:'confirmModalContainer', 
+		onShow: function (dialog) {
+			dialog.data.find('.message').append(message);
+
+			// if the user clicks "yes"
+			dialog.data.find('.yes').click(function () {
+				// call the callback
+				if ($.isFunction(callback)) {
+					callback.apply();
+				}
+				// close the dialog
+				$.modal.close();
+			});
+		}
+	});
+}
