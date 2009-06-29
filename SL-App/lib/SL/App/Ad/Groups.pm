8cf95544a7628c4fdc1fe8d3ab6be9bbe2e7c133
Fred Moyer <fred@redhotpenguin.com>
2009-06-29 01:05:16 +0000

    Add Ad Zone working
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2884 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-App/lib/SL/App/Ad/Groups.pm
+++ SL-App/lib/SL/App/Ad/Groups.pm
@@ -11,14 +11,18 @@ use Data::FormValidator ();
 use Apache2::Request    ();
 use Apache2::SubRequest ();
 
+# be careful, this was breaking things
 #use JavaScript::Minifier::XS qw(minify);
 
 use base 'SL::App';
 
+use SL::Config        ();
 use SL::Model         ();
 use SL::Model::App    ();
 use SL::App::Template ();
 use Data::Dumper;
+
+our $Config = SL::Config->new;
 
 our $Tmpl = SL::App::Template->template();
 
@@ -34,7 +38,7 @@ sub dispatch_index {
     return $self->ok( $r, $output );
 }
 
-sub dispatch_clone {
+sub dispatch_add {
     my ( $self, $r ) = @_;
 
     my $req = Apache2::Request->new($r);
@@ -42,26 +46,52 @@ sub dispatch_clone {
 
     if ( $r->method_number == Apache2::Const::M_GET ) {
 
-        my $ad_zone = $reg->get_ad_zone($req->param('id'));
+        my $ad_zone = SL::Model::App->resultset('AdZone')->create(
+            {
+                name       => 'New Ad Zone',
+                account_id => $reg->account_id,
+                reg_id     => $reg->reg_id,
+                ad_size_id => 12,
+                code       => '',
+            }
+        );
+        $ad_zone->name('New Ad Zone');
+        $ad_zone->update;
+
+=cut
+
+
+        # fix this when dbix::class::row->copy is fixed
+        my ($ad_zone) = SL::Model::App->resultset('AdZone')->search(
+            {
+                'me.account_id' => $reg->account_id,
+                'me.active'     => 't',
+                'ad_size.grouping'   => 1,
+            },
+            { join => 'ad_size', order_by => 'me.mts asc', limit    => 1 }
+        );
+=cut
+
         return Apache2::Const::NOT_FOUND unless $ad_zone;
 
-        my $clone = $ad_zone->copy({ name => $ad_zone->name . ' 2' });
-
-        $r->pnotes('session')->{msg} =
-             sprintf( "Ad Zone '%s' was cloned", $ad_zone->name );
+        my $clone = $ad_zone->copy( { name => 'New Ad Zone' } );
 
         $r->headers_out->set(
-            Location => $r->construct_url('/app/ad/groups/list') );
+            Location => $r->construct_url(
+                    $Config->base_uri
+                  . '/app/ad/groups/edit?id='
+                  . $clone->ad_zone_id
+            )
+        );
         return Apache2::Const::REDIRECT;
 
+    }
+    else {
 
-    } else {
-
-         return Apache2::Const::HTTP_METHOD_NOT_ALLOWED;
-   }
+        return Apache2::Const::HTTP_METHOD_NOT_ALLOWED;
+    }
 
 }
-
 
 sub dispatch_assign {
     my ( $self, $r ) = @_;
@@ -71,38 +101,37 @@ sub dispatch_assign {
 
     if ( $r->method_number == Apache2::Const::M_GET ) {
 
-        my $ad_zone = $reg->get_ad_zone($req->param('id'));
+        my $ad_zone = $reg->get_ad_zone( $req->param('id') );
         return Apache2::Const::NOT_FOUND unless $ad_zone;
 
         my @routers = $reg->get_routers;
 
         foreach my $router (@routers) {
 
-          my $assign = SL::Model::App->resultset('RouterAdZone')
-            ->find_or_create({
-                  router_id => $router->router_id,
-                  ad_zone_id => $ad_zone->ad_zone_id,
-          });
-          $assign->update;
+            my $assign =
+              SL::Model::App->resultset('RouterAdZone')->find_or_create(
+                {
+                    router_id  => $router->router_id,
+                    ad_zone_id => $ad_zone->ad_zone_id,
+                }
+              );
+            $assign->update;
         }
 
         $r->pnotes('session')->{msg} =
-             sprintf( "Ad Zone '%s' was assigned to all routers",
-                      $ad_zone->name );
+          sprintf( "Ad Zone '%s' was assigned to all routers", $ad_zone->name );
 
         $r->headers_out->set(
             Location => $r->construct_url('/app/ad/groups/list') );
         return Apache2::Const::REDIRECT;
 
+    }
+    else {
 
-    } else {
-
-         return Apache2::Const::HTTP_METHOD_NOT_ALLOWED;
-   }
+        return Apache2::Const::HTTP_METHOD_NOT_ALLOWED;
+    }
 
 }
-
-
 
 sub dispatch_remove {
     my ( $self, $r ) = @_;
@@ -112,37 +141,35 @@ sub dispatch_remove {
 
     if ( $r->method_number == Apache2::Const::M_GET ) {
 
-        my $ad_zone = $reg->get_ad_zone($req->param('id'));
+        my $ad_zone = $reg->get_ad_zone( $req->param('id') );
         return Apache2::Const::NOT_FOUND unless $ad_zone;
 
         my @routers = $reg->get_routers;
 
         foreach my $router (@routers) {
 
-          my @razs = grep { $_->ad_zone->ad_size->grouping ==
-                              $ad_zone->ad_size->grouping   }
-            $router->router__ad_zones;
+            my @razs = grep {
+                $_->ad_zone->ad_size->grouping == $ad_zone->ad_size->grouping
+            } $router->router__ad_zones;
 
-          $_->delete for @razs;
+            $_->delete for @razs;
         }
 
         $r->pnotes('session')->{msg} =
-             sprintf( "Ad Zone '%s' was removed from all routers",
-                      $ad_zone->name );
+          sprintf( "Ad Zone '%s' was removed from all routers",
+            $ad_zone->name );
 
         $r->headers_out->set(
             Location => $r->construct_url('/app/ad/groups/list') );
         return Apache2::Const::REDIRECT;
 
+    }
+    else {
 
-    } else {
-
-         return Apache2::Const::HTTP_METHOD_NOT_ALLOWED;
-   }
+        return Apache2::Const::HTTP_METHOD_NOT_ALLOWED;
+    }
 
 }
-
-
 
 sub dispatch_edit {
     my ( $self, $r, $args_ref ) = @_;
@@ -150,15 +177,13 @@ sub dispatch_edit {
     my $req = $args_ref->{req} || Apache2::Request->new($r);
     my $reg = $r->pnotes( $r->user );
 
-    my $ad_zone = $reg->get_ad_zone($req->param('id'));
+    my $ad_zone = $reg->get_ad_zone( $req->param('id') );
     return Apache2::Const::NOT_FOUND unless $ad_zone;
 
     if ( $r->method_number == Apache2::Const::M_GET ) {
 
-
         my @ad_sizes = sort { $a->grouping <=> $b->grouping }
-          sort { $a->name cmp $b->name }
-            $reg->get_persistent_sizes;
+          sort { $a->name cmp $b->name } $reg->get_persistent_sizes;
 
         my %tmpl_data = (
             ad_sizes => \@ad_sizes,
@@ -166,7 +191,6 @@ sub dispatch_edit {
             errors   => $args_ref->{errors},
             req      => $req,
         );
-
 
         my $output;
         $Tmpl->process( 'ad/groups/edit.tmpl', \%tmpl_data, \$output, $r )
@@ -181,22 +205,23 @@ sub dispatch_edit {
         # validate input
         my @required = qw( name ad_size_id zone_type );
         my $constraints;
-        if ($req->param('zone_type') eq 'banner') {
+        if ( $req->param('zone_type') eq 'banner' ) {
 
             push @required, qw( image_href link_href );
-            $constraints = { image_href => $self->valid_link(),
-                             link_href  => $self->valid_link(), };
+            $constraints = {
+                image_href => $self->valid_link(),
+                link_href  => $self->valid_link(),
+            };
 
-
-        } elsif ($req->param('zone_type' eq 'code')) {
+        }
+        elsif ( $req->param( 'zone_type' eq 'code' ) ) {
 
             push @required, 'code';
         }
 
-
         my %profile = ( required => \@required, );
         if ($constraints) {
-           $profile{constraint_methods} = $constraints;
+            $profile{constraint_methods} = $constraints;
         }
 
         my $results = Data::FormValidator->check( $req, \%profile );
@@ -231,23 +256,26 @@ sub dispatch_edit {
         is_default => $req->param('is_default'),
     );
 
-    if ($req->param('zone_type') eq 'code') {
+    if ( $req->param('zone_type') eq 'code' ) {
 
         # use the invocation code
-        $args{'code'} = $req->param('code');
+        $args{'code'}       = $req->param('code');
         $args{'image_href'} = '';
-        $args{'link_href'} = '';
+        $args{'link_href'}  = '';
 
-    } elsif ($req->param('zone_type') eq 'banner') {
+    }
+    elsif ( $req->param('zone_type') eq 'banner' ) {
 
         # create a simple link
         $args{'image_href'} = $req->param('image_href');
-        $args{'link_href'} = $req->param('link_href');
-        $args{'code'} = sprintf('<a href="%s"><img src="%s"></a>', $args{'link_href'}, $args{'image_href'});;
+        $args{'link_href'}  = $req->param('link_href');
+        $args{'code'}       = sprintf( '<a href="%s"><img src="%s"></a>',
+            $args{'link_href'}, $args{'image_href'} );
     }
 
-
 =cut
+
+
     if ( my $double = $req->param('code_double') ) {
 
         #$double = minify( $double );
@@ -283,8 +311,9 @@ sub dispatch_list {
 
     #$r->log->debug( "ad zones: " . Dumper( \@ad_zones ) ) if DEBUG;
 
-    $_->mts(DateTime::Format::Pg->parse_datetime($_->mts)
-            ->strftime("%a %b %e,%l:%m %p")) for @ad_zones;
+    $_->mts( DateTime::Format::Pg->parse_datetime( $_->mts )
+          ->strftime("%a %b %e,%l:%m %p") )
+      for @ad_zones;
 
     my %tmpl_data = (
         session  => $r->pnotes('session'),
