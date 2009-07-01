37c2cc86b901771341abe0812941ce0ff796156c
Fred Moyer <fred@redhotpenguin.com>
2009-07-01 21:32:56 +0000

    update schema changer
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@2906 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Model/bin/sl_update_schema.pl
+++ SL-Model/bin/sl_update_schema.pl
@@ -39,6 +39,8 @@ SQL
 
 foreach my $bug ( @{$bugs} ) {
 
+    next if (($bug->ad_size_id == 22) or ($ad_size_id == 23));
+
     $dbh->do(<<SQL, {}, ($bug->{account_id}, 14, $bug->{image_href}, $bug->{link_href}));
 INSERT INTO AD_ZONE
 (code,name,account_id,ad_size_id,reg_id,image_href,link_href)
@@ -47,7 +49,7 @@ VALUES
 SQL
 
 
-    my $ad_zone_ids = $dbh->selectall_arrayref(<<SQL, {}, $bug->{bug_id} );
+    my $ad_zone_ids = $dbh->selectall_arrayref(<<SQL, {Slice => {}}, $bug->{bug_id} );
 SELECT ad_zone_id FROM ad_zone WHERE bug_id = ?
 SQL
 
@@ -55,13 +57,13 @@ SQL
     foreach my $ad_zone_id (@{$ad_zone_ids}) {
       $dbh->do("update ad_zone set is_default='t' where ad_zone_id = $bug_id");
 
-      my $radzones = $dbh->selectall_arrayref(<<SQL, {}, $ad_zone_id->[0]);
+      my $radzones = $dbh->selectall_arrayref(<<SQL, {Slice => {}}, $ad_zone_id->{ad_zone_id});
 SELECT router_id from router__ad_zone where ad_zone_id = ?
 SQL
       foreach my $radzone ( @{$radzones}) {
 
-        $dbh->do(sprintf("insert into router__ad_zone (router_id, ad_zone_id) values ( %s, %s ) ", $radzone->[0],
-                         $bug_id));
+        $dbh->do(sprintf("insert into router__ad_zone (router_id, ad_zone_id) values ( %s, %s ) ", $radzone->{router_id},
+                         $radzone->{ad_zone_id}));
 
       }
 
