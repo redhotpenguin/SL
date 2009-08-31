b7bdaef8b199bbf7e6e0972dc08074e413f565fc
Fred Moyer <fred@redhotpenguin.com>
2009-08-31 05:38:18 +0000

    Traffic chart rocking
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@3106 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Resources/resources/chart/line/data.pl
+++ SL-Resources/resources/chart/line/data.pl
@@ -10,9 +10,9 @@ foreach my $part ( qw( AM PM ) ) {
     foreach my $five ( 0..11 ) {
 
       my $time = sprintf('%d:%02d %s', $hour, $five * 5, $part);
-      my $users = int(rand(100));
-      my $up = $users * int(rand(10));
-      my $down = $users * int(rand(100));
+      my $users = int(rand(10 * $hour));
+      my $up = $users * int(rand(1 * $hour));
+      my $down = $users * int(rand(10*$hour));
 
       my $line = join(',', $time, $down, $up, $users);
       print FH $line . "\n";
