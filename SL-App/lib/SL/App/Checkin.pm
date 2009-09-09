c0282c558d05d713362e83d1299042967c96db3d
Fred Moyer <fred@redhotpenguin.com>
2009-09-09 04:00:58 +0000

    24 hour network overview traffic graph fully functional.
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@3146 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-App/lib/SL/App/Checkin.pm
+++ SL-App/lib/SL/App/Checkin.pm
@@ -4,8 +4,8 @@ use strict;
 use warnings;
 
 use Apache2::Const -compile => qw(OK HTTP_SERVICE_UNAVAILABLE NOT_FOUND);
-use Apache2::Log     ();
-use Apache2::Request ();
+use Apache2::Log        ();
+use Apache2::Request    ();
 use Apache2::Connection ();
 
 use base 'SL::App';
@@ -17,89 +17,100 @@ use constant DEBUG => $ENV{SL_DEBUG} || 0;
 sub handler {
     my $r = shift;
 
-
     # we get checkin string with unescaped + signs, so use args
     my %args;
-    my @pairs = split(/\&/, $r->args);
+    my @pairs = split( /\&/, $r->args );
     foreach my $pair (@pairs) {
 
-      my ($key, $value) = split(/\=/, $pair);
-      $args{$key} = $value || '';
+        my ( $key, $value ) = split( /\=/, $pair );
+        $args{$key} = $value || 0;
     }
-    $r->log->debug("args string: " . $r->args);
-    $r->log->debug("args: " . Dumper(\%args));
+    $r->log->debug( "args string: " . $r->args );
+    $r->log->debug( "args: " . Dumper( \%args ) );
 
-    my $ip = $r->connection->remote_ip;
+    my $ip  = $r->connection->remote_ip;
     my $mac = $args{mac};
 
-    unless ($mac        && $args{users} &&
-            $args{kbup} && $args{kbdown}) {
+    unless ( $mac
+        && defined $args{users}
+        && defined $args{kbup}
+        && defined $args{kbdown} )
+    {
 
-      $r->log->error("missing args for device at ip $ip");
-      return Apache2::Const::SERVER_ERROR;
+        $r->log->error("missing args for device at ip $ip");
+        return Apache2::Const::SERVER_ERROR;
     }
 
-    my ($router) = SL::Model::App->resultset('Router')->search({ macaddr => $mac });
+    my ($router) =
+      SL::Model::App->resultset('Router')->search( { macaddr => $mac } );
 
     unless ($router) {
-      $r->log->error("no router found with mac $mac");
-      return Apache2::Const::NOT_FOUND;
+        $r->log->error("no router found with mac $mac");
+        return Apache2::Const::NOT_FOUND;
     }
 
     # update the router time
     my $now = DateTime->now;
     $now->set_time_zone('local');
-    $router->last_ping(DateTime::Format::Pg->format_datetime($now));
+    $router->last_ping( DateTime::Format::Pg->format_datetime($now) );
 
     # update the ip
-    unless ($router->wan_ip eq $ip) {
+    unless ( $router->wan_ip eq $ip ) {
         $router->wan_ip($ip);
     }
 
     # update the latest seen users
-    $router->clients($args{users});
+    $router->clients( $args{users} );
 
     $router->update;
 
     # log the router entry
-    my $checkin = SL::Model::App->resultset('Checkin')->create({
-          router_id => $router->router_id,
-          memfree   => $args{memfree},
-          users     => $args{users},
-          kbup      => $args{kbup},
-          kbdown    => $args{kbdown},
-    });
-
-    $r->log->debug("new checkin entry for " . $router->router_id) if DEBUG;
-
-    my $top_users = $args{top_users};
-    $r->log->debug("processing user string $top_users") if DEBUG;
-    my @users = split(/\+/, $top_users);
-
-   shift(@users); # first part of split is blank
-   $r->log->debug("users: " . Dumper(\@users)) if DEBUG;
-
-
-    my %users;
-    foreach my $line (@users) {
-        my ($kbtotal, $kbdown, $kbup, $usermac, $hostname) = split(/\,/, $line);
-
-        my $usertrack = SL::Model::App->resultset('Usertrack')->create({
+    my $checkin = SL::Model::App->resultset('Checkin')->create(
+        {
             router_id => $router->router_id,
-            totalkb   => $kbtotal,
-            kbup      => $kbup,
-            kbdown    => $kbdown,
-            mac       => $usermac,
-        });
+            memfree   => $args{memfree},
+            users     => $args{users},
+            kbup      => $args{kbup},
+            kbdown    => $args{kbdown},
+        }
+    );
 
-        $r->log->debug("new checkin for user $usermac") if DEBUG;
+    $r->log->debug( "new checkin entry for " . $router->router_id ) if DEBUG;
 
+
+    # log user data if there is any
+    my $top_users = $args{top_users};
+    if ( defined $top_users ) {
+
+        $r->log->debug("processing user string $top_users") if DEBUG;
+        my @users = split( /\+/, $top_users );
+
+        shift(@users);    # first part of split is blank
+        $r->log->debug( "users: " . Dumper( \@users ) ) if DEBUG;
+
+        my %users;
+        foreach my $line (@users) {
+            my ( $kbtotal, $kbdown, $kbup, $usermac, $hostname ) =
+              split( /\,/, $line );
+
+            my $usertrack = SL::Model::App->resultset('Usertrack')->create(
+                {
+                    router_id => $router->router_id,
+                    totalkb   => $kbtotal,
+                    kbup      => $kbup,
+                    kbdown    => $kbdown,
+                    mac       => $usermac,
+                }
+            );
+
+            $r->log->debug("new checkin for user $usermac") if DEBUG;
+
+        }
     }
 
     $r->content_type('text/plain');
     $r->print("FOO");
     return Apache2::Const::OK;
 }
-
 
 1;
--- SL-Model/bin/sl_network_overview.pl
+++ SL-Model/bin/sl_network_overview.pl
@@ -17,11 +17,15 @@ my $yesterday = DateTime->now( time_zone => "local" )->subtract( hours => 24 );
 
 my $dbh = SL::Model->connect;
 
+# devices
 my $sql = <<'SQL';
-select checkin.kbup, checkin.kbdown,
+SELECT checkin.kbup, checkin.kbdown,
 account.account_id, checkin.cts, router.router_id
-from checkin, router, account where checkin.cts > '%s'
-and router.account_id = account.account_id and checkin.router_id = router.router_id order by cts desc
+FROM checkin, router, account
+WHERE checkin.cts > '%s'
+and router.account_id = account.account_id and
+checkin.router_id = router.router_id
+ORDER BY cts desc
 SQL
 
 $sql = sprintf( $sql, DateTime::Format::Pg->format_datetime($yesterday) );
@@ -34,7 +38,34 @@ my $now = DateTime->now( time_zone => "local" );
 foreach my $row (@$results) {
 
     # and group by router
-    push @{ $refined{ $row->{account_id} }{ $row->{router_id} } },
+    push @{ $refined{ $row->{account_id} }{routers}{ $row->{router_id} } },
+      {
+        kbup   => $row->{kbup},
+        kbdown => $row->{kbdown},
+        cts    => DateTime::Format::Pg->parse_datetime( $row->{cts} ),
+      };
+}
+
+# users
+$sql = <<'SQL';
+SELECT account.account_id, usertrack.mac, usertrack.cts,
+usertrack.kbup, usertrack.kbdown
+FROM usertrack, account, router
+WHERE
+router.account_id = account.account_id
+AND usertrack.router_id = router.router_id
+AND  usertrack.cts > '%s'
+ORDER BY usertrack.cts DESC
+SQL
+
+$sql = sprintf( $sql, DateTime::Format::Pg->format_datetime($yesterday) );
+
+my $users = $dbh->selectall_arrayref( $sql, { Slice => {} } );
+
+foreach my $row (@$users) {
+
+    # and group by router
+    push @{ $refined{ $row->{account_id} }{users}{ $row->{mac} } },
       {
         kbup   => $row->{kbup},
         kbdown => $row->{kbdown},
@@ -44,81 +75,116 @@ foreach my $row (@$results) {
 
 foreach my $account_id ( keys %refined ) {
 
-
     # every 15 minutes for 24 hours is 4*24 = 96 - time, kbdown, kbup
     # setup an array to hold the data
 
     my @array;
-    for (1..24*12) { # 5 minutes
-      push @array, [ $now->clone->subtract( minutes => 5 * $_ ), 0, 0 ];
+    for ( 1 .. 24 * 12 ) {    # 5 minutes
+        push @array,
+          [ $now->clone->subtract( minutes => 5 * $_ - 1 ), 0, 0, 0 ];
     }
 
-    foreach my $router_id ( keys %{ $refined{$account_id} } ) {
+    # aggregate the router data
+    foreach my $router_id ( keys %{ $refined{$account_id}{routers} } ) {
 
         # loop over the data
-#        $DB::single = 1;
-        my (%last_row, %placeholder);
+        #        $DB::single = 1;
+        my ( %last_row, %placeholder );
         foreach my $row ( sort { $a->{cts}->epoch <=> $b->{cts}->epoch }
-                          @{ $refined{$account_id}{$router_id} } )
+            @{ $refined{$account_id}{routers}{$router_id} } )
         {
 
             # handle first row
-            unless (keys %last_row) {
-                %last_row = ( kbup => $row->{kbup},
-                              kbdown => $row->{kbdown} );
-              }
+            unless ( keys %last_row ) {
+                %last_row = (
+                    kbup   => $row->{kbup},
+                    kbdown => $row->{kbdown}
+                );
+            }
 
-            %placeholder  = ( kbup => $row->{kbup},
-                              kbdown => $row->{kbdown} );
+            %placeholder = (
+                kbup   => $row->{kbup},
+                kbdown => $row->{kbdown}
+            );
 
             # get the difference
-            $row->{kbup}   -= $last_row{kbup};
-            $row->{kbdown} -= $last_row{kbdown};
-            warn(sprintf("kbup %s, kbdown %s", $row->{kbup}, $row->{kbdown}))
+            unless ( $row->{kbup} == 0 && $row->{kbdown} == 0 ) {
+                $row->{kbup}   -= $last_row{kbup};
+                $row->{kbdown} -= $last_row{kbdown};
+            }
+
+            warn(
+                sprintf( "kbup %s, kbdown %s", $row->{kbup}, $row->{kbdown} ) )
               if DEBUG;
 
             # at this point we're processing  time based data for $router_id.
             # add the totals to to the array in the correct time slot.
             # loop over the output @array until the row fits the slot.
-            for (my $i = 0; $i <= $#array; $i++) {
-
+            for ( my $i = 0 ; $i <= $#array ; $i++ ) {
 
                 # is this timestamp less than the next element?  That's a match
-                if ( DateTime->compare( $row->{cts}, $array[ $i + 1 ]->[0] ) >
-                    0 )
-                {
+                if ( DateTime->compare( $row->{cts}, $array[$i]->[0] ) > 0 ) {
 
                     # then log it on the current element
-                    $array[$i]->[1] += sprintf("%2.2f", $row->{kbdown}/1024);
-                    $array[$i]->[2] += sprintf("%2.2f", $row->{kbup}/1024);
-                    last;  # last $row
+                    $array[$i]->[1] +=
+                      sprintf( "%2.2f", $row->{kbdown} / 1024 );
+                    $array[$i]->[2] += sprintf( "%2.2f", $row->{kbup} / 1024 );
+                    last;    # last $row
                 }
-
 
             }
 
-
             %last_row = %placeholder;
-          }
+        }
+
+    }
+
+    # aggregate the user data
+    foreach my $mac ( keys %{ $refined{$account_id}{users} } ) {
+
+        # loop over the data
+        #        $DB::single = 1;
+        my ( %last_row, %placeholder );
+        foreach my $row ( sort { $b->{cts}->epoch <=> $a->{cts}->epoch }
+            @{ $refined{$account_id}{users}{$mac} } )
+        {
+
+            # at this point we're processing time based data for $router_id.
+            # add the totals to to the array in the correct time slot.
+            # loop over the output @array until the row fits the slot.
+            for ( my $i = 0 ; $i <= $#array ; $i++ ) {
+
+                # is this timestamp less than the next element?  That's a match
+                if ( DateTime->compare( $row->{cts}, $array[$i]->[0] ) > 0 ) {
+
+                    # then log it on the current element
+                    $array[$i]->[3]++;
+                    last;    # last $row
+                }
+
+            }
+
+        }
 
     }
 
     # reinitialize the array
-#    $DB::single = 1;
+    #    $DB::single = 1;
     $_->[0] = $_->[0]->strftime("%l:%M %p") for @array;
-    warn("array is " . Dumper(\@array)) if DEBUG;
-    my ($account) = SL::Model::App->resultset('Account')->search({
-        account_id => $account_id });
+    warn( "array is " . Dumper( \@array ) ) if DEBUG;
+    my ($account) =
+      SL::Model::App->resultset('Account')
+      ->search( { account_id => $account_id } );
 
     die "missing account for account $account_id" unless $account;
 
     my $filename =
-          join ( '/', $account->report_dir_base, "network_overview.csv" );
+      join( '/', $account->report_dir_base, "network_overview.csv" );
 
     my $fh;
-    open($fh, '>', $filename) or die "could not open $filename: " . $!;
+    open( $fh, '>', $filename ) or die "could not open $filename: " . $!;
     foreach my $line (@array) {
-        print $fh join(',',@{$line}) . "\n";
+        print $fh join( ',', @{$line} ) . "\n";
     }
     close $fh or die $!;
 
