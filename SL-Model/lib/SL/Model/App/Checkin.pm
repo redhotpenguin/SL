b1ebf54ee77d53f04678be42ca55f747d29f3bf5
Fred Moyer <fred@redhotpenguin.com>
2009-09-11 06:00:05 +0000

    Database updates
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@3153 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Model/lib/SL/Model/App/Checkin.pm
+++ SL-Model/lib/SL/Model/App/Checkin.pm
@@ -39,6 +39,26 @@ __PACKAGE__->add_columns(
     is_nullable => 1,
     size => 8,
   },
+  "ping_ms",
+  { data_type => "integer", default_value => 0, is_nullable => 0, size => 4 },
+  "speed_kbytes",
+  { data_type => "integer", default_value => 0, is_nullable => 0, size => 4 },
+  "hops",
+  { data_type => "integer", default_value => 0, is_nullable => 0, size => 4 },
+  "nodes",
+  {
+    data_type => "text",
+    default_value => "''::text",
+    is_nullable => 0,
+    size => undef,
+  },
+  "nodes_rssi",
+  {
+    data_type => "text",
+    default_value => "''::text",
+    is_nullable => 0,
+    size => undef,
+  },
 );
 __PACKAGE__->set_primary_key("checkin_id");
 __PACKAGE__->belongs_to(
@@ -48,8 +68,17 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.04999_08 @ 2009-09-01 13:55:06
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:TY3SkhVpWOcT8BTDfiSKiw
+# Created by DBIx::Class::Schema::Loader v0.04999_08 @ 2009-09-10 23:29:29
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:7PcWA4fsrJhQ+P56qK+2CQ
+# These lines were loaded from '/home/phred/dev/perl/lib/site_perl/5.8.9/SL/Model/App/Checkin.pm' found in @INC.
+# They are now part of the custom portion of this file
+# for you to hand-edit.  If you do not either delete
+# this section or remove that file from @INC, this section
+# will be repeated redundantly when you re-create this
+# file again via Loader!
+
+
+# End of lines loaded from '/home/phred/dev/perl/lib/site_perl/5.8.9/SL/Model/App/Checkin.pm' 
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
