7eefcda8a5e449e145445e00780d514fd4abe7de
Fred Moyer <fred@redhotpenguin.com>
2009-11-07 19:05:59 +0000

    spaces to tabs
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@3284 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Kernel/linux-2.6.26.8/net/ipv4/netfilter/nf_nat_sl.c
+++ SL-Kernel/linux-2.6.26.8/net/ipv4/netfilter/nf_nat_sl.c
@@ -42,250 +42,255 @@ static char xslr[XSLR_LEN+1] = "X-SLR";
 /* removes :8135 from the host name */
 
 static int sl_remove_port(struct sk_buff *skb,
-                          struct nf_conn *ct,
-                          enum   ip_conntrack_info ctinfo,
-                          unsigned int host_offset,
-                          unsigned int dataoff,
-                          unsigned int datalen,
-                          unsigned int end_of_host,
-                          unsigned char *user_data)
+						  struct nf_conn *ct,
+						  enum   ip_conntrack_info ctinfo,
+						  unsigned int host_offset,
+						  unsigned int dataoff,
+						  unsigned int datalen,
+						  unsigned int end_of_host,
+						  unsigned char *user_data)
 {
-    
-    if (strncmp(search[PORT].string, 
-                &user_data[end_of_host-search[PORT].len+search[NEWLINE].len],
-                search[PORT].len)) {
+	
+	if (strncmp(search[PORT].string, 
+				&user_data[end_of_host-search[PORT].len+search[NEWLINE].len],
+				search[PORT].len)) {
 
 #ifdef SL_DEBUG
-        printk(KERN_DEBUG "no port rewrite found in packet strncmp\n");
-        printk(KERN_DEBUG "end of host packet dump:\n%s\n",
-               (unsigned char *)((unsigned int)user_data+end_of_host-search[PORT].len+search[NEWLINE].len));
+		printk(KERN_DEBUG "no port rewrite found in packet strncmp\n");
+		printk(KERN_DEBUG "end of host packet dump:\n%s\n",
+			   (unsigned char *)((unsigned int)user_data+end_of_host-search[PORT].len+search[NEWLINE].len));
 #endif
-	return 0;
-    }
+		return 0;
+	}
 
 
 #ifdef SL_DEBUG
-    printk(KERN_DEBUG "remove_port found a port at offset %u\n",
+	printk(KERN_DEBUG "remove_port found a port at offset %u\n",
 	end_of_host-search[PORT].len+search[NEWLINE].len );
 #endif
 
-    /* remove the port */
-    if (!nf_nat_mangle_tcp_packet(skb, ct, ctinfo,
-        end_of_host-search[PORT].len+search[NEWLINE].len,
-        search[PORT].len-(search[NEWLINE].len*2), // subtract \r\n
-	NULL,
-	0))
-    {
-        printk(KERN_ERR "unable to remove port needle\n");
+	/* remove the port */
+	if (!nf_nat_mangle_tcp_packet(
+			skb,
+			ct,
+			ctinfo,
+			end_of_host-search[PORT].len+search[NEWLINE].len,
+			search[PORT].len-(search[NEWLINE].len*2), // subtract \r\n
+		NULL,0))
+	{
+		printk(KERN_ERR "unable to remove port needle\n");
 	// we've already found the port, so we return 1 regardless
-        return 1;
-    }
+		return 1;
+	}
 
 #ifdef SL_DEBUG
-    printk(KERN_DEBUG "port removed ok, new packet\n%s\n",
+	printk(KERN_DEBUG "port removed ok, new packet\n%s\n",
 	(unsigned char *)user_data);
 
 #endif
 
-    return 1; 
+	return 1; 
 }
 
 
 static unsigned int add_sl_header(struct sk_buff *skb,
-                                  struct nf_conn *ct, 
-                                  enum ip_conntrack_info ctinfo,
-				  unsigned int host_offset, 
-				  unsigned int dataoff, 
-				  unsigned int datalen,
-				  unsigned int end_of_host,
-				  unsigned char *user_data)
-{                      
-       
-    /* first make sure there is room */
-    if ( skb->len >= ( MAX_PACKET_LEN - SL_HEADER_LEN ) ) {
+								  struct nf_conn *ct, 
+								  enum ip_conntrack_info ctinfo,
+	  unsigned int host_offset, 
+	  unsigned int dataoff, 
+	  unsigned int datalen,
+	  unsigned int end_of_host,
+	  unsigned char *user_data)
+{					  
+	   
+	/* first make sure there is room */
+	if ( skb->len >= ( MAX_PACKET_LEN - SL_HEADER_LEN ) ) {
 
 #ifdef SL_DEBUG
-        printk(KERN_DEBUG "\nskb too big, length: %d\n", (skb->len));
+		printk(KERN_DEBUG "\nskb too big, length: %d\n", (skb->len));
 #endif
-        return 0;
-    }
+		return 0;
+	}
 
 
-    /* next make sure an x-slr header is not already present */
-    if (!strncmp(xslr,(unsigned char *)((unsigned int)user_data+end_of_host+1),
+	/* next make sure an x-slr header is not already present */
+	if (!strncmp(xslr,(unsigned char *)((unsigned int)user_data+end_of_host+1),
 		 XSLR_LEN)) {
 #ifdef SL_DEBUG
-    	printk(KERN_DEBUG "\npkt x-slr already present\n");
+		printk(KERN_DEBUG "\npkt x-slr already present\n");
 #endif
-    	return 0;
-    }
+		return 0;
+	}
 
 #ifdef SL_DEBUG
-        printk(KERN_DEBUG "\nno x-slr header present, adding\n");
+	printk(KERN_DEBUG "\nno x-slr header present, adding\n");
 #endif
 
-    {
-    unsigned int jhashed, slheader_len;
-    char slheader[SL_HEADER_LEN];
-    char src_string[MACADDR_SIZE];
-    unsigned char *pSrc_string = src_string;
-    struct ethhdr *bigmac = eth_hdr(skb);
-    unsigned char *pHsource = bigmac->h_source;
+	{
+	unsigned int jhashed, slheader_len;
+	char slheader[SL_HEADER_LEN];
+	char src_string[MACADDR_SIZE];
+	unsigned char *pSrc_string = src_string;
+	struct ethhdr *bigmac = eth_hdr(skb);
+	unsigned char *pHsource = bigmac->h_source;
  	int i = 0;
 
-    /* convert the six octects into a hex string via bitmask and bitshift */
-    while (i<6)
+	/* convert the six octet mac source address into a hex  string
+	   via bitmask and bitshift on each octet */
+	while (i<6)
 	{
 
-        *(pSrc_string++) = int2Hex[(*pHsource)>>4];
-        *(pSrc_string++) = int2Hex[(*pHsource)&0x0f];
+		*(pSrc_string++) = int2Hex[(*pHsource)>>4];
+		*(pSrc_string++) = int2Hex[(*pHsource)&0x0f];
 
-        pHsource++;
-        i++;
-    }
-    *pSrc_string = '\0';
+		pHsource++;
+		i++;
+	}
+
+	/* null terminate it just to be safe */
+	*pSrc_string = '\0';
 
 #ifdef SL_DEBUG
-    printk(KERN_DEBUG "\nsrc macaddr %s\n", src_string);
-#endif        
+	printk(KERN_DEBUG "\nsrc macaddr %s\n", src_string);
+#endif		
 
-    /********************************************/
-    /* create the http header */
-    /* jenkins hash obfuscation of source mac */
-    jhashed = jhash((void *)src_string, MACADDR_SIZE, JHASH_SALT);
+	/********************************************/
+	/* create the http header */
+	/* jenkins hash obfuscation of source mac */
+	jhashed = jhash((void *)src_string, MACADDR_SIZE, JHASH_SALT);
 
-    /* create the X-SLR Header */        
-    slheader_len = sprintf(slheader, "X-SLR: %08x|%s\r\n", jhashed, sl_device);
+	/* create the X-SLR Header */		
+	slheader_len = sprintf(slheader, "X-SLR: %08x|%s\r\n", jhashed, sl_device);
 
-    /* handle sprintf failure */
+	/* handle sprintf failure */
    if (slheader_len != SL_HEADER_LEN) {
-        printk(KERN_ERR "exp header %s len %d doesnt match calc len %d\n",
-               (char *)slheader, SL_HEADER_LEN, slheader_len );
-        return 0;
-    }
+		printk(KERN_ERR "exp header %s len %d doesnt match calc len %d\n",
+			   (char *)slheader, SL_HEADER_LEN, slheader_len );
+		return 0;
+	}
 
 #ifdef SL_DEBUG
-    printk(KERN_DEBUG "xslr %s, len %d\n", slheader, slheader_len);
-#endif        
+	printk(KERN_DEBUG "xslr %s, len %d\n", slheader, slheader_len);
+#endif		
 
 
-    /********************************************/
-    /* insert the slheader into the http headers */
-    if (!nf_nat_mangle_tcp_packet( skb,
-                                   ct, 
-                                   ctinfo,
-                                   end_of_host + search[NEWLINE].len,
-                                   0, 
-                                   slheader,
-                                   slheader_len)) {  
+	/********************************************/
+	/* insert the slheader into the http headers */
+	if (!nf_nat_mangle_tcp_packet( skb,
+								   ct, 
+								   ctinfo,
+								   end_of_host + search[NEWLINE].len,
+								   0, 
+								   slheader,
+								   slheader_len)) {  
 
-        printk(KERN_ERR " failed to mangle packet\n");
-        return 0;
-    }
+		printk(KERN_ERR " failed to mangle packet\n");
+		return 0;
+	}
 
 #ifdef SL_DEBUG
-        printk(KERN_DEBUG "packet mangled ok:\n%s\n",
+		printk(KERN_DEBUG "packet mangled ok:\n%s\n",
 		(unsigned char *)((unsigned int)user_data));
-#endif        
+#endif		
 
-    return 1;
-    }
+	return 1;
+	}
 }
 
 
 /* So, this packet has hit the connection tracking matching code.
    Mangle it, and change the expectation to match the new version. */
 static unsigned int nf_nat_sl(struct sk_buff *skb,
-                              enum ip_conntrack_info ctinfo,
-                              struct nf_conntrack_expect *exp,
-                              unsigned int host_offset,
-                              unsigned int dataoff,
-                              unsigned int datalen,
-			      unsigned char *user_data)
+							  enum ip_conntrack_info ctinfo,
+							  struct nf_conntrack_expect *exp,
+							  unsigned int host_offset,
+							  unsigned int dataoff,
+							  unsigned int datalen,
+		 unsigned char *user_data)
 {
-    struct nf_conn *ct = exp->master;
-    struct iphdr *iph = ip_hdr(skb);
-    unsigned int port_status = 0;
-    unsigned int end_of_host;
-    char dest_ip[16];
+	struct nf_conn *ct = exp->master;
+	struct iphdr *iph = ip_hdr(skb);
+	unsigned int port_status = 0;
+	unsigned int end_of_host;
+	char dest_ip[16];
 
 #ifdef SL_DEBUG
-    printk(KERN_DEBUG "\nhere is the proxy ip %s\n", sl_proxy);
-    printk(KERN_DEBUG "\nsource %u.%u.%u.%u, dest %u.%u.%u.%u\n",
-			 NIPQUAD(iph->saddr), NIPQUAD(iph->daddr));
+	printk(KERN_DEBUG "\nhere is the proxy ip %s\n", sl_proxy);
+	printk(KERN_DEBUG "\nsource %u.%u.%u.%u, dest %u.%u.%u.%u\n",
+	NIPQUAD(iph->saddr), NIPQUAD(iph->daddr));
 #endif
 
-    // make sure we have an end of host header
-    // scan to the end of the host header
-    end_of_host = host_offset;
-    while ( ++(end_of_host) < (host_offset+HOST_SEARCH_LEN) ) {
+	// make sure we have an end of host header
+	// scan to the end of the host header
+	end_of_host = host_offset;
+	while ( ++(end_of_host) < (host_offset+HOST_SEARCH_LEN) ) {
 	if (!strncmp(search[NEWLINE].string, &user_data[end_of_host],
 		search[NEWLINE].len))
-	    break;
-    } 
+	 break;
+	} 
 
-    if (end_of_host == (host_offset+HOST_SEARCH_LEN-1)) {
+	if (end_of_host == (host_offset+HOST_SEARCH_LEN-1)) {
 	// host header is split between two packets?
-        printk(KERN_ERR "\nend of host not found in search\n");
-        return NF_ACCEPT;
-    }
+		printk(KERN_ERR "\nend of host not found in search\n");
+		return NF_ACCEPT;
+	}
 
 #ifdef SL_DEBUG
-    printk(KERN_DEBUG "\nfound end_of_host %u\n", end_of_host);
-    printk(KERN_DEBUG "\npacket dump:\n%s\n",
+	printk(KERN_DEBUG "\nfound end_of_host %u\n", end_of_host);
+	printk(KERN_DEBUG "\npacket dump:\n%s\n",
 		(unsigned char *)((unsigned int)user_data+end_of_host));
-#endif        
+#endif		
 
-    // if it isn't destined for the proxy, try to remove the port
-    sprintf(dest_ip, "%u.%u.%u.%u", NIPQUAD(iph->daddr));
-    if (strcmp(sl_proxy, dest_ip)) {
-
-#ifdef SL_DEBUG
-      printk(KERN_DEBUG "\nsl_proxy %s, dest %s no match, checking port\n",
-	     sl_proxy, dest_ip);
-#endif
-
-      /* look for a port rewrite and remove it if exists */
-      port_status = sl_remove_port(skb, ct, ctinfo, 
- 		       host_offset, dataoff, datalen,
-  		       end_of_host, user_data );
+	// if it isn't destined for the proxy, try to remove the port
+	sprintf(dest_ip, "%u.%u.%u.%u", NIPQUAD(iph->daddr));
+	if (strcmp(sl_proxy, dest_ip)) {
 
 #ifdef SL_DEBUG
-      printk(KERN_DEBUG "\nport status: %d\n\n", port_status);
+	  printk(KERN_DEBUG "\nsl_proxy %s, dest %s no match, checking port\n",
+	  sl_proxy, dest_ip);
 #endif
 
-
-      if (port_status) {
+	  /* look for a port rewrite and remove it if exists */
+	  port_status = sl_remove_port(skb, ct, ctinfo, 
+		  host_offset, dataoff, datalen,
+		   end_of_host, user_data );
 
 #ifdef SL_DEBUG
-          printk(KERN_DEBUG "\nport rewrite removed :8135 successfully\n\n");
+	  printk(KERN_DEBUG "\nport status: %d\n\n", port_status);
 #endif
-          return NF_ACCEPT;
-      }
 
-    }
+
+	  if (port_status) {
 
 #ifdef SL_DEBUG
-    printk(KERN_DEBUG "\nsl_proxy %s, dest_ip %s match\n", sl_proxy, dest_ip);
+		  printk(KERN_DEBUG "\nport rewrite removed :8135 successfully\n\n");
 #endif
+		  return NF_ACCEPT;
+	  }
 
-    /* attempt to insert the X-SLR header, since this is sl destined */
-    if (!add_sl_header(skb, 
-                       ct, 
-                       ctinfo, 
-                       host_offset, 
-                       dataoff, 
-                       datalen,
-		       end_of_host,
-		       user_data))
-    {
+	}
 
 #ifdef SL_DEBUG
-        printk(KERN_ERR "\nadd_sl_header returned not added\n");
+	printk(KERN_DEBUG "\nsl_proxy %s, dest_ip %s match\n", sl_proxy, dest_ip);
 #endif
-    }
 
-    return NF_ACCEPT;
+	/* attempt to insert the X-SLR header, since this is sl destined */
+	if (!add_sl_header(skb, 
+					   ct, 
+					   ctinfo, 
+					   host_offset, 
+					   dataoff, 
+					   datalen,
+		 end_of_host,
+		 user_data))
+	{
+
+#ifdef SL_DEBUG
+		printk(KERN_ERR "\nadd_sl_header returned not added\n");
+#endif
+	}
+
+	return NF_ACCEPT;
 }
 
 
@@ -301,8 +306,8 @@ static int __init nf_nat_sl_init(void)
 	rcu_assign_pointer(nf_nat_sl_hook, nf_nat_sl);
 
 #ifdef SL_DEBUG
-    printk(KERN_DEBUG "nf_nat_sl starting, proxy %s, device %s\n",
-        sl_proxy, sl_device);
+	printk(KERN_DEBUG "nf_nat_sl starting, proxy %s, device %s\n",
+		sl_proxy, sl_device);
 #endif
 
 	return 0;
--- SL-Kernel/linux-2.6.26.8/net/netfilter/nf_conntrack_sl.c
+++ SL-Kernel/linux-2.6.26.8/net/netfilter/nf_conntrack_sl.c
@@ -22,214 +22,214 @@ static char get[GET_LEN+1] = "GET /";
 static char host[HOST_LEN+1] = "Host: ";
 
 unsigned int (*nf_nat_sl_hook)(struct sk_buff *skb,
-                               enum ip_conntrack_info ctinfo,
-                               struct nf_conntrack_expect *exp,
-                               unsigned int host_offset,
-                               unsigned int data_offset,
-                               unsigned int datalen,
-                               unsigned char *user_data )
-                               __read_mostly;
+							   enum ip_conntrack_info ctinfo,
+							   struct nf_conntrack_expect *exp,
+							   unsigned int host_offset,
+							   unsigned int data_offset,
+							   unsigned int datalen,
+							   unsigned char *user_data )
+							   __read_mostly;
 EXPORT_SYMBOL_GPL(nf_nat_sl_hook);
 
 
 static int sl_help (struct sk_buff *skb,
-                    unsigned int protoff,
-                    struct nf_conn *ct,
-                    enum   ip_conntrack_info ctinfo)     
+					unsigned int protoff,
+					struct nf_conn *ct,
+					enum   ip_conntrack_info ctinfo)	 
 {
-    struct tcphdr _tcph, *th;
-    unsigned int dataoff, datalen;
-    struct nf_conntrack_expect *exp;
-    int ret = NF_ACCEPT;
-    typeof(nf_nat_sl_hook) nf_nat_sl;
+	struct tcphdr _tcph, *th;
+	unsigned int dataoff, datalen;
+	struct nf_conntrack_expect *exp;
+	int ret = NF_ACCEPT;
+	typeof(nf_nat_sl_hook) nf_nat_sl;
 
-    /* only operate on established connections */
-    if (ctinfo != IP_CT_ESTABLISHED
-         && ctinfo != IP_CT_ESTABLISHED+IP_CT_IS_REPLY)
-        return NF_ACCEPT;
+	/* only operate on established connections */
+	if (ctinfo != IP_CT_ESTABLISHED
+		 && ctinfo != IP_CT_ESTABLISHED+IP_CT_IS_REPLY)
+		return NF_ACCEPT;
 
-    /* only mangle outbound packets */
-    if ( ctinfo == IP_CT_IS_REPLY )
-        return NF_ACCEPT;
+	/* only mangle outbound packets */
+	if ( ctinfo == IP_CT_IS_REPLY )
+		return NF_ACCEPT;
 
-    /* No NAT? */
-    if (!(ct->status & IPS_NAT_MASK))
-      return NF_ACCEPT;
+	/* No NAT? */
+	if (!(ct->status & IPS_NAT_MASK))
+	  return NF_ACCEPT;
 
-#ifdef SKB_DEBUG                
-    printk(KERN_DEBUG "conntrackinfo = %u\n", ctinfo);
-#endif    
+#ifdef SKB_DEBUG				
+	printk(KERN_DEBUG "conntrackinfo = %u\n", ctinfo);
+#endif	
 
-    /* not a full tcp header */
-    th = skb_header_pointer(skb, protoff, sizeof(_tcph), &_tcph);
-    if (th == NULL)
-        return NF_ACCEPT;
-    
+	/* not a full tcp header */
+	th = skb_header_pointer(skb, protoff, sizeof(_tcph), &_tcph);
+	if (th == NULL)
+		return NF_ACCEPT;
+	
 #ifdef SKB_DEBUG
 
-    printk(KERN_DEBUG "tcphdr dst %d, src %d, ack seq %u\n",
-           ntohs(th->dest), ntohs(th->source), th->ack_seq);
+	printk(KERN_DEBUG "tcphdr dst %d, src %d, ack seq %u\n",
+		   ntohs(th->dest), ntohs(th->source), th->ack_seq);
 
-    /* let SYN, FIN, RST, PSH, ACK, ECE, CWR, URG packets pass */
-        printk(KERN_DEBUG "FIN %d, SYN %d, RST %d, PSH %d, ACK %d, ECE %d\n",
-         th->fin, th->syn, th->rst, th->psh, th->ack, th->ece);
-#endif    
+	/* let SYN, FIN, RST, PSH, ACK, ECE, CWR, URG packets pass */
+		printk(KERN_DEBUG "FIN %d, SYN %d, RST %d, PSH %d, ACK %d, ECE %d\n",
+		 th->fin, th->syn, th->rst, th->psh, th->ack, th->ece);
+#endif	
 
-    /* only work on push or ack packets */
-    if (!( (th->psh == 1) || (th->ack == 1)) ) {
+	/* only work on push or ack packets */
+	if (!( (th->psh == 1) || (th->ack == 1)) ) {
 #ifdef SKB_DEBUG
-    	printk(KERN_DEBUG "not psh or ack, return\n\n");
-#endif    
-        return NF_ACCEPT;
-    }
+		printk(KERN_DEBUG "not psh or ack, return\n\n");
+#endif	
+		return NF_ACCEPT;
+	}
 
-    /* get tcp data offset */
-    dataoff = protoff + th->doff*4;
-    if (dataoff >= skb->len) {
+	/* get tcp data offset */
+	dataoff = protoff + th->doff*4;
+	if (dataoff >= skb->len) {
 
 #ifdef SKB_DEBUG
 	printk(KERN_DEBUG "dataoff(%u) >= skblen(%u), return\n\n", dataoff,
-			 skb->len);
+	skb->len);
 #endif
-        return NF_ACCEPT;
-    }
+		return NF_ACCEPT;
+	}
 
-    /* get tcp data length */
-    datalen = skb->len - dataoff;
+	/* get tcp data length */
+	datalen = skb->len - dataoff;
 
-    /* if not MIN_PACKET_LEN we aren't interested */
-    if (datalen < MIN_PACKET_LEN) {
+	/* if not MIN_PACKET_LEN we aren't interested */
+	if (datalen < MIN_PACKET_LEN) {
 #ifdef SKB_DEBUG
-    	printk(KERN_DEBUG "skb data too small,  %d bytes, return\n", datalen);
-#endif    
-        return NF_ACCEPT;
-    }
+		printk(KERN_DEBUG "skb data too small,  %d bytes, return\n", datalen);
+#endif	
+		return NF_ACCEPT;
+	}
 
 
 #ifdef SL_DEBUG
-    printk(KERN_DEBUG "dataoff %u, packet length %d, data length %d\n",
-        dataoff, skb->len, datalen);
-#endif    
+	printk(KERN_DEBUG "dataoff %u, packet length %d, data length %d\n",
+		dataoff, skb->len, datalen);
+#endif	
 
 
-    {
-        
-        unsigned char *user_data;
-        int j=0;
-        unsigned int start_offset, stop_offset, host_offset;
+	{
+		
+		unsigned char *user_data;
+		int j=0;
+		unsigned int start_offset, stop_offset, host_offset;
 
-        /* see if this is a GET request */
-        user_data = (void *)th + th->doff*4;
+		/* see if this is a GET request */
+		user_data = (void *)th + th->doff*4;
 
-        /* look for 'GET /' */
-        if (strncmp(get, user_data, GET_LEN)) {    
-
-#ifdef SL_DEBUG
-            printk(KERN_DEBUG "no get_needle found in packet, return\n\n");
-#endif  	      
-
-            return NF_ACCEPT;
-        } 
-
-        /* allocate space for an expectation */
-        exp = nf_ct_expect_alloc(ct);
-        if (exp == NULL)
-            return NF_ACCEPT;
-
-        datalen = skb->len - dataoff;
-
-        start_offset = GET_LEN;
-        stop_offset = datalen - HOST_LEN - start_offset;
-
+		/* look for 'GET /' */
+		if (strncmp(get, user_data, GET_LEN)) {	
 
 #ifdef SL_DEBUG
-        printk(KERN_DEBUG "packet dump:\n%s\n\n", user_data);
+			printk(KERN_DEBUG "no get_needle found in packet, return\n\n");
+#endif		 
 
-        // see if the packet contains a Host header
-        printk(KERN_DEBUG "\ndataoff %u, user_data %u\n",
-	    dataoff, (unsigned int)user_data );
-    
-        printk(KERN_DEBUG "host search:  search_start %u, search_stop %u\n",
-        start_offset, stop_offset );
+			return NF_ACCEPT;
+		} 
 
-        if (start_offset > stop_offset) {
-            printk(KERN_ERR "invalid stop offset, return\n");
-            return NF_ACCEPT;
-        }
+		/* allocate space for an expectation */
+		exp = nf_ct_expect_alloc(ct);
+		if (exp == NULL)
+			return NF_ACCEPT;
+
+		datalen = skb->len - dataoff;
+
+		start_offset = GET_LEN;
+		stop_offset = datalen - HOST_LEN - start_offset;
+
+
+#ifdef SL_DEBUG
+		printk(KERN_DEBUG "packet dump:\n%s\n\n", user_data);
+
+		// see if the packet contains a Host header
+		printk(KERN_DEBUG "\ndataoff %u, user_data %u\n",
+	 dataoff, (unsigned int)user_data );
+	
+		printk(KERN_DEBUG "host search:  search_start %u, search_stop %u\n",
+		start_offset, stop_offset );
+
+		if (start_offset > stop_offset) {
+			printk(KERN_ERR "invalid stop offset, return\n");
+			return NF_ACCEPT;
+		}
 #endif
 
-        /* search for a host header */
-        while ( start_offset++ < stop_offset) {
+		/* search for a host header */
+		while ( start_offset++ < stop_offset) {
 
-            if ( !memcmp(&user_data[start_offset], &host[j], 1 )) {
+			if ( !memcmp(&user_data[start_offset], &host[j], 1 )) {
 
 #ifdef SL_DEBUG
-                printk(KERN_DEBUG "found match i %d, j %d\n", start_offset, j);
+				printk(KERN_DEBUG "found match i %d, j %d\n", start_offset, j);
 #endif
 
-                if (j == HOST_LEN-1) {
+				if (j == HOST_LEN-1) {
 
 #ifdef SL_DEBUG
-                    printk(KERN_DEBUG "FULL MATCH AT i %d, j %d\n", start_offset+HOST_LEN+GET_LEN+1, j);
-                    printk(KERN_DEBUG "match packet dump:\n%s\n", &user_data[start_offset+1]);
+					printk(KERN_DEBUG "FULL MATCH AT i %d, j %d\n", start_offset+HOST_LEN+GET_LEN+1, j);
+					printk(KERN_DEBUG "match packet dump:\n%s\n", &user_data[start_offset+1]);
 #endif
 
-                    break;
-                }
-                j++;
-            } else {
-                j = 0;
+					break;
+				}
+				j++;
+			} else {
+				j = 0;
 
-            }
+			}
 
-        }
+		}
 
-        if (j != HOST_LEN-1) {
+		if (j != HOST_LEN-1) {
 #ifdef SL_DEBUG
-            printk("no host header found, j is %d, start_offset is %d, max is %d\n", j, start_offset, datalen-start_offset -HOST_LEN);
+			printk("no host header found, j is %d, start_offset is %d, max is %d\n", j, start_offset, datalen-start_offset -HOST_LEN);
 #endif
-            return NF_ACCEPT;
-        }
-        host_offset = start_offset;
+			return NF_ACCEPT;
+		}
+		host_offset = start_offset;
 
 #ifdef SL_DEBUG
 
-        printk(KERN_DEBUG "packet dump start offset:\n%s\n",
-               (unsigned char *)((unsigned int)user_data+ start_offset));
+		printk(KERN_DEBUG "packet dump start offset:\n%s\n",
+			   (unsigned char *)((unsigned int)user_data+ start_offset));
 
-        printk(KERN_DEBUG "packet dump stop offset:\n%s\n",
-               (unsigned char *)((unsigned int)user_data+ stop_offset-10));
+		printk(KERN_DEBUG "packet dump stop offset:\n%s\n",
+			   (unsigned char *)((unsigned int)user_data+ stop_offset-10));
 
 
-        printk(KERN_DEBUG "passing packet to nat module, host offset: %u\n", host_offset);
-        printk(KERN_DEBUG "packet dump:\n%s\n",
-               (unsigned char *)((unsigned int)user_data+host_offset));
+		printk(KERN_DEBUG "passing packet to nat module, host offset: %u\n", host_offset);
+		printk(KERN_DEBUG "packet dump:\n%s\n",
+			   (unsigned char *)((unsigned int)user_data+host_offset));
 #endif
  	
-        nf_nat_sl = rcu_dereference(nf_nat_sl_hook);
-        ret = nf_nat_sl(skb, ctinfo, exp,
-                        host_offset, dataoff, datalen, user_data);
+		nf_nat_sl = rcu_dereference(nf_nat_sl_hook);
+		ret = nf_nat_sl(skb, ctinfo, exp,
+						host_offset, dataoff, datalen, user_data);
    
-        return ret;
+		return ret;
 
-    }
+	}
 
 }
 
 
 static const struct nf_conntrack_expect_policy sl_exp_policy = {
 	.max_expected	= 0,
-	.timeout        = 60,
+	.timeout		= 60,
 };
 
 static struct nf_conntrack_helper sl_helper __read_mostly = {
-		.name                     = "sl",
-		.tuple.src.l3num          = AF_INET,
-		.tuple.dst.protonum       = IPPROTO_TCP,
-		.tuple.src.u.tcp.port     = __constant_htons(SL_PORT),
-		.me                       = THIS_MODULE,
-		.help                     = sl_help,
-		.expect_policy            = &sl_exp_policy,
+	.name					 = "sl",
+	.tuple.src.l3num		  = AF_INET,
+	.tuple.dst.protonum	   = IPPROTO_TCP,
+	.tuple.src.u.tcp.port	 = __constant_htons(SL_PORT),
+	.me					   = THIS_MODULE,
+	.help					 = sl_help,
+	.expect_policy			= &sl_exp_policy,
 };
   
 
@@ -238,23 +238,23 @@ static void nf_conntrack_sl_fini(void)
 {
 
 #ifdef SL_DEBUG
-	    printk(KERN_DEBUG " unregistering for port %d\n", SL_PORT);
+	printk(KERN_DEBUG " unregistering for port %d\n", SL_PORT);
 #endif
 
-        nf_conntrack_helper_unregister(&sl_helper); 
+	nf_conntrack_helper_unregister(&sl_helper); 
 }
 
 static int __init nf_conntrack_sl_init(void)
 {
  
-        int ret = 0;
+	int ret = 0;
 
 
 #ifdef SL_DEBUG
-        printk(KERN_DEBUG "Registering nf_conntrack_sl, port %d\n", SL_PORT);
+	printk(KERN_DEBUG "Registering nf_conntrack_sl, port %d\n", SL_PORT);
 #endif
 
-        ret = nf_conntrack_helper_register(&sl_helper);
+	ret = nf_conntrack_helper_register(&sl_helper);
 	if (ret < 0) {
 
 	  printk(KERN_ERR "error registering module: %d\n\n", ret);
