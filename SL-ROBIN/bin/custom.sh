de3bec15ba328f48c190a593062baf21516af89c
Fred Moyer <fred@redhotpenguin.com>
2009-12-10 18:37:01 +0000

    Update custom.sh for new firmware
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@3354 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-ROBIN/bin/custom.sh
+++ SL-ROBIN/bin/custom.sh
@@ -2,9 +2,9 @@
 
 VERSION=0.14
 DESCRIPTION="This program installs the Silver Lining ipkgs onto Open-Mesh.com ROBIN enabled devices.\n\n"
-LICENSE="Copyright 2009 Silver Lining Networks, Inc., email support@silverliningnetworks.com for the full license.\n"
-echo $DESCRIPTION
-echo $LICENSE
+LICENSE="Copyright 2009 Silver Lining Networks, Inc., email support@silverliningnetworks.com for a license copy.\n"
+logger -st $DESCRIPTION
+logger -st $LICENSE
 
 # see if there is enough room
 FREE_MEM=$(free |grep 'Mem:' |awk '{print $4}')
@@ -19,12 +19,12 @@ KVERSION=$(uname -r | awk -F '\.' '{print $3}')
 # bash sucks
 KVOK=0
 if [ $KVERSION -eq 23 ] ; then
-    echo "found kernel version 2.6.23.17"
+    logger -st "found kernel version 2.6.23.17"
     KVOK=1
 fi
 
 if [ $KVERSION -eq 26 ] ; then
-    echo "found kernel version 2.6.26.28"
+    logger -st "found kernel version 2.6.26.28"
     KVOK=1
 fi
 
@@ -66,8 +66,10 @@ elif [ $KVERSION -eq 26 ] ; then
     KMODSLN_FILE=kmod-sln_$KMOD_EXT\_mips.ipk
     SLN_EXT=$SL_VER-$SLN_RELEASE
     SLN_FILE=sln_$SLN_EXT\_mips.ipk
+    TEXTSEARCH_FILE=kmod-textsearch_$KMOD_EXT\_mips.ipk
     URL_KMODSLN=http://fw.slwifi.com/SL-ROBIN/sln/$SL_VER\_mips/$KMODSLN_FILE
     URL_SLN=http://fw.slwifi.com/SL-ROBIN/sln/$SL_VER\_mips/$SLN_FILE
+    URL_TEXTSEARCH=http://fw.slwifi.com/SL-ROBIN/textsearch/$TEXTSEARCH_FILE
 fi
 
 MP_VER=5.10.1-1
@@ -77,7 +79,7 @@ WGET="/usr/bin/wget -T 30 -t 2"
 
 # assume we are being executed in a safe environment, so we don't
 # need to shut down cron, etc
-echo "Starting SLN ipkg install"
+logger -st "Starting SLN ipkg install"
 cd /tmp
 
 # determine whether or not we should start silverlining
@@ -88,12 +90,12 @@ LOAD=0
 IPKG=microperl
 
 # remove existing files
-echo "removing old $IPKG files"
+logger -st "removing old $IPKG files"
 [ -e $MICROPERL_FILE ] && rm -f $MICROPERL_FILE
 [ -e $MICROPERL_FILE.md5 ] && rm -f $MICROPERL_FILE.md5
 
 # is $IPKG installed?  skip it
-echo "checking for old $IPKG install"
+logger -st "checking for old $IPKG install"
 
 if [ $KVERSION -eq 23 ] ; then
     MICROPERL_INSTALLED=$($TOOL list_installed $IPKG)
@@ -103,7 +105,7 @@ if [ $KVERSION -eq 23 ] ; then
     fi
 elif [ $KVERSION -eq 26 ] ; then
     MICROPERL_INSTALLED=$($TOOL list_installed $IPKG | awk '{ print $3 }')
-    echo "is microperl installed?,  $MICROPERL_INSTALLED"
+    logger -st "is microperl installed?,  $MICROPERL_INSTALLED"
     if ! [ $MICROPERL_INSTALLED ] ; then
         MICROPERL_INSTALLED=
     fi
@@ -120,49 +122,49 @@ fi
 
 # if microperl is not installed already then install it
 if [ -z $MICROPERL_INSTALLED ] ; then
-    echo "Yes, install microperl"
+    logger -st "Yes, install microperl"
     INSTALL_MP=1
 fi
 
 if [ $INSTALL_MP -eq 0 ] ; then
-    echo "$IPKG $MICROPERL_INSTALLED already installed"
+    logger -st "$IPKG $MICROPERL_INSTALLED already installed"
 else
-    echo "installing $IPKG"
+    logger -st "installing $IPKG"
 
     # grab the new package
-    echo "grabbing new $IPKG files"
+    logger -st "grabbing new $IPKG files"
     $($WGET $URL_MICROPERL)
     if [ $? -ne 0 ] ; then
-        echo "could not retrieve $URL_MICROPERL"
+        logger -st "could not retrieve $URL_MICROPERL"
         exit 1
     fi
     $($WGET "$URL_MICROPERL.md5")
     if [ $? -ne 0 ] ; then
-        echo "could not retrieve $URL_MICROPERL.md5"
+        logger -st "could not retrieve $URL_MICROPERL.md5"
         exit 1
     fi
 
     # check the md5
-    echo "checking md5s"
+    logger -st "checking md5s"
 
     MICROPERL_DL_MD5=$(/usr/bin/md5sum $MICROPERL_FILE | head -c 32)
     MICROPERL_MD5=$(/bin/cat $MICROPERL_FILE.md5 | head -c 32);
 
     if [ $MICROPERL_MD5 != $MICROPERL_DL_MD5 ] ; then
 
-        echo "md5sum mismatch installing $URL_MICROPERL"
-        echo "Expected md5sum - $MICROPERL_MD5"
-        echo "Calculated md5sum - $MICROPERL_DL_MD5"
+        logger -st "md5sum mismatch installing $URL_MICROPERL"
+        logger -st "Expected md5sum - $MICROPERL_MD5"
+        logger -st "Calculated md5sum - $MICROPERL_DL_MD5"
 
         exit 1
     fi
 
     # md5s check out, install the new ipkg
-    echo "installing new package $MICROPERL_FILE"
+    logger -st "installing new package $MICROPERL_FILE"
 
     INSTALLED=$($TOOL -V3 install "$MICROPERL_FILE")
 
-    echo "$MICROPERL_FILE installed ok - $INSTALLED"
+    logger -st "$MICROPERL_FILE installed ok - $INSTALLED"
 
     LOAD_SLN=1
 
@@ -171,6 +173,57 @@ else
     [ -e $MICROPERL_FILE.md5 ] && rm -f $MICROPERL_FILE.md5
 fi
 
+
+if [ $KVERSION -eq 23 ] ; then
+    ################################
+    # install textsearch kernel modules next
+    IPKG=kmod-textsearch
+
+    [ -e $TEXTSEARCH_FILE ] && rm -f $TEXTSEARCH_FILE
+    [ -e $TEXTSEARCH_FILE.md5 ] && rm -f $TEXTSEARCH_FILE.md5
+
+    TEXTSEARCH_INSTALLED=$($TOOL list_installed $IPKG | awk '{ print $3 }')
+    if ! [ $TEXTSEARCH_INSTALLED ] ; then
+    
+        # install the module
+    
+        # grab the packages
+        logger -st "grabbing new $IPKG files"
+        $($WGET $URL_TEXTSEARCH)
+        if [ $? -ne 0 ] ; then
+            logger -st "could not retrieve $URL_TEXTSEARCH"
+            exit 1
+        fi
+        $($WGET "$URL_TEXTSEARCH.md5")
+        if [ $? -ne 0 ] ; then
+            logger -st "could not retrieve $URL_TEXTSEARCH.md5"
+            exit 1
+        fi
+
+        # check the md5
+        logger -st "checking md5s"
+
+        TEXTSEARCH_DL_MD5=$(/usr/bin/md5sum $TEXTSEARCH_FILE | head -c 32)
+        TEXTSEARCH_MD5=$(/bin/cat $TEXTSEARCH_FILE.md5 | head -c 32);
+        logger -st "calculated md5 is $TEXTSEARCH_DL_MD5"
+        logger -st "expected md5 is   $TEXTSEARCH_MD5"
+
+        if [ $TEXTSEARCH_DL_MD5 != $TEXTSEARCH_MD5 ] ; then
+
+            logger -st "md5sum mismatch installing $URL_TEXTSEARCH"
+            exit 1
+        fi
+
+        # md5s check out, install the new ipkg
+        logger -st "installing new package $TEXTSEARCH_FILE"
+
+        INSTALLED=$($TOOL -V3 install "$TEXTSEARCH_FILE")
+
+        logger -st "$TEXTSEARCH_FILE installed ok - $INSTALLED"
+
+        [ -e $TEXTSEARCH_FILE ] && rm -f $TEXTSEARCH_FILE
+        [ -e $TEXTSEARCH_FILE.md5 ] && rm -f $TEXTSEARCH_FILE.md5
+fi
 
 
 ################################
@@ -182,7 +235,7 @@ IPKG=kmod-sln
 [ -e $KMODSLN_FILE.md5 ] && rm -f $KMODSLN_FILE.md5
 
 # is $IPKG installed?  remove it
-echo "checking for old $IPKG install"
+logger -st "checking for old $IPKG install"
 
 if [ $KVERSION -eq 23 ] ; then
     KMOD_INSTALLED=$($TOOL list_installed $IPKG)
@@ -192,7 +245,7 @@ if [ $KVERSION -eq 23 ] ; then
     fi
 elif [ $KVERSION -eq 26 ] ; then
     KMOD_INSTALLED=$($TOOL list_installed $IPKG | awk '{ print $3 }')
-    echo "$KMOD_EXT is kmod_sln installed?,  $KMOD_INSTALLED"
+    logger -st "$KMOD_EXT is kmod_sln installed?,  $KMOD_INSTALLED"
     if ! [ $KMOD_INSTALLED ] ; then
         KMOD_INSTALLED=0
     fi
@@ -201,47 +254,47 @@ fi
 
 if [ $KMOD_INSTALLED == $KMOD_EXT ] ; then
 
-    echo "kmod-sln versions are equal, $KMOD_EXT, $KMOD_INSTALLED"
+    logger -st "kmod-sln versions are equal, $KMOD_EXT, $KMOD_INSTALLED"
 else
-    echo "old kmod-sln version $KMOD_INSTALLED installed, removing"
+    logger -st "old kmod-sln version $KMOD_INSTALLED installed, removing"
     $(/usr/bin/sl_fw_ha allstop)
     `$TOOL -V3 remove -force-depends $IPKG`
 
     # grab the packages
-    echo "grabbing new $IPKG files"
+    logger -st "grabbing new $IPKG files"
     $($WGET $URL_KMODSLN)
     if [ $? -ne 0 ] ; then
-        echo "could not retrieve $URL_KMODSLN"
+        logger -st "could not retrieve $URL_KMODSLN"
         exit 1
     fi
     $($WGET "$URL_KMODSLN.md5")
     if [ $? -ne 0 ] ; then
-        echo "could not retrieve $URL_KMODSLN.md5"
+        logger -st "could not retrieve $URL_KMODSLN.md5"
         exit 1
     fi
 
     # check the md5
-    echo "checking md5s"
+    logger -st "checking md5s"
 
     KMODSLN_DL_MD5=$(/usr/bin/md5sum $KMODSLN_FILE | head -c 32)
     KMODSLN_MD5=$(/bin/cat $KMODSLN_FILE.md5 | head -c 32);
-    echo "calculated md5 is $KMODSLN_DL_MD5"
-    echo "expected md5 is   $KMODSLN_MD5"
+    logger -st "calculated md5 is $KMODSLN_DL_MD5"
+    logger -st "expected md5 is   $KMODSLN_MD5"
 
     if [ $KMODSLN_MD5 != $KMODSLN_DL_MD5 ] ; then
 
-        echo "md5sum mismatch installing $URL_KMODSLN"
+        logger -st "md5sum mismatch installing $URL_KMODSLN"
         exit 1
     fi
 
     # md5s check out, install the new ipkg
-    echo "installing new package $KMODSLN_FILE"
+    logger -st "installing new package $KMODSLN_FILE"
 
     INSTALLED=$($TOOL -V3 install "$KMODSLN_FILE")
 
     LOAD_SLN=1
 
-    echo "$KMODSLN_FILE installed ok - $INSTALLED"
+    logger -st "$KMODSLN_FILE installed ok - $INSTALLED"
 
     [ -e $KMODSLN_FILE ] && rm -f $KMODSLN_FILE
     [ -e $KMODSLN_FILE.md5 ] && rm -f $KMODSLN_FILE.md5
@@ -257,7 +310,7 @@ IPKG=sln
 [ -e $SLN_FILE.md5 ] && rm -f $SLN_FILE.md5
 
 # is $IPKG installed?  remove it
-echo "checking for old $IPKG install"
+logger -st "checking for old $IPKG install"
 
 if [ $KVERSION -eq 23 ] ; then
     SLN_INSTALLED=$($TOOL list_installed $IPKG)
@@ -267,7 +320,7 @@ if [ $KVERSION -eq 23 ] ; then
     fi
 elif [ $KVERSION -eq 26 ] ; then
     SLN_INSTALLED=$($TOOL list_installed $IPKG | awk '{ print $3 }')
-    echo "is sln installed?,  $SLN_INSTALLED"
+    logger -st "is sln installed?,  $SLN_INSTALLED"
     if ! [ $SLN_INSTALLED ] ; then
         SLN_INSTALLED=0
     fi
@@ -275,47 +328,47 @@ fi
 
 if [ $SLN_INSTALLED == $SLN_EXT ] ; then
 
-    echo "sln versions are equal, $SLN_EXT, $SLN_INSTALLED"
+    logger -st "sln versions are equal, $SLN_EXT, $SLN_INSTALLED"
 else
-    echo "old sln version $SLN_INSTALLED installed, removing"
+    logger -st "old sln version $SLN_INSTALLED installed, removing"
     $(/usr/bin/sl_fw_ha allstop)
     `$TOOL -V3 remove -force-depends $IPKG`
 
     # grab the packages
-    echo "grabbing new $IPKG files"
+    logger -st "grabbing new $IPKG files"
     $($WGET $URL_SLN)
     if [ $? -ne 0 ] ; then
-        echo "could not retrieve $URL_SLN"
+        logger -st "could not retrieve $URL_SLN"
         exit 1
     fi
     $($WGET "$URL_SLN.md5")
     if [ $? -ne 0 ] ; then
-        echo "could not retrieve $URL_SLN.md5"
+        logger -st "could not retrieve $URL_SLN.md5"
         exit 1
     fi
 
     # check the md5
-    echo "checking md5s"
+    logger -st "checking md5s"
 
     SLN_DL_MD5=$(/usr/bin/md5sum $SLN_FILE | head -c 32)
     SLN_MD5=$(/bin/cat $SLN_FILE.md5 | head -c 32);
-    echo "calculated md5 is $SLN_DL_MD5"
-    echo "expected md5 is   $SLN_MD5"
+    logger -st "calculated md5 is $SLN_DL_MD5"
+    logger -st "expected md5 is   $SLN_MD5"
 
     if [ $SLN_MD5 != $SLN_DL_MD5 ] ; then
 
-        echo "md5sum mismatch installing $URL_SLN"
+        logger -st "md5sum mismatch installing $URL_SLN"
         exit 1
     fi
 
     # md5s check out, install the new ipkg
-    echo "installing new package $SLN_FILE"
+    logger -st "installing new package $SLN_FILE"
 
     INSTALLED=$($TOOL -V3 install --force-overwrite "$SLN_FILE")
 
     LOAD_SLN=1
 
-    echo "$SLN_FILE installed ok - $INSTALLED"
+    logger -st "$SLN_FILE installed ok - $INSTALLED"
 
     [ -e $SLN_FILE ] && rm -f $SLN_FILE
     [ -e $SLN_FILE.md5 ] && rm -f $SLN_FILE.md5
@@ -325,26 +378,26 @@ fi
 
 if [ "$LOAD_SLN" -eq 1 ] ; then
 
-    echo "Starting Silver Lining..."
+    logger -st "Starting Silver Lining..."
 
 
     $(/etc/init.d/sln start)
     if [ $? -ne 0 ] ; then
 
-        echo "error starting silverlining"
+        logger -st "error starting silverlining"
         exit 1
     fi
 
     # success!
-    echo "silverlining loaded succesfully!"
+    logger -st "silverlining loaded succesfully!"
 
 else
 
-    echo "silverlining versions up to date, nothing installed"
+    logger -st "silverlining versions up to date, nothing installed"
     exit
 fi
 
-echo "SLN installation finished"
+logger -st "SLN installation finished"
     
 # see if this is an OM1P
 phyID1=$(/usr/sbin/mii-tool -vv eth0 |awk 'FNR>2'|head -n 1 |awk '{print $3}')
@@ -353,7 +406,7 @@ phyID2=$(/usr/sbin/mii-tool -vv eth0 |awk 'FNR>2'|head -n 1 |awk '{print $4}')
 phyID="${phyID1}:${phyID2}"
 if [ "$phyID" == "004d:d021" ] ; then
         BOARD="OM1P"
-        echo "OM1P detected, rebooting"
+        logger -st "OM1P detected, rebooting"
 	    /sbin/do_reboot 91
 fi
 
