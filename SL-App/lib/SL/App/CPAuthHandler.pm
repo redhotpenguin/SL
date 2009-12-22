eb25cc1e38cfc9bc71659416db3ea1d20d3e1593
Fred Moyer <fred@redhotpenguin.com>
2009-12-22 09:24:06 +0000

    more changes
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@3403 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-App/lib/SL/App/CPAuthHandler.pm
+++ SL-App/lib/SL/App/CPAuthHandler.pm
@@ -20,25 +20,8 @@ sub handler {
 
     my $url = $r->construct_url( $r->unparsed_uri );
 
-    my ($location) =
-      SL::Model::App->resultset('Location')->search( { ip => $ip } );
-
-    unless ($location) {
-        $r->log->info("no registered location for ip $ip and url $url");
-        return Apache2::Const::NOT_FOUND;
-    }
-
-    my ($router__location) =
-      SL::Model::App->resultset('RouterLocation')
-      ->search( { location_id => $location->location_id },
-        { order_by => 'router.last_ping DESC', }, join => [ 'router' ], );
-
-    unless ($router__location) {
-        $r->log->error("$$ no registered routers at ip $ip and url $url");
-        return Apache2::Const::NOT_FOUND;
-    }
-
-    my $router = $router__location->router;
+    my ($router) =
+      SL::Model::App->resultset('Router')->search( { wan_ip => $ip } );
 
     unless ($router
         && $router->lan_ip
