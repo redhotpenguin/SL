a4af424001774749d55547719f2006363aafd3ff
Fred Moyer <fred@redhotpenguin.com>
2010-01-18 23:49:29 +0000

    configure new IAB ad sizes for stealth swap
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@3438 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Model/bin/sl_update_schema.pl
+++ SL-Model/bin/sl_update_schema.pl
@@ -28,6 +28,19 @@ my $dbh = DBI->connect( $dsn, 'phred', '', $db_options );
 
 ##############################
 use SL::Model::App;
-$dbh->do("alter table ad_size add column height integer not null default 0");
-$dbh->do("alter table ad_size add column width integer not null default 0");
+$dbh->do("alter table ad_size add column swap boolean default 'f'");
+$dbh->do("update ad_size set width=728,height=90,swap='t', grouping=3 where ad_size_id=1");
+$dbh->do("update ad_size set width=468,height=60,swap='t', grouping=13 where ad_size_id=2");
+$dbh->do("update ad_size set width=120,height=600,swap='t', grouping=18 where ad_size_id=4");
+$dbh->do("update ad_size set width=160,height=600,swap='t', grouping=4 where ad_size_id=5");
+$dbh->do("update ad_size set width=300,height=600,swap='t', grouping=5 where ad_size_id=6");
+$dbh->do("update ad_size set width=300,height=250,swap='t', grouping=1 where ad_size_id=15");
+$dbh->do("update ad_size set width=250,height=250,swap='t', grouping=8 where ad_size_id=16");
+$dbh->do("update ad_size set width=240,height=400,swap='t', grouping=9 where ad_size_id=17");
+$dbh->do("update ad_size set width=336,height=280,swap='t', grouping=10 where ad_size_id=18");
+$dbh->do("update ad_size set width=180,height=150,swap='t', grouping=2 where ad_size_id=19");
+$dbh->do("update ad_size set width=120,height=90,swap='t', grouping=15 where ad_size_id=20");
+$dbh->do("update ad_size set width=120,height=60,swap='t', grouping=6 where ad_size_id=21");
+$dbh->do("update ad_size set width=88,height=31,swap='t', grouping=7 where ad_size_id=24");
+
 
