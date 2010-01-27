f7c83fb6b7d95e222495b22549725a688a50179e
Fred Moyer <fred@redhotpenguin.com>
2010-01-27 01:19:37 +0000

    track zone insertions per day
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@3442 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Model/lib/SL/Model/App/AdZone.pm
+++ SL-Model/lib/SL/Model/App/AdZone.pm
@@ -118,6 +118,14 @@ __PACKAGE__->add_columns(
     is_nullable => 0,
     size => 4,
   },
+  "insertions_yesterday",
+  {
+    data_type => "integer",
+    default_value => 0,
+    is_foreign_key => 0,
+    is_nullable => 0,
+    size => 4,
+  },
 );
 __PACKAGE__->set_primary_key("ad_zone_id");
 __PACKAGE__->belongs_to("reg", "SL::Model::App::Reg", { reg_id => "reg_id" });
