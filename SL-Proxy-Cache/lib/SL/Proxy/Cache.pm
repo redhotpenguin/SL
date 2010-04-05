e916f396c64acc6596e8b1f68ff16cbd33b78ffd
Fred Moyer <fred@redhotpenguin.com>
2010-04-05 01:09:55 +0000

    more changes
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@3477 bd012f14-6cfe-0310-9a23-91b8f1fede10

--- SL-Proxy-Cache/lib/SL/Proxy/Cache.pm
+++ SL-Proxy-Cache/lib/SL/Proxy/Cache.pm
@@ -6,7 +6,7 @@ use warnings;
 use SL::Config;
 use Cache::Memcached ();
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 our ($Config, $Memd);
 
@@ -15,8 +15,19 @@ BEGIN {
     $Memd = Cache::Memcached->new({ servers => [ '127.0.0.1:11211' ] });
 }
 
+sub new {
+    my $class = shift;
+
+    my %self = ( memd => $Memd );
+
+    bless \%self, $class;
+
+    return \%self;
+}
+
 sub memd {
-  return $Memd;
+  my $self = shift;
+  return $self->{memd};
 }
 
 sub add_known_html {
@@ -30,13 +41,34 @@ sub add_known_html {
     return 1;
 }
 
-sub is_known_not_html {
+
+sub is_known_html {
     my ( $self, $url ) = @_;
     die unless $url;
 
     my $content_type = $self->memd->get( join ( '|', 'known_html', $url ) );
     return 1 if ( $content_type && ( $content_type !~ m/text\/html/ ) );
     return;
+}
+
+
+sub add_known_not_html {
+   my ( $self, $url, $content_type ) = @_;
+    unless ( $url && $content_type ) {
+        warn("url $url or content type $content_type missing");
+        return;
+    }
+
+    $self->memd->set( join ( '|', 'known_not_html', $url ) => $content_type );
+    return 1;
+}
+
+sub is_known_not_html {
+    my ( $self, $url ) = @_;
+    die unless $url;
+
+    my $content_type = $self->memd->get( join ( '|', 'known_not_html', $url ));
+    return $content_type;
 }
 
 1;
