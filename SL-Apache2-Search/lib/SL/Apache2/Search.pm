e0c12141253e73c369cf093c78ba1eabea756098
Fred Moyer <fred@redhotpenguin.com>
2010-05-10 18:49:08 +0000

    Search m odule
    git-svn-id: https://www.redhotpenguin.com/svn/sl/trunk@3514 bd012f14-6cfe-0310-9a23-91b8f1fede10

---
+++ SL-Apache2-Search/lib/SL/Apache2/Search.pm
@@ -0,0 +1,41 @@
+package SL::Apache2::Search;
+
+use strict;
+use warnings;
+
+our $VERSION = 0.01;
+
+=head1 NAME
+
+ SL::Apache2::Search - mod_perl2 silverlining search handler
+
+=cut
+
+sub handler {
+
+
+}
+
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+  PerlResponseHandler SL::Apache2::Search
+
+=head1 DESCRIPTION
+
+Does searching.
+
+=head1 AUTHOR
+
+Fred Moyer <fred@slwifi.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Silver Lining Networks.
+
+This software is proprietary under the Silver Lining Networks software license.
+
+=cut
